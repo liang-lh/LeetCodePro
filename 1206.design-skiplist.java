2e2c3cc48d29f9f6da8951b8b4e6f5f2e2e27adc
liang-lh <liang-lh@users.noreply.github.com>
2026-02-10 04:51:10 +0800

    Batch add 1 solutions: 1206.design-skiplist...

--- 1206.design-skiplist.java
+++ 1206.design-skiplist.java
@@ -4,36 +4,32 @@
 # [1206] Design Skiplist
 #
 # @lc code=start
-import java.util.*;
+import java.util.Random;
+
 class Skiplist {
-    static class Node {
+    private static final int MAX_LEVEL = 16;
+    private static final double P = 0.5;
+    
+    class Node {
         int val;
         Node[] forward;
+        
         Node(int val, int level) {
             this.val = val;
             this.forward = new Node[level + 1];
         }
     }
-    private static final int MAX_LEVEL = 16;
-    private static final double P = 0.5;
+    
     private Node head;
     private int level;
-    private Random rand;
-
+    private Random random;
+    
     public Skiplist() {
         head = new Node(-1, MAX_LEVEL);
         level = 0;
-        rand = new Random();
+        random = new Random();
     }
-
-    private int randomLevel() {
-        int lvl = 0;
-        while (lvl < MAX_LEVEL && rand.nextDouble() < P) {
-            lvl++;
-        }
-        return lvl;
-    }
-
+    
     public boolean search(int target) {
         Node curr = head;
         for (int i = level; i >= 0; i--) {
@@ -44,57 +40,77 @@ class Skiplist {
         curr = curr.forward[0];
         return curr != null && curr.val == target;
     }
-
+    
     public void add(int num) {
         Node[] update = new Node[MAX_LEVEL + 1];
         Node curr = head;
+        
         for (int i = level; i >= 0; i--) {
             while (curr.forward[i] != null && curr.forward[i].val < num) {
                 curr = curr.forward[i];
             }
             update[i] = curr;
         }
-        int lvl = randomLevel();
-        if (lvl > level) {
-            for (int i = level + 1; i <= lvl; i++) {
+        
+        int newLevel = randomLevel();
+        if (newLevel > level) {
+            for (int i = level + 1; i <= newLevel; i++) {
                 update[i] = head;
             }
-            level = lvl;
+            level = newLevel;
         }
-        Node newNode = new Node(num, lvl);
-        for (int i = 0; i <= lvl; i++) {
+        
+        Node newNode = new Node(num, newLevel);
+        for (int i = 0; i <= newLevel; i++) {
             newNode.forward[i] = update[i].forward[i];
             update[i].forward[i] = newNode;
         }
     }
-
+    
     public boolean erase(int num) {
         Node[] update = new Node[MAX_LEVEL + 1];
         Node curr = head;
+        
         for (int i = level; i >= 0; i--) {
             while (curr.forward[i] != null && curr.forward[i].val < num) {
                 curr = curr.forward[i];
             }
             update[i] = curr;
         }
+        
         curr = curr.forward[0];
-        if (curr == null || curr.val != num) return false;
+        if (curr == null || curr.val != num) {
+            return false;
+        }
+        
         for (int i = 0; i <= level; i++) {
-            if (update[i].forward[i] != curr) break;
+            if (update[i].forward[i] != curr) {
+                break;
+            }
             update[i].forward[i] = curr.forward[i];
         }
+        
         while (level > 0 && head.forward[level] == null) {
             level--;
         }
+        
         return true;
+    }
+    
+    private int randomLevel() {
+        int lvl = 0;
+        while (lvl < MAX_LEVEL && random.nextDouble() < P) {
+            lvl++;
+        }
+        return lvl;
     }
 }
 
 /**
-* Your Skiplist object will be instantiated and called as such:
-* Skiplist obj = new Skiplist();
-* boolean param_1 = obj.search(target);
-* obj.add(num);
-* boolean param_3 = obj.erase(num);
-*/
+ * Your Skiplist object will be instantiated and called as such:
+ * Skiplist obj = new Skiplist();
+ * boolean param_1 = obj.search(target);
+ * obj.add(num);
+ * boolean param_3 = obj.erase(num);
+ */
 # @lc code=end
