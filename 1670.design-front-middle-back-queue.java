fda9c1db95b2e39ada6b936524b8eb3338d73d49
liang-lh <liang-lh@users.noreply.github.com>
2026-02-10 04:54:15 +0800

    Batch add 1 solutions: 1670.design-front-mi...

--- 1670.design-front-middle-back-queue.java
+++ 1670.design-front-middle-back-queue.java
@@ -4,87 +4,54 @@
 # [1670] Design Front Middle Back Queue
 #
 # @lc code=start
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 class FrontMiddleBackQueue {
-    LinkedList<Integer> left;
-    LinkedList<Integer> right;
-
+    private List<Integer> list;
+    
     public FrontMiddleBackQueue() {
-        left = new LinkedList<>();
-        right = new LinkedList<>();
+        list = new ArrayList<>();
     }
-
-    private void balance() {
-        // Maintain the invariant: left.size() == right.size() or left.size() == right.size() + 1
-        while (left.size() > right.size() + 1) {
-            right.addFirst(left.removeLast());
-        }
-        while (left.size() < right.size()) {
-            left.addLast(right.removeFirst());
-        }
-    }
-
+    
     public void pushFront(int val) {
-        left.addFirst(val);
-        balance();
+        list.add(0, val);
     }
-
+    
     public void pushMiddle(int val) {
-        // Ensure the new element becomes the frontmost middle in even-sized queues
-        if (left.size() > right.size()) {
-            right.addFirst(left.removeLast());
-        }
-        left.addLast(val);
-        balance();
+        int mid = list.size() / 2;
+        list.add(mid, val);
     }
-
+    
     public void pushBack(int val) {
-        right.addLast(val);
-        balance();
+        list.add(val);
     }
-
+    
     public int popFront() {
-        if (left.isEmpty() && right.isEmpty()) return -1;
-        int res;
-        if (!left.isEmpty()) {
-            res = left.removeFirst();
-        } else {
-            res = right.removeFirst();
-        }
-        balance();
-        return res;
+        if (list.isEmpty()) return -1;
+        return list.remove(0);
     }
-
+    
     public int popMiddle() {
-        if (left.isEmpty() && right.isEmpty()) return -1;
-        // Always remove the end of left (frontmost middle in even-sized queue)
-        int res = left.removeLast();
-        balance();
-        return res;
+        if (list.isEmpty()) return -1;
+        int mid = (list.size() - 1) / 2;
+        return list.remove(mid);
     }
-
+    
     public int popBack() {
-        if (left.isEmpty() && right.isEmpty()) return -1;
-        int res;
-        if (!right.isEmpty()) {
-            res = right.removeLast();
-        } else {
-            res = left.removeLast();
-        }
-        balance();
-        return res;
+        if (list.isEmpty()) return -1;
+        return list.remove(list.size() - 1);
     }
 }
 
 /**
-* Your FrontMiddleBackQueue object will be instantiated and called as such:
-* FrontMiddleBackQueue obj = new FrontMiddleBackQueue();
-* obj.pushFront(val);
-* obj.pushMiddle(val);
-* obj.pushBack(val);
-* int param_4 = obj.popFront();
-* int param_5 = obj.popMiddle();
-* int param_6 = obj.popBack();
-*/
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue obj = new FrontMiddleBackQueue();
+ * obj.pushFront(val);
+ * obj.pushMiddle(val);
+ * obj.pushBack(val);
+ * int param_4 = obj.popFront();
+ * int param_5 = obj.popMiddle();
+ * int param_6 = obj.popBack();
+ */
 # @lc code=end
