8120421fcb5f64f6e10eaca63ac1e8c02c315fbe
liang-lh <liang-lh@users.noreply.github.com>
2026-02-10 23:08:33 +0800

    Batch add 1 solutions: 3217.delete-nodes-fr...

--- 3217.delete-nodes-from-linked-list-present-in-array.go
+++ 3217.delete-nodes-from-linked-list-present-in-array.go
@@ -1,31 +1,8 @@
-#
-# @lc app=leetcode id=3217 lang=golang
-#
-# [3217] Delete Nodes From Linked List Present in Array
-#
-
-# @lc code=start
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func modifiedList(nums []int, head *ListNode) *ListNode {
-    numsSet := make(map[int]bool)
-    for _, num := range nums {
-        numsSet[num] = true
-    }
-    dummy := &ListNode{Val: 0, Next: head}
-    prev := dummy
-    for curr := head; curr != nil; curr = curr.Next {
-        if numsSet[curr.Val] {
-            prev.Next = curr.Next
-        } else {
-            prev = curr
-        }
-    }
-    return dummy.Next
+<solution_variables>
+<solution>
+{
+    "reasoning": "Step 1: Understand the problem - remove all nodes from the linked list whose value appears in the nums array and return the head of the modified list.\nStep 2: Create a map[int]bool from nums for O(1) value lookups since nums is unique and up to 10^5.\nStep 3: Use a dummy node pointing to head to handle cases where head needs removal.\nStep 4: Initialize prev to dummy, iterate curr from head; if curr.Val in map, skip by setting prev.Next = curr.Next; else advance prev to curr.\nStep 5: Always advance curr to curr.Next.\nStep 6: Return dummy.Next as new head. This correctly handles all examples and constraints.",
+    "result": "#\n# @lc app=leetcode id=3217 lang=golang\n#\n# [3217] Delete Nodes From Linked List Present in Array\n#\n# @lc code=start\n/**\n* Definition for singly-linked list.\n* type ListNode struct {\n*     Val int\n*     Next *ListNode\n* }\n*/\nfunc modifiedList(nums []int, head *ListNode) *ListNode {\n\tdeleted := make(map[int]bool)\n\tfor _, num := range nums {\n\t\tdeleted[num] = true\n\t}\n\tdummy := &ListNode{0, head}\n\tprev := dummy\n\tfor curr := head; curr != nil; curr = curr.Next {\n\t\tif deleted[curr.Val] {\n\t\t\tprev.Next = curr.Next\n\t\t} else {\n\t\t\tprev = curr\n\t\t}\n\t}\n\treturn dummy.Next\n}\n# @lc code=end"
 }
-# @lc code=end
+</solution>
+</solution_variables>
