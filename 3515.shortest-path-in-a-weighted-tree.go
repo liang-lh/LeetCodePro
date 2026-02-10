b603392d7dc81923b6966f84978d6b8c2b3039c6
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 02:36:41 +0800

    Batch add 1 solutions: 3515.shortest-path-i...

--- 3515.shortest-path-in-a-weighted-tree.go
+++ 3515.shortest-path-in-a-weighted-tree.go
@@ -3,87 +3,116 @@
 #
 # [3515] Shortest Path in a Weighted Tree
 #
+
 # @lc code=start
-func treeQueries(n int, edges [][]int, queries [][]int) []int {
-    // Store edge weights in a map
-    edgeWeight := make(map[[2]int]int)
-    
-    // Build adjacency list (without weights, just connections)
-    graph := make(map[int][]int, n+1)
-    
-    for _, edge := range edges {
-        u, v, w := edge[0], edge[1], edge[2]
-        graph[u] = append(graph[u], v)
-        graph[v] = append(graph[v], u)
-        key := [2]int{min(u, v), max(u, v)}
-        edgeWeight[key] = w
-    }
-    
-    result := []int{}
-    
-    for _, query := range queries {
-        if len(query) == 4 && query[0] == 1 {
-            // Update query
-            u, v, w := query[1], query[2], query[3]
-            key := [2]int{min(u, v), max(u, v)}
-            edgeWeight[key] = w
-        } else if len(query) == 2 && query[0] == 2 {
-            // Distance query
-            x := query[1]
-            dist := computeDistance(graph, edgeWeight, 1, x, n)
-            result = append(result, dist)
-        }
-    }
-    
-    return result
+type Pair struct {
+    to int
+    w  int
 }
 
-func computeDistance(graph map[int][]int, edgeWeight map[[2]int]int, start, target, n int) int {
-    if start == target {
-        return 0
+type Fenwick struct {
+    tree []int
+    n    int
+}
+
+func (f *Fenwick) update(idx int, val int) {
+    for idx <= f.n {
+        f.tree[idx] += val
+        idx += idx & -idx
     }
-    
-    visited := make([]bool, n+1)
-    queue := []State{{start, 0}}
-    visited[start] = true
-    
-    for len(queue) > 0 {
-        curr := queue[0]
-        queue = queue[1:]
-        
-        if curr.node == target {
-            return curr.dist
+}
+
+func (f *Fenwick) prefix(idx int) int {
+    res := 0
+    for idx > 0 {
+        res += f.tree[idx]
+        idx -= idx & -idx
+    }
+    return res
+}
+
+type Frame struct {
+    Node, Par, Wto, ChildIdx int
+}
+
+func treeQueries(n int, edges [][]int, queries [][]int) []int {
+    adj := make([][]Pair, n+1)
+    for _, e := range edges {
+        u, v, w := e[0], e[1], e[2]
+        adj[u] = append(adj[u], Pair{to: v, w: w})
+        adj[v] = append(adj[v], Pair{to: u, w: w})
+    }
+    parent := make([]int, n+1)
+    edgeToParent := make([]int, n+1)
+    dfn := make([]int, n+1)
+    sz := make([]int, n+1)
+    timer := 1
+    stack := []Frame{{Node: 1, Par: 0, Wto: 0, ChildIdx: 0}}
+    for len(stack) > 0 {
+        idx := len(stack) - 1
+        f := &stack[idx]
+        if f.ChildIdx == 0 {
+            dfn[f.Node] = timer
+            timer++
+            parent[f.Node] = f.Par
+            edgeToParent[f.Node] = f.Wto
         }
-        
-        for _, neighbor := range graph[curr.node] {
-            if !visited[neighbor] {
-                visited[neighbor] = true
-                key := [2]int{min(curr.node, neighbor), max(curr.node, neighbor)}
-                weight := edgeWeight[key]
-                queue = append(queue, State{neighbor, curr.dist + weight})
+        found := false
+        for f.ChildIdx < len(adj[f.Node]) {
+            cand := adj[f.Node][f.ChildIdx]
+            f.ChildIdx++
+            if cand.to != f.Par {
+                stack = append(stack, Frame{Node: cand.to, Par: f.Node, Wto: cand.w, ChildIdx: 0})
+                found = true
+                break
             }
         }
+        if !found {
+            sz[f.Node] = 1
+            for _, p := range adj[f.Node] {
+                if p.to != f.Par {
+                    sz[f.Node] += sz[p.to]
+                }
+            }
+            stack = stack[:len(stack)-1]
+        }
     }
-    
-    return -1 // Should not reach here in a connected tree
-}
-
-type State struct {
-    node int
-    dist int
-}
-
-func min(a, b int) int {
-    if a < b {
-        return a
+    ft := &Fenwick{
+        tree: make([]int, n+3),
+        n:    n + 1,
     }
-    return b
-}
-
-func max(a, b int) int {
-    if a > b {
-        return a
+    for i := 2; i <= n; i++ {
+        l := dfn[i]
+        r1 := l + sz[i]
+        ft.update(l, edgeToParent[i])
+        ft.update(r1, -edgeToParent[i])
     }
-    return b
+    ans := []int{}
+    for _, q := range queries {
+        if len(q) == 2 {
+            x := q[1]
+            if x == 1 {
+                ans = append(ans, 0)
+                continue
+            }
+            ans = append(ans, ft.prefix(dfn[x]))
+        } else {
+            u := q[1]
+            v := q[2]
+            wnew := q[3]
+            child := v
+            if parent[v] != u {
+                child = u
+            }
+            wold := edgeToParent[child]
+            delta := wnew - wold
+            edgeToParent[child] = wnew
+            l := dfn[child]
+            r1 := l + sz[child]
+            ft.update(l, delta)
+            ft.update(r1, -delta)
+        }
+    }
+    return ans
 }
 # @lc code=end
