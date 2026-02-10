23940c768c714dd2346618fb5ee402d58bfcb9c2
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 03:06:46 +0800

    Batch add 1 solutions: 3544.subtree-inversi...

--- 3544.subtree-inversion-sum.go
+++ 3544.subtree-inversion-sum.go
@@ -3,78 +3,75 @@
 #
 # [3544] Subtree Inversion Sum
 #
+
 # @lc code=start
 func subtreeInversionSum(edges [][]int, nums []int, k int) int64 {
-    n := len(nums)
-    
-    // Build adjacency list from edges
-    graph := make([][]int, n)
-    for _, edge := range edges {
-        u, v := edge[0], edge[1]
-        graph[u] = append(graph[u], v)
-        graph[v] = append(graph[v], u)
-    }
-    
-    // Build rooted tree starting from node 0
-    children := make([][]int, n)
-    visited := make([]bool, n)
-    
-    var buildTree func(int)
-    buildTree = func(node int) {
-        visited[node] = true
-        for _, neighbor := range graph[node] {
-            if !visited[neighbor] {
-                children[node] = append(children[node], neighbor)
-                buildTree(neighbor)
-            }
-        }
-    }
-    buildTree(0)
-    
-    // Memoization for DP
-    type State struct {
-        node, cooldown, parity int
-    }
-    memo := make(map[State]int64)
-    
-    var dfs func(int, int, int) int64
-    dfs = func(node, cooldown, parity int) int64 {
-        state := State{node, cooldown, parity}
-        if val, exists := memo[state]; exists {
-            return val
-        }
-        
-        // Current node value with parity applied
-        val := int64(nums[node])
-        if parity == 1 {
-            val = -val
-        }
-        
-        // Option 1: Don't invert this node
-        result := val
-        for _, child := range children[node] {
-            nextCooldown := cooldown
-            if nextCooldown > 0 {
-                nextCooldown--
-            }
-            result += dfs(child, nextCooldown, parity)
-        }
-        
-        // Option 2: Invert this node (if allowed)
-        if cooldown == 0 {
-            inverted := -val
-            for _, child := range children[node] {
-                inverted += dfs(child, k-1, 1-parity)
-            }
-            if inverted > result {
-                result = inverted
-            }
-        }
-        
-        memo[state] = result
-        return result
-    }
-    
-    return dfs(0, 0, 0)
+	n := len(nums)
+	adj := make([][]int, n)
+	for _, e := range edges {
+		adj[e[0]] = append(adj[e[0]], e[1])
+		adj[e[1]] = append(adj[e[1]], e[0])
+	}
+	children := make([][]int, n)
+	var build func(int, int)
+	build = func(u, p int) {
+		for _, v := range adj[u] {
+			if v != p {
+				children[u] = append(children[u], v)
+				build(v, u)
+			}
+		}
+	}
+	build(0, -1)
+	maxdp := make([][]int64, n)
+	mindp := make([][]int64, n)
+	for i := 0; i < n; i++ {
+		maxdp[i] = make([]int64, k+1)
+		mindp[i] = make([]int64, k+1)
+	}
+	var dfs func(int)
+	dfs = func(u int) {
+		for _, c := range children[u] {
+			dfs(c)
+		}
+		num := int64(nums[u])
+		for d := 1; d <= k; d++ {
+			allowed := d == k
+			// inv0 s=1
+			cd0 := d
+			if d < k {
+				cd0 = d + 1
+			}
+			sum_best0 := int64(0)
+			sum_worst0 := int64(0)
+			for _, c := range children[u] {
+				sum_best0 += maxdp[c][cd0]
+				sum_worst0 += mindp[c][cd0]
+			}
+			max0 := num + sum_best0
+			min0 := num + sum_worst0
+			maxdp[u][d] = max0
+			mindp[u][d] = min0
+			if allowed {
+				cd1 := 1
+				sum_best1 := int64(0)
+				sum_worst1 := int64(0)
+				for _, c := range children[u] {
+					sum_best1 -= mindp[c][cd1]
+					sum_worst1 -= maxdp[c][cd1]
+				}
+				max1 := -num + sum_best1
+				min1 := -num + sum_worst1
+				if max1 > maxdp[u][d] {
+					maxdp[u][d] = max1
+				}
+				if min1 < mindp[u][d] {
+					mindp[u][d] = min1
+				}
+			}
+		}
+	}
+	dfs(0)
+	return maxdp[0][k]
 }
 # @lc code=end
