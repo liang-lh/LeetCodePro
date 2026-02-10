a3038acde0103767e47dd502197793f42760cf0a
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 01:31:22 +0800

    Batch add 1 solutions: 3464.maximize-the-di...

--- 3464.maximize-the-distance-between-points-on-a-square.go
+++ 3464.maximize-the-distance-between-points-on-a-square.go
@@ -3,82 +3,157 @@
 #
 # [3464] Maximize the Distance Between Points on a Square
 #
+
 # @lc code=start
 func maxDistance(side int, points [][]int, k int) int {
-    n := len(points)
-    
-    // Helper function to calculate Manhattan distance
-    manhattanDist := func(p1, p2 []int) int {
-        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-    }
-    
-    // Check if we can select k points with minimum distance d
-    canSelect := func(d int) bool {
-        // Use backtracking to find if we can select k points
-        var backtrack func(idx int, selected []int) bool
-        backtrack = func(idx int, selected []int) bool {
-            if len(selected) == k {
-                return true
-            }
-            if idx == n {
-                return false
-            }
-            
-            // Pruning: if remaining points + selected < k, we can't reach k
-            if n - idx + len(selected) < k {
-                return false
-            }
-            
-            // Try selecting current point if compatible
-            compatible := true
-            for _, s := range selected {
-                if manhattanDist(points[s], points[idx]) < d {
-                    compatible = false
-                    break
-                }
-            }
-            
-            if compatible {
-                newSelected := make([]int, len(selected)+1)
-                copy(newSelected, selected)
-                newSelected[len(selected)] = idx
-                if backtrack(idx+1, newSelected) {
-                    return true
-                }
-            }
-            
-            // Try not selecting current point
-            if backtrack(idx+1, selected) {
-                return true
-            }
-            
-            return false
-        }
-        
-        return backtrack(0, []int{})
-    }
-    
-    // Binary search on the answer
-    left, right := 0, 2*side
-    result := 0
-    
-    for left <= right {
-        mid := (left + right) / 2
-        if canSelect(mid) {
-            result = mid
-            left = mid + 1
-        } else {
-            right = mid - 1
-        }
-    }
-    
-    return result
+	lo, hi := 0, 2*side
+	for lo < hi {
+		mid := (lo + hi + 1) / 2
+		if check(side, points, k, mid) {
+			lo = mid
+		} else {
+			hi = mid - 1
+		}
+	}
+	return lo
 }
 
-func abs(x int) int {
-    if x < 0 {
-        return -x
-    }
-    return x
+func check(side int, points [][]int, k, d int) bool {
+	bottoms, rights, tops, lefts := []int{}, []int{}, []int{}, []int{}
+	for _, p := range points {
+		x, y := p[0], p[1]
+		if y == 0 {
+			bottoms = append(bottoms, x)
+		} else if x == side {
+			rights = append(rights, y)
+		} else if y == side {
+			tops = append(tops, x)
+		} else if x == 0 {
+			lefts = append(lefts, y)
+		}
+	}
+	sort.Ints(bottoms)
+	sort.Ints(rights)
+	sort.Ints(tops)
+	sort.Ints(lefts)
+
+	type SideInfo struct {
+		minmax [26]int
+		maxmin [26]int
+		mp     int
+	}
+	getInfo := func(ps []int) SideInfo {
+		info := SideInfo{}
+		for i := range info.minmax[:] {
+			info.minmax[i] = -1
+			info.maxmin[i] = -1
+		}
+		m := len(ps)
+		if m == 0 {
+			return info
+		}
+		// forward greedy: minmax
+		var fgreedy []int
+		prev := int64(-2000000000)
+		for _, cur := range ps {
+			if int64(cur)-prev >= int64(d) {
+				fgreedy = append(fgreedy, cur)
+				prev = int64(cur)
+			}
+		}
+		info.mp = len(fgreedy)
+		for rr := 1; rr <= info.mp && rr <= 25; rr++ {
+			info.minmax[rr] = fgreedy[rr-1]
+		}
+		// backward greedy: maxmin
+		var bgreedy []int
+		prev = 2000000002
+		for ii := m - 1; ii >= 0; ii-- {
+			cur := int64(ps[ii])
+			if prev-cur >= int64(d) {
+				bgreedy = append(bgreedy, ps[ii])
+				prev = cur
+			}
+		}
+		for rr := 1; rr <= len(bgreedy) && rr <= 25; rr++ {
+			info.maxmin[rr] = bgreedy[rr-1]
+		}
+		return info
+	}
+	infB := getInfo(bottoms)
+	infR := getInfo(rights)
+	infT := getInfo(tops)
+	infL := getInfo(lefts)
+
+	delta := 0
+	if d > side {
+		delta = d - side
+	}
+
+	for aa := 0; aa <= k; aa++ {
+		if aa > 0 && (aa > infB.mp || infB.minmax[aa] == -1) {
+			continue
+		}
+		for bb := 0; bb <= k-aa; bb++ {
+			if bb > 0 && (bb > infR.mp || infR.minmax[bb] == -1) {
+				continue
+			}
+			for cc := 0; cc <= k-aa-bb; cc++ {
+				if cc > 0 && (cc > infT.mp || infT.minmax[cc] == -1) {
+					continue
+				}
+				ee := k - aa - bb - cc
+				if ee > infL.mp || (ee > 0 && infL.minmax[ee] == -1) {
+					continue
+				}
+				ok := true
+				// adjacent bottom-right
+				if aa > 0 && bb > 0 {
+					if infR.maxmin[bb] < infB.minmax[aa]+(d-side) {
+						ok = false
+					}
+				}
+				// right-top
+				if bb > 0 && cc > 0 {
+					if infR.minmax[bb]+infT.minmax[cc] > 2*side-d {
+						ok = false
+					}
+				}
+				// top-left
+				if cc > 0 && ee > 0 {
+					if infT.maxmin[cc] < infL.minmax[ee]+(d-side) {
+						ok = false
+					}
+				}
+				// left-bottom
+				if ee > 0 && aa > 0 {
+					if infB.maxmin[aa]+infL.maxmin[ee] < d {
+						ok = false
+					}
+				}
+				// opposite bottom-top
+				if aa > 0 && cc > 0 {
+					sep1 := infB.minmax[aa] + delta <= infT.maxmin[cc]
+					sep2 := infT.minmax[cc] + delta <= infB.maxmin[aa]
+					if delta > 0 && !sep1 && !sep2 {
+						ok = false
+					}
+				}
+				// opposite left-right
+				if ee > 0 && bb > 0 {
+					sep1 := infL.minmax[ee] + delta <= infR.maxmin[bb]
+					sep2 := infR.minmax[bb] + delta <= infL.maxmin[ee]
+					if delta > 0 && !sep1 && !sep2 {
+						ok = false
+					}
+				}
+				if ok {
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
+
 # @lc code=end
