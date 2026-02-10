3dbeb63f60ea522144c987b095ded74a99c3aef6
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 02:00:51 +0800

    Batch add 1 solutions: 3493.properties-grap...

--- 3493.properties-graph.go
+++ 3493.properties-graph.go
@@ -3,65 +3,59 @@
 #
 # [3493] Properties Graph
 #
+
 # @lc code=start
 func numberOfComponents(properties [][]int, k int) int {
-    n := len(properties)
-    
-    // Helper function to calculate intersection count of distinct integers
-    intersect := func(a, b []int) int {
-        setA := make(map[int]bool)
-        for _, val := range a {
-            setA[val] = true
-        }
-        
-        count := 0
-        counted := make(map[int]bool)
-        for _, val := range b {
-            if setA[val] && !counted[val] {
-                count++
-                counted[val] = true
-            }
-        }
-        return count
-    }
-    
-    // Union-Find data structure
-    parent := make([]int, n)
-    for i := range parent {
-        parent[i] = i
-    }
-    
-    var find func(int) int
-    find = func(x int) int {
-        if parent[x] != x {
-            parent[x] = find(parent[x]) // Path compression
-        }
-        return parent[x]
-    }
-    
-    union := func(x, y int) {
-        rootX := find(x)
-        rootY := find(y)
-        if rootX != rootY {
-            parent[rootX] = rootY
-        }
-    }
-    
-    // Build the graph by checking all pairs
-    for i := 0; i < n; i++ {
-        for j := i + 1; j < n; j++ {
-            if intersect(properties[i], properties[j]) >= k {
-                union(i, j)
-            }
-        }
-    }
-    
-    // Count connected components
-    components := make(map[int]bool)
-    for i := 0; i < n; i++ {
-        components[find(i)] = true
-    }
-    
-    return len(components)
+	n := len(properties)
+	adj := make([][]int, n)
+
+	intersect := func(a, b []int) int {
+		sa := make(map[int]struct{})
+		for _, x := range a {
+			sa[x] = struct{}{}
+		}
+		sb := make(map[int]struct{})
+		for _, x := range b {
+			sb[x] = struct{}{}
+		}
+		res := 0
+		for x := range sa {
+			if _, ok := sb[x]; ok {
+				res++
+			}
+		}
+		return res
+	}
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if intersect(properties[i], properties[j]) >= k {
+				adj[i] = append(adj[i], j)
+				adj[j] = append(adj[j], i)
+			}
+		}
+	}
+
+	visited := make([]bool, n)
+
+	var dfs func(int)
+	dfs = func(node int) {
+		visited[node] = true
+		for _, nei := range adj[node] {
+			if !visited[nei] {
+				dfs(nei)
+			}
+		}
+	}
+
+	components := 0
+	for i := 0; i < n; i++ {
+		if !visited[i] {
+			components++
+			dfs(i)
+		}
+	}
+
+	return components
 }
 # @lc code=end
