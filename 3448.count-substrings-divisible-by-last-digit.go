3bdf2e7f8e4c03d63844732897b5eb42e6135e17
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 00:41:17 +0800

    Batch add 1 solutions: 3448.count-substring...

--- 3448.count-substrings-divisible-by-last-digit.go
+++ 3448.count-substrings-divisible-by-last-digit.go
@@ -1,1 +1,32 @@
-C
+#
+# @lc app=leetcode id=3448 lang=golang
+#
+# [3448] Count Substrings Divisible By Last Digit
+#
+
+# @lc code=start
+func countSubstrings(s string) int64 {
+	n := len(s)
+	freq := [10][10]int64{}
+	ans := int64(0)
+	for i := 0; i < n; i++ {
+		dig := int(s[i] - '0')
+		for dd := 1; dd <= 9; dd++ {
+			var newFreq [10]int64
+			for r := 0; r < dd; r++ {
+				contrib := (int64(r) * 10 + int64(dig)) % int64(dd)
+				newFreq[contrib] += freq[dd][r]
+			}
+			smod := int64(dig) % int64(dd)
+			newFreq[smod] += 1
+			if dig == dd {
+				ans += newFreq[0]
+			}
+			for r := 0; r < dd; r++ {
+				freq[dd][r] = newFreq[r]
+			}
+		}
+	}
+	return ans
+}
+# @lc code=end
