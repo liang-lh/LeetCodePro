8f5bdb5ff12cc665a8c1e809598d08bf121d5e65
liang-lh <liang-lh@users.noreply.github.com>
2026-02-10 11:23:55 +0800

    Batch add 1 solutions: 3765.complete-prime-...

---
+++ 3765.complete-prime-number.java
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode id=3765 lang=java
+#
+# [3765] Complete Prime Number
+#
+# @lc code=start
+class Solution {
+    public boolean completePrime(int num) {
+        String numStr = String.valueOf(num);
+        int len = numStr.length();
+        
+        // Check all prefixes
+        for (int i = 1; i <= len; i++) {
+            String prefix = numStr.substring(0, i);
+            int prefixNum = Integer.parseInt(prefix);
+            if (!isPrime(prefixNum)) {
+                return false;
+            }
+        }
+        
+        // Check all suffixes
+        for (int i = 0; i < len; i++) {
+            String suffix = numStr.substring(i);
+            int suffixNum = Integer.parseInt(suffix);
+            if (!isPrime(suffixNum)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+# @lc code=end
