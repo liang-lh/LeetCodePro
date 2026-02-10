12303881f9aa0b71ff9f78eb5204c37c87955988
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 00:52:38 +0800

    Batch add 1 solutions: 3457.eat-pizzas.go...

--- 3457.eat-pizzas.go
+++ 3457.eat-pizzas.go
@@ -3,35 +3,26 @@
 #
 # [3457] Eat Pizzas!
 #
+
 # @lc code=start
 import "sort"
 
 func maxWeight(pizzas []int) int64 {
-    n := len(pizzas)
-    totalDays := n / 4
-    oddDays := (totalDays + 1) / 2
-    evenDays := totalDays / 2
-    
-    // Sort in descending order
-    sort.Slice(pizzas, func(i, j int) bool {
-        return pizzas[i] > pizzas[j]
-    })
-    
-    var result int64
-    
-    // Sum first oddDays pizzas (Z on odd days)
-    for i := 0; i < oddDays; i++ {
-        result += int64(pizzas[i])
-    }
-    
-    // Sum pizzas that become Y on even days
-    // These are at indices [oddDays+evenDays, oddDays+2*evenDays)
-    start := oddDays + evenDays
-    end := oddDays + 2*evenDays
-    for i := start; i < end && i < n; i++ {
-        result += int64(pizzas[i])
-    }
-    
-    return result
+	sort.Slice(pizzas, func(i, j int) bool {
+		return pizzas[i] > pizzas[j]
+	})
+	k := len(pizzas) / 4
+	numOdd := (k + 1) / 2
+	numEven := k / 2
+	ans := int64(0)
+	for i := 0; i < numOdd; i++ {
+		ans += int64(pizzas[i])
+	}
+	pos := numOdd
+	for i := 0; i < numEven; i++ {
+		ans += int64(pizzas[pos + 1])
+		pos += 2
+	}
+	return ans
 }
 # @lc code=end
