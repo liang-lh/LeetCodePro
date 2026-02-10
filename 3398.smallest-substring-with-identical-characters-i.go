c1e3ba7b6e142cae5d24b0ee355c53ba75190aa1
liang-lh <liang-lh@users.noreply.github.com>
2026-02-10 23:37:57 +0800

    Batch add 1 solutions: 3398.smallest-substr...

--- 3398.smallest-substring-with-identical-characters-i.go
+++ 3398.smallest-substring-with-identical-characters-i.go
@@ -5,65 +5,33 @@
 #
 # @lc code=start
 func minLength(s string, numOps int) int {
-    n := len(s)
-    
-    // Check if we can create an alternating pattern with available operations
-    flips1 := 0  // flips needed for pattern 010101...
-    flips2 := 0  // flips needed for pattern 101010...
-    for i := 0; i < n; i++ {
-        if i % 2 == 0 {
-            if s[i] == '1' {
-                flips1++
-            } else {
-                flips2++
-            }
-        } else {
-            if s[i] == '0' {
-                flips1++
-            } else {
-                flips2++
-            }
-        }
-    }
-    
-    if flips1 <= numOps || flips2 <= numOps {
-        return 1
-    }
-    
-    // Binary search for the minimum achievable max length
-    left, right := 2, n
-    for left < right {
-        mid := (left + right) / 2
-        if canAchieve(s, mid, numOps) {
-            right = mid
-        } else {
-            left = mid + 1
-        }
-    }
-    return left
-}
+	n := len(s)
+	runs := []int{}
+	i := 0
+	for i < n {
+		j := i
+		for j < n && s[j] == s[i] {
+			j++
+		}
+		runs = append(runs, j - i)
+		i = j
+	}
 
-func canAchieve(s string, maxLen int, numOps int) bool {
-    flipsNeeded := 0
-    i := 0
-    
-    // Process each run of identical characters
-    for i < len(s) {
-        j := i
-        // Find the end of the current run
-        for j < len(s) && s[j] == s[i] {
-            j++
-        }
-        runLen := j - i
-        
-        // Calculate flips needed to break this run into segments of at most maxLen
-        if runLen > maxLen {
-            flipsNeeded += runLen / (maxLen + 1)
-        }
-        
-        i = j
-    }
-    
-    return flipsNeeded <= numOps
+	lo := 1
+	hi := n
+	for lo < hi {
+		mid := lo + (hi - lo)/2
+		need := 0
+		for _, length := range runs {
+			need += length / (mid + 1)
+		}
+		if need <= numOps {
+			hi = mid
+		} else {
+			lo = mid + 1
+		}
+	}
+
+	return lo
 }
 # @lc code=end
