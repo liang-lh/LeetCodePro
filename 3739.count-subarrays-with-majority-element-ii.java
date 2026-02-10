f0ab835b2fe35f6e57f418b22ebd7fe10819fa3c
liang-lh <liang-lh@users.noreply.github.com>
2026-02-10 15:19:49 +0800

    Batch add 1 solutions: 3739.count-subarrays...

--- 3739.count-subarrays-with-majority-element-ii.java
+++ 3739.count-subarrays-with-majority-element-ii.java
@@ -3,35 +3,26 @@
 #
 # [3739] Count Subarrays With Majority Element II
 #
-
 # @lc code=start
 class Solution {
     public long countMajoritySubarrays(int[] nums, int target) {
+        long result = 0;
         int n = nums.length;
-        long count = 0;
         
-        // Use TreeMap to maintain sorted prefix sums
-        TreeMap<Integer, Long> prefixCount = new TreeMap<>();
-        prefixCount.put(0, 1L); // Empty prefix has sum 0
-        
-        int prefixSum = 0;
         for (int i = 0; i < n; i++) {
-            // Transform: target -> +1, others -> -1
-            prefixSum += (nums[i] == target) ? 1 : -1;
-            
-            // For subarray [j, i] to have target as majority:
-            // sum(j, i) > 0, which means prefixSum[i] > prefixSum[j-1]
-            // Count all prefixes with sum < current prefixSum
-            Map<Integer, Long> lessThan = prefixCount.headMap(prefixSum);
-            for (Long val : lessThan.values()) {
-                count += val;
+            int count = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] == target) {
+                    count++;
+                }
+                int length = j - i + 1;
+                if (2 * count > length) {
+                    result++;
+                }
             }
-            
-            // Add current prefix sum
-            prefixCount.put(prefixSum, prefixCount.getOrDefault(prefixSum, 0L) + 1);
         }
         
-        return count;
+        return result;
     }
 }
 # @lc code=end
