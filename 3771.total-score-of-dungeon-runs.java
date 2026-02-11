8b27399637ab4898e7bc6de766cb0332db3129da
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 20:32:48 +0800

    Batch add 1 solutions: 3771.total-score-of-...

--- 3771.total-score-of-dungeon-runs.java
+++ 3771.total-score-of-dungeon-runs.java
@@ -3,54 +3,34 @@
 #
 # [3771] Total Score of Dungeon Runs
 #
+
 # @lc code=start
 class Solution {
     public long totalScore(int hp, int[] damage, int[] requirement) {
         int n = damage.length;
         long[] prefix = new long[n + 1];
-        
         for (int i = 0; i < n; i++) {
             prefix[i + 1] = prefix[i] + damage[i];
         }
-        
-        long totalScore = 0;
-        java.util.List<Long> sortedPrefixes = new java.util.ArrayList<>();
-        sortedPrefixes.add(prefix[0]);
-        
-        for (int i = 0; i < n; i++) {
-            long threshold = requirement[i] + prefix[i + 1] - hp;
-            int count = countGreaterOrEqual(sortedPrefixes, threshold);
-            totalScore += count;
-            insertSorted(sortedPrefixes, prefix[i + 1]);
-        }
-        
-        return totalScore;
-    }
-    
-    private int countGreaterOrEqual(java.util.List<Long> list, long threshold) {
-        int left = 0, right = list.size();
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (list.get(mid) >= threshold) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        long ans = 0;
+        for (int k = 0; k < n; k++) {
+            long thresh = prefix[k + 1] + (long) requirement[k] - hp;
+            int left = 0, right = k;
+            int jmin = k + 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (prefix[mid] >= thresh) {
+                    jmin = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            if (jmin <= k) {
+                ans += (k - jmin + 1L);
             }
         }
-        return list.size() - left;
-    }
-    
-    private void insertSorted(java.util.List<Long> list, long value) {
-        int left = 0, right = list.size();
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (list.get(mid) < value) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        list.add(left, value);
+        return ans;
     }
 }
 # @lc code=end
