5c32525094a2514472f75d00130ed4c09752345d
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 02:22:10 +0800

    Batch add 1 solutions: 3640.trionic-array-i...

--- 3640.trionic-array-ii.java
+++ 3640.trionic-array-ii.java
@@ -9,43 +9,47 @@ class Solution {
     public long maxSumTrionic(int[] nums) {
         int n = nums.length;
         long[] prefix = new long[n + 1];
-        for (int i = 1; i <= n; ++i) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
         }
-        long INF = -1000000000000000000L;
-        long[] left = new long[n];
-        long[] rightDp = new long[n];
-        for (int i = 0; i < n; ++i) {
-            left[i] = INF;
-            rightDp[i] = INF;
-        }
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > nums[i - 1]) {
-                left[i] = (long) nums[i] + Math.max((long) nums[i - 1], left[i - 1]);
+        long[] ldp = new long[n];
+        java.util.Arrays.fill(ldp, Long.MIN_VALUE / 2);
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] < nums[i]) {
+                long prev = Math.max((long) nums[i - 1], ldp[i - 1]);
+                ldp[i] = (long) nums[i] + prev;
             }
         }
-        for (int i = n - 2; i >= 0; --i) {
+        long[] rdp = new long[n];
+        java.util.Arrays.fill(rdp, Long.MIN_VALUE / 2);
+        for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                rightDp[i] = (long) nums[i] + Math.max((long) nums[i + 1], rightDp[i + 1]);
+                long nextv = Math.max((long) nums[i + 1], rdp[i + 1]);
+                rdp[i] = (long) nums[i] + nextv;
             }
         }
-        long[] val = new long[n];
-        for (int i = 0; i < n; ++i) {
-            val[i] = left[i] == INF ? INF : left[i] - prefix[i + 1];
+        long[] Aval = new long[n];
+        for (int i = 0; i < n; i++) {
+            Aval[i] = ldp[i] - prefix[i + 1];
         }
-        long ans = INF;
-        long curMax = INF;
-        for (int q = 1; q < n; ++q) {
-            if (nums[q - 1] > nums[q]) {
-                curMax = Math.max(curMax, val[q - 1]);
-                if (curMax != INF && rightDp[q] != INF) {
-                    long cand = curMax + prefix[q + 1] + rightDp[q] - nums[q];
-                    if (cand > ans) {
-                        ans = cand;
-                    }
-                }
-            } else {
-                curMax = INF;
+        long[] Bval = new long[n];
+        for (int i = 0; i < n; i++) {
+            Bval[i] = prefix[i] + rdp[i];
+        }
+        long ans = Long.MIN_VALUE / 2;
+        int dec_start = 0;
+        long maxA = Long.MIN_VALUE / 2;
+        for (int q = 1; q < n; q++) {
+            if (nums[q - 1] <= nums[q]) {
+                dec_start = q;
+                maxA = Long.MIN_VALUE / 2;
+            }
+            if (q - 1 >= dec_start) {
+                maxA = Math.max(maxA, Aval[q - 1]);
+            }
+            if (dec_start <= q - 1) {
+                long cand = maxA + Bval[q];
+                if (cand > ans) ans = cand;
             }
         }
         return ans;
