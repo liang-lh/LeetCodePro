a0604f6f378c9ad86f55c9f07b5dbaefd2dff880
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 00:38:22 +0800

    Batch add 1 solutions: 3538.merge-operation...

--- 3538.merge-operations-for-minimum-travel-time.java
+++ 3538.merge-operations-for-minimum-travel-time.java
@@ -8,49 +8,42 @@
 class Solution {
     public int minTravelTime(int l, int n, int k, int[] position, int[] time) {
         final int INF = 2000000000;
-        final int MAXR = 101;
-        int[] prefix = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + time[i];
-        }
-        int[][][] dp = new int[n][k + 1][MAXR];
+        int[][][] dp = new int[n][k + 1][101];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= k; j++) {
-                for (int r = 0; r < MAXR; r++) {
-                    dp[i][j][r] = INF;
+                for (int t = 0; t <= 100; t++) {
+                    dp[i][j][t] = INF;
                 }
             }
         }
-        if (n > 0 && time[0] < MAXR) {
-            dp[0][0][time[0]] = 0;
+        dp[0][0][time[0]] = 0;
+        long[] presum = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            presum[i + 1] = presum[i] + time[i];
         }
-        for (int p = 0; p < n; p++) {
-            for (int j = 0; j <= k; j++) {
-                for (int r = 0; r < MAXR; r++) {
-                    if (dp[p][j][r] == INF) continue;
-                    for (int i = p + 1; i < n; i++) {
-                        int merges_add = i - p - 1;
-                        if (j + merges_add > k) break;
-                        long dist = (long) position[i] - position[p];
-                        long added_cost = dist * r;
-                        long temp = (long) dp[p][j][r] + added_cost;
-                        if (temp >= INF) continue;
-                        int new_cost = (int) temp;
-                        int new_r = prefix[i + 1] - prefix[p + 1];
-                        if (new_r < 0 || new_r >= MAXR) continue;
-                        int nj = j + merges_add;
-                        if (new_cost < dp[i][nj][new_r]) {
-                            dp[i][nj][new_r] = new_cost;
+        for (int i = 1; i < n; i++) {
+            for (int p = 0; p < i; p++) {
+                int merges_add = i - p - 1;
+                if (merges_add < 0) continue;
+                for (int jprev = 0; jprev <= k - merges_add; jprev++) {
+                    for (int s = 0; s <= 100; s++) {
+                        if (dp[p][jprev][s] == INF) continue;
+                        long sumadd = presum[i] - presum[p + 1];
+                        int effnew = time[i] + (int) sumadd;
+                        if (effnew > 100) continue;
+                        long addcost = (long) (position[i] - position[p]) * s;
+                        long newcost = (long) dp[p][jprev][s] + addcost;
+                        int jj = jprev + merges_add;
+                        if (newcost < dp[i][jj][effnew]) {
+                            dp[i][jj][effnew] = (int) newcost;
                         }
                     }
                 }
             }
         }
         int ans = INF;
-        for (int r = 0; r < MAXR; r++) {
-            if (dp[n - 1][k][r] < ans) {
-                ans = dp[n - 1][k][r];
-            }
+        for (int t = 0; t <= 100; t++) {
+            ans = Math.min(ans, dp[n - 1][k][t]);
         }
         return ans;
     }
