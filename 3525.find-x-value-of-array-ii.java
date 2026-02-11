982d534b4adab541a183ae69997c298b97baaece
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 17:00:02 +0800

    Batch add 1 solutions: 3525.find-x-value-of...

--- 3525.find-x-value-of-array-ii.java
+++ 3525.find-x-value-of-array-ii.java
@@ -3,37 +3,116 @@
 #
 # [3525] Find X Value of Array II
 #
+
 # @lc code=start
 class Solution {
     public int[] resultArray(int[] nums, int k, int[][] queries) {
+        final int B = 400;
+        final int MAXK = 6;
         int n = nums.length;
-        int q = queries.length;
-        int[] result = new int[q];
-        
-        for (int qi = 0; qi < q; qi++) {
-            int index = queries[qi][0];
-            int value = queries[qi][1];
-            int start = queries[qi][2];
-            int x = queries[qi][3];
-            
-            // Update nums (persists for future queries)
-            nums[index] = value;
-            
-            // Count the number of ways to remove suffix such that product % k == x
-            int count = 0;
-            int product = 1;
-            
-            for (int i = start; i < n; i++) {
-                product = (product * (nums[i] % k)) % k;
-                if (product == x) {
-                    count++;
+        int num_blocks = (n + B - 1) / B;
+        int[][][] blockCounts = new int[num_blocks][MAXK][MAXK];
+        int[] blockTotalProd = new int[num_blocks];
+        int[] arr = nums.clone();
+
+        // Initial build
+        for (int b = 0; b < num_blocks; b++) {
+            for (int em = 0; em < k; em++) {
+                for (int tm = 0; tm < k; tm++) {
+                    blockCounts[b][em][tm] = 0;
                 }
             }
-            
-            result[qi] = count;
+            int left = b * B;
+            int right = Math.min(n - 1, left + B - 1);
+            int sz = right - left + 1;
+            if (sz <= 0) continue;
+            long[] prefix = new long[sz + 1];
+            prefix[1] = arr[left] % k;
+            for (int t = 2; t <= sz; t++) {
+                long mul = arr[left + t - 1] % k;
+                prefix[t] = (prefix[t - 1] * mul) % k;
+            }
+            for (int t = 1; t <= sz; t++) {
+                long pre = prefix[t];
+                for (int enter = 0; enter < k; enter++) {
+                    int fullm = (int) (((long) enter * pre) % k);
+                    blockCounts[b][enter][fullm]++;
+                }
+            }
+            blockTotalProd[b] = (int) prefix[sz];
         }
-        
-        return result;
+
+        int qlen = queries.length;
+        int[] ans = new int[qlen];
+        for (int qi = 0; qi < qlen; qi++) {
+            int[] qu = queries[qi];
+            int idx = qu[0];
+            int val = qu[1];
+            int start = qu[2];
+            int x = qu[3];
+            arr[idx] = val;
+            // Rebuild affected block
+            int ub = idx / B;
+            {
+                int b = ub;
+                for (int em = 0; em < k; em++) {
+                    for (int tm = 0; tm < k; tm++) {
+                        blockCounts[b][em][tm] = 0;
+                    }
+                }
+                int left = b * B;
+                int right = Math.min(n - 1, left + B - 1);
+                int sz = right - left + 1;
+                if (sz > 0) {
+                    long[] prefix = new long[sz + 1];
+                    prefix[1] = arr[left] % k;
+                    for (int t = 2; t <= sz; t++) {
+                        long mul = arr[left + t - 1] % k;
+                        prefix[t] = (prefix[t - 1] * mul) % k;
+                    }
+                    for (int t = 1; t <= sz; t++) {
+                        long pre = prefix[t];
+                        for (int enter = 0; enter < k; enter++) {
+                            int fullm = (int) (((long) enter * pre) % k);
+                            blockCounts[b][enter][fullm]++;
+                        }
+                    }
+                    blockTotalProd[b] = (int) prefix[sz];
+                }
+            }
+            // Compute answer for suffix starting at 'start'
+            int res = 0;
+            long cur_mod = 1L % k;
+            int pos = start;
+            int cb = pos / B;
+            int blew = cb * B;
+            int bright = Math.min(n - 1, blew + B - 1);
+            // First partial block
+            for (int p = pos; p <= bright && p < n; p++) {
+                long mul = arr[p] % k;
+                cur_mod = (cur_mod * mul) % k;
+                if ((int) cur_mod == x) res++;
+            }
+            if (bright < n - 1) {
+                int lb = (n - 1) / B;
+                // Full blocks: cb+1 to lb-1
+                for (int bb = cb + 1; bb < lb; bb++) {
+                    res += blockCounts[bb][(int) cur_mod][x];
+                    cur_mod = (cur_mod * (long) blockTotalProd[bb]) % k;
+                }
+                // Last partial block
+                if (lb > cb) {
+                    int lleft = lb * B;
+                    for (int p = lleft; p < n; p++) {
+                        long mul = arr[p] % k;
+                        cur_mod = (cur_mod * mul) % k;
+                        if ((int) cur_mod == x) res++;
+                    }
+                }
+            }
+            ans[qi] = res;
+        }
+        return ans;
     }
 }
 # @lc code=end
