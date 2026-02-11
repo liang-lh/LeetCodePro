b2c2786fec13efd239bbd81d47d25965c721b389
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 02:34:00 +0800

    Batch add 1 solutions: 3646.next-special-pa...

--- 3646.next-special-palindrome-number.java
+++ 3646.next-special-palindrome-number.java
@@ -1,84 +1,82 @@
-import java.util.*;
 #
 # @lc app=leetcode id=3646 lang=java
 #
 # [3646] Next Special Palindrome Number
 #
+
 # @lc code=start
 class Solution {
-    private static final String MAX_LONG_STR = "9223372036854775807";
-
     public long specialPalindrome(long n) {
-        List<Long> specials = new ArrayList<>();
-        for (int mask = 1; mask < (1 << 9); mask++) {
+        java.util.TreeSet<java.lang.Long> specials = new java.util.TreeSet<>();
+        int[] evens = {2, 4, 6, 8};
+        // Even subsets
+        for (int mask = 0; mask < 16; mask++) {
+            int[] freq = new int[10];
             int L = 0;
-            int[] req = new int[10];
-            for (int k = 1; k <= 9; k++) {
-                if ((mask & (1 << (k - 1))) != 0) {
-                    L += k;
-                    req[k] = k;
+            for (int b = 0; b < 4; b++) {
+                if ((mask & (1 << b)) != 0) {
+                    int e = evens[b];
+                    freq[e] = e;
+                    L += e;
                 }
             }
-            if (L == 0 || L > 19) continue;
-            int num_odd = 0;
-            int odd_d = -1;
-            for (int d = 1; d <= 9; d++) {
-                if (req[d] % 2 == 1) {
-                    num_odd++;
-                    odd_d = d;
+            if (L == 0 || L > 20) continue;
+            int half = L / 2;
+            int[] rem = freq.clone();
+            java.lang.StringBuilder prefix = new java.lang.StringBuilder();
+            generate(specials, rem, prefix, half, L);
+        }
+        // Odd-inclusive subsets
+        int[] odds = {1, 3, 5, 7, 9};
+        for (int i = 0; i < 5; i++) {
+            int od = odds[i];
+            for (int mask = 0; mask < 16; mask++) {
+                int[] freq = new int[10];
+                freq[od] = od;
+                int L = od;
+                for (int b = 0; b < 4; b++) {
+                    if ((mask & (1 << b)) != 0) {
+                        int e = evens[b];
+                        freq[e] = e;
+                        L += e;
+                    }
                 }
-            }
-            boolean is_odd_len = (L % 2 == 1);
-            if ((is_odd_len && num_odd != 1) || (!is_odd_len && num_odd != 0)) continue;
-            int[] half_cnt = new int[10];
-            for (int d = 1; d <= 9; d++) {
-                half_cnt[d] = req[d] / 2;
-            }
-            int left_len = L / 2;
-            char[] path = new char[left_len];
-            int[] remain = new int[10];
-            for (int d = 1; d <= 9; d++) remain[d] = half_cnt[d];
-            generate(0, path, remain, specials, L, odd_d, left_len);
-        }
-        Collections.sort(specials);
-        for (long x : specials) {
-            if (x > n) {
-                return x;
+                if (L > 20) continue;
+                int half = L / 2;
+                int[] rem = freq.clone();
+                java.lang.StringBuilder prefix = new java.lang.StringBuilder();
+                generate(specials, rem, prefix, half, L);
             }
         }
-        return -1L; // Theoretically unreachable
+        return specials.higher(n);
     }
 
-    private void generate(int pos, char[] path, int[] remain, List<Long> specials, int L, int odd_d, int left_len) {
-        if (pos == left_len) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < left_len; i++) {
-                sb.append(path[i]);
+    private void generate(java.util.TreeSet<java.lang.Long> specials, int[] rem, java.lang.StringBuilder prefix, int half, int L) {
+        int targetLen = half + (L % 2);
+        if (prefix.length() == targetLen) {
+            java.lang.String leftStr = prefix.toString();
+            java.lang.StringBuilder revBuilder = new java.lang.StringBuilder(leftStr.substring(0, half));
+            revBuilder.reverse();
+            java.lang.String fullStr = leftStr + revBuilder.toString();
+            if (fullStr.length() <= 18) {
+                try {
+                    long num = java.lang.Long.parseLong(fullStr);
+                    specials.add(num);
+                } catch (java.lang.NumberFormatException e) {
+                    // ignore too large
+                }
             }
-            String left = sb.toString();
-            if (L % 2 == 1) {
-                sb.append((char) ('0' + odd_d));
-            }
-            sb.append(new StringBuilder(left).reverse().toString());
-            String numStr = sb.toString();
-            if (numStr.length() == 19 && numStr.compareTo(MAX_LONG_STR) > 0) {
-                return;
-            }
-            long num;
-            try {
-                num = Long.parseLong(numStr);
-            } catch (NumberFormatException e) {
-                return;
-            }
-            specials.add(num);
             return;
         }
+        int pos = prefix.length();
+        int req = ((L % 2 == 1) && (pos == half)) ? 1 : 2;
         for (int d = 1; d <= 9; d++) {
-            if (remain[d] > 0) {
-                path[pos] = (char) ('0' + d);
-                remain[d]--;
-                generate(pos + 1, path, remain, specials, L, odd_d, left_len);
-                remain[d]++;
+            if (rem[d] >= req) {
+                rem[d] -= req;
+                prefix.append((char) ('0' + d));
+                generate(specials, rem, prefix, half, L);
+                prefix.deleteCharAt(prefix.length() - 1);
+                rem[d] += req;
             }
         }
     }
