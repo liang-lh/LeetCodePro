856f9aabc2422efac4195afb802bcbd3ff76efc5
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 20:48:57 +0800

    Batch add 1 solutions: 3786.total-sum-of-in...

--- 3786.total-sum-of-interaction-cost-in-tree-groups.java
+++ 3786.total-sum-of-interaction-cost-in-tree-groups.java
@@ -3,67 +3,44 @@
 #
 # [3786] Total Sum of Interaction Cost in Tree Groups
 #
-# @lc code=start
-import java.util.*;
 
+# @lc code=start
 class Solution {
-    private List<List<Integer>> adj;
-    private int[] group;
-    private Map<Integer, Integer> totalCount;
-    private long totalCost;
-    
     public long interactionCosts(int n, int[][] edges, int[] group) {
-        this.group = group;
-        this.totalCost = 0;
-        
-        // Build adjacency list
-        adj = new ArrayList<>();
+        @SuppressWarnings("unchecked")
+        List<Integer>[] adj = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+            adj[i] = new ArrayList<>();
         }
         for (int[] edge : edges) {
-            adj.get(edge[0]).add(edge[1]);
-            adj.get(edge[1]).add(edge[0]);
+            int u = edge[0], v = edge[1];
+            adj[u].add(v);
+            adj[v].add(u);
         }
-        
-        // Count total nodes per group
-        totalCount = new HashMap<>();
-        for (int g : group) {
-            totalCount.put(g, totalCount.getOrDefault(g, 0) + 1);
+        int[] gsize = new int[21];
+        for (int i = 0; i < n; i++) {
+            gsize[group[i]]++;
         }
-        
-        // DFS from root 0
-        dfs(0, -1);
-        
-        return totalCost;
-    }
-    
-    private Map<Integer, Integer> dfs(int node, int parent) {
-        // Count of each group in the subtree rooted at node
-        Map<Integer, Integer> subtreeCount = new HashMap<>();
-        subtreeCount.put(group[node], 1);
-        
-        for (int child : adj.get(node)) {
-            if (child == parent) continue;
-            
-            Map<Integer, Integer> childCount = dfs(child, node);
-            
-            // The edge (node, child) separates the tree
-            // Calculate contribution of this edge
-            for (int g : totalCount.keySet()) {
-                int countInChild = childCount.getOrDefault(g, 0);
-                int countOutside = totalCount.get(g) - countInChild;
-                totalCost += (long) countInChild * countOutside;
-            }
-            
-            // Merge childCount into subtreeCount
-            for (Map.Entry<Integer, Integer> entry : childCount.entrySet()) {
-                subtreeCount.put(entry.getKey(), 
-                    subtreeCount.getOrDefault(entry.getKey(), 0) + entry.getValue());
+        long[] totalAns = new long[1];
+        class DfsHelper {
+            int dfs(int node, int par, int g, int[] grp, List<Integer>[] ad, int[] gs, long[] tans) {
+                int cnt = (grp[node] == g ? 1 : 0);
+                for (int nei : ad[node]) {
+                    if (nei != par) {
+                        int sub = dfs(nei, node, g, grp, ad, gs, tans);
+                        tans[0] += (long) sub * (gs[g] - sub);
+                        cnt += sub;
+                    }
+                }
+                return cnt;
             }
         }
-        
-        return subtreeCount;
+        DfsHelper helper = new DfsHelper();
+        for (int g = 1; g <= 20; g++) {
+            if (gsize[g] < 2) continue;
+            helper.dfs(0, -1, g, group, adj, gsize, totalAns);
+        }
+        return totalAns[0];
     }
 }
 # @lc code=end
