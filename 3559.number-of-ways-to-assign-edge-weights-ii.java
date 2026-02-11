cfda844c1c6f38ee72df777a3ed6c4e28a4b89f8
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 01:05:37 +0800

    Batch add 1 solutions: 3559.number-of-ways-...

--- 3559.number-of-ways-to-assign-edge-weights-ii.java
+++ 3559.number-of-ways-to-assign-edge-weights-ii.java
@@ -5,47 +5,43 @@
 #
 
 # @lc code=start
+import java.util.*;
 class Solution {
+    private static final long MOD = 1000000007L;
+    private static final int LOG = 18;
+
     public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
         int n = edges.length + 1;
-        @SuppressWarnings("unchecked")
-        List<Integer>[] adj = new ArrayList[n + 1];
+        List<List<Integer>> adj = new ArrayList<>(n + 1);
         for (int i = 0; i <= n; i++) {
-            adj[i] = new ArrayList<>();
+            adj.add(new ArrayList<>());
         }
         for (int[] e : edges) {
             int u = e[0], v = e[1];
-            adj[u].add(v);
-            adj[v].add(u);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        final long MOD = 1000000007L;
-        final int LOG = 18;
-        int[][] par = new int[LOG][n + 1];
-        int[] dep = new int[n + 1];
-        boolean[] vis = new boolean[n + 1];
+        int[][] up = new int[LOG][n + 1];
+        int[] depth = new int[n + 1];
+        Arrays.fill(depth, -1);
         Queue<Integer> q = new LinkedList<>();
+        depth[1] = 0;
+        up[0][1] = 1;
         q.offer(1);
-        vis[1] = true;
-        dep[1] = 0;
-        par[0][1] = -1;
         while (!q.isEmpty()) {
             int u = q.poll();
-            for (int v : adj[u]) {
-                if (!vis[v]) {
-                    vis[v] = true;
-                    dep[v] = dep[u] + 1;
-                    par[0][v] = u;
+            for (int v : adj.get(u)) {
+                if (depth[v] == -1) {
+                    depth[v] = depth[u] + 1;
+                    up[0][v] = u;
                     q.offer(v);
                 }
             }
         }
-        for (int k = 1; k < LOG; k++) {
+        for (int j = 1; j < LOG; j++) {
             for (int i = 1; i <= n; i++) {
-                if (par[k - 1][i] != -1) {
-                    par[k][i] = par[k - 1][par[k - 1][i]];
-                } else {
-                    par[k][i] = -1;
-                }
+                int p = up[j - 1][i];
+                up[j][i] = up[j - 1][p];
             }
         }
         int[] ans = new int[queries.length];
@@ -56,47 +52,46 @@ class Solution {
                 ans[i] = 0;
                 continue;
             }
-            int uu = u;
-            int vv = v;
-            if (dep[uu] > dep[vv]) {
-                int temp = uu;
-                uu = vv;
-                vv = temp;
-            }
-            int diff = dep[vv] - dep[uu];
-            for (int k = 0; k < LOG; k++) {
-                if ((diff & (1 << k)) != 0) {
-                    vv = par[k][vv];
-                }
-            }
-            if (uu == vv) {
-                int l = uu;
-                int dist = dep[u] + dep[v] - 2 * dep[l];
-                ans[i] = (int) modpow(2L, (long)dist - 1L, MOD);
-                continue;
-            }
-            for (int k = LOG - 1; k >= 0; k--) {
-                if (par[k][uu] != par[k][vv]) {
-                    uu = par[k][uu];
-                    vv = par[k][vv];
-                }
-            }
-            int l = par[0][uu];
-            int dist = dep[u] + dep[v] - 2 * dep[l];
-            ans[i] = (int) modpow(2L, (long)dist - 1L, MOD);
+            int l = getLCA(u, v, depth, up);
+            long k = (long) depth[u] + depth[v] - 2L * depth[l];
+            ans[i] = (int) modPow(2L, k - 1, MOD);
         }
         return ans;
     }
 
-    private static long modpow(long b, long e, long mod) {
-        long res = 1;
-        b %= mod;
-        while (e > 0) {
-            if ((e & 1) == 1) {
-                res = res * b % mod;
+    private int getLCA(int u, int v, int[] depth, int[][] up) {
+        if (depth[u] > depth[v]) {
+            int temp = u;
+            u = v;
+            v = temp;
+        }
+        int diff = depth[v] - depth[u];
+        for (int j = 0; j < LOG; j++) {
+            if ((diff & (1 << j)) != 0) {
+                v = up[j][v];
             }
-            b = b * b % mod;
-            e >>= 1;
+        }
+        if (u == v) {
+            return u;
+        }
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (up[j][u] != up[j][v]) {
+                u = up[j][u];
+                v = up[j][v];
+            }
+        }
+        return up[0][u];
+    }
+
+    private long modPow(long base, long exp, long mod) {
+        long res = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                res = res * base % mod;
+            }
+            base = base * base % mod;
+            exp >>= 1;
         }
         return res;
     }
