55a472061a27acb1b17571e60bf619ab39a39f81
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 01:50:29 +0800

    Batch add 1 solutions: 3594.minimum-time-to...

--- 3594.minimum-time-to-transport-all-individuals.java
+++ 3594.minimum-time-to-transport-all-individuals.java
@@ -7,90 +7,79 @@
 # @lc code=start
 class Solution {
     private static class State implements Comparable<State> {
-        double time;
-        int mask, stage, boat;
-        State(double time, int mask, int stage, int boat) {
-            this.time = time;
+        double t;
+        int mask, stage;
+        State(double t, int mask, int stage) {
+            this.t = t;
             this.mask = mask;
             this.stage = stage;
-            this.boat = boat;
         }
         @Override
-        public int compareTo(State other) {
-            return Double.compare(this.time, other.time);
+        public int compareTo(State o) {
+            return Double.compare(this.t, o.t);
         }
     }
 
     public double minTime(int n, int k, int m, int[] time, double[] mul) {
-        final double INF = 1e100;
-        int maxmask = 1 << n;
-        double[] max_time = new double[maxmask];
-        for (int mk = 0; mk < maxmask; ++mk) {
-            double mx = 0.0;
-            for (int i = 0; i < n; ++i) {
-                if ((mk & (1 << i)) != 0) {
-                    mx = Math.max(mx, (double)time[i]);
-                }
-            }
-            max_time[mk] = mx;
+        double INF = Double.POSITIVE_INFINITY;
+        int N = 1 << n;
+        double[][] dp = new double[N][m];
+        for (int i = 0; i < N; i++) {
+            java.util.Arrays.fill(dp[i], INF);
         }
-        double[][][] dist = new double[maxmask][m][2];
-        for (int i = 0; i < maxmask; ++i) {
-            for (int j = 0; j < m; ++j) {
-                dist[i][j][0] = INF;
-                dist[i][j][1] = INF;
-            }
-        }
+        dp[0][0] = 0.0;
         java.util.PriorityQueue<State> pq = new java.util.PriorityQueue<>();
-        int full = (1 << n) - 1;
-        dist[full][0][0] = 0.0;
-        pq.offer(new State(0.0, full, 0, 0));
-        while (!pq.isEmpty()) {
-            State curr = pq.poll();
-            if (curr.time > dist[curr.mask][curr.stage][curr.boat]) {
-                continue;
-            }
-            int mask = curr.mask;
-            int stage = curr.stage;
-            int boat = curr.boat;
-            double ctime = curr.time;
-            if (boat == 0) {
-                for (int sub = mask; sub > 0; sub = (sub - 1) & mask) {
-                    if (Integer.bitCount(sub) > k) {
-                        continue;
-                    }
-                    double maxt = max_time[sub];
-                    double cost = maxt * mul[stage];
-                    int adv = (int) Math.floor(cost) % m;
-                    int nstage = (stage + adv) % m;
-                    int nmask = mask ^ sub;
-                    double ntime = ctime + cost;
-                    if (ntime < dist[nmask][nstage][1]) {
-                        dist[nmask][nstage][1] = ntime;
-                        pq.offer(new State(ntime, nmask, nstage, 1));
-                    }
-                }
-            } else {
-                for (int r = 0; r < n; ++r) {
-                    if ((mask & (1 << r)) == 0) {
-                        double cost = (double) time[r] * mul[stage];
-                        int adv = (int) Math.floor(cost) % m;
-                        int nstage = (stage + adv) % m;
-                        int nmask = mask | (1 << r);
-                        double ntime = ctime + cost;
-                        if (ntime < dist[nmask][nstage][0]) {
-                            dist[nmask][nstage][0] = ntime;
-                            pq.offer(new State(ntime, nmask, nstage, 0));
-                        }
-                    }
-                }
-            }
-        }
+        pq.offer(new State(0.0, 0, 0));
         double ans = INF;
-        for (int j = 0; j < m; ++j) {
-            ans = Math.min(ans, dist[0][j][1]);
+        int full = (1 << n) - 1;
+        java.util.List<Integer> groups = new java.util.ArrayList<>();
+        int[] gmax = new int[N];
+        for (int mask = 1; mask < N; mask++) {
+            if (Integer.bitCount(mask) > k) continue;
+            int mx = 0;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    mx = Math.max(mx, time[i]);
+                }
+            }
+            groups.add(mask);
+            gmax[mask] = mx;
         }
-        return ans < INF / 2 ? ans : -1.0;
+        while (!pq.isEmpty()) {
+            State cur = pq.poll();
+            double ct = cur.t;
+            int cmask = cur.mask;
+            int cstage = cur.stage;
+            if (ct > dp[cmask][cstage]) continue;
+            int smask = full ^ cmask;
+            for (int sub : groups) {
+                if ((sub & smask) != sub) continue;
+                int maxt = gmax[sub];
+                double d = maxt * mul[cstage];
+                int adv = (int) d % m;
+                int nstage = (cstage + adv) % m;
+                int nmask = cmask | sub;
+                double nt_fwd = ct + d;
+                int cleft = Integer.bitCount(full ^ nmask);
+                if (cleft == 0) {
+                    ans = Math.min(ans, nt_fwd);
+                    continue;
+                }
+                for (int r = 0; r < n; r++) {
+                    if ((nmask & (1 << r)) == 0) continue;
+                    double rt = time[r] * mul[nstage];
+                    int adv2 = (int) rt % m;
+                    int nstage2 = (nstage + adv2) % m;
+                    int nmask2 = nmask ^ (1 << r);
+                    double nt = nt_fwd + rt;
+                    if (nt < dp[nmask2][nstage2]) {
+                        dp[nmask2][nstage2] = nt;
+                        pq.offer(new State(nt, nmask2, nstage2));
+                    }
+                }
+            }
+        }
+        return ans == INF ? -1.0 : ans;
     }
 }
 # @lc code=end
