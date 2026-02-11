6060355f29a2476d38bffaed03e8de220fa51535
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 13:18:51 +0800

    Batch add 1 solutions: 3398.smallest-substr...

--- 3398.smallest-substring-with-identical-characters-i.java
+++ 3398.smallest-substring-with-identical-characters-i.java
@@ -3,83 +3,56 @@
 #
 # [3398] Smallest Substring With Identical Characters I
 #
+
 # @lc code=start
 class Solution {
     public int minLength(String s, int numOps) {
         int n = s.length();
-        
-        if (numOps == 0) {
-            // Can't flip anything, return current max run length
-            return getMaxRunLength(s);
-        }
-        
-        // Special case: check if we can make alternating pattern
-        int flips0 = 0; // flips to make "010101..."
-        int flips1 = 0; // flips to make "101010..."
-        for (int i = 0; i < n; i++) {
-            char expected0 = (i % 2 == 0) ? '0' : '1';
-            char expected1 = (i % 2 == 0) ? '1' : '0';
-            if (s.charAt(i) != expected0) flips0++;
-            if (s.charAt(i) != expected1) flips1++;
-        }
-        
-        if (Math.min(flips0, flips1) <= numOps) {
-            return 1;
-        }
-        
-        // Binary search on the answer
-        int left = 2, right = n;
-        int result = n;
-        
-        while (left <= right) {
+        int left = 1, right = n;
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (canAchieve(s, numOps, mid)) {
-                result = mid;
-                right = mid - 1;
+            if (canAchieve(mid, s, numOps)) {
+                right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        
-        return result;
+        return left;
     }
-    
-    private int getMaxRunLength(String s) {
-        int maxRun = 1;
-        int currentRun = 1;
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                currentRun++;
-                maxRun = Math.max(maxRun, currentRun);
-            } else {
-                currentRun = 1;
-            }
-        }
-        return maxRun;
-    }
-    
-    private boolean canAchieve(String s, int numOps, int maxLen) {
+
+    private boolean canAchieve(int K, String s, int numOps) {
         int n = s.length();
-        int flipsNeeded = 0;
-        int i = 0;
-        
-        while (i < n) {
-            int j = i;
-            // Find the end of current run
-            while (j < n && s.charAt(j) == s.charAt(i)) {
-                j++;
+        int INF = n + 1;
+        int[][][] dp = new int[n + 1][K + 1][2];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= K; j++) {
+                dp[i][j][0] = INF;
+                dp[i][j][1] = INF;
             }
-            
-            int runLen = j - i;
-            if (runLen > maxLen) {
-                // Need to break this run
-                flipsNeeded += runLen / (maxLen + 1);
-            }
-            
-            i = j;
         }
-        
-        return flipsNeeded <= numOps;
+        int orig = s.charAt(0) - '0';
+        dp[1][1][0] = (orig == 0 ? 0 : 1);
+        dp[1][1][1] = (orig == 1 ? 0 : 1);
+        for (int i = 1; i < n; i++) {
+            orig = s.charAt(i) - '0';
+            for (int prevJ = 1; prevJ <= K; prevJ++) {
+                for (int prevC = 0; prevC < 2; prevC++) {
+                    int prevCost = dp[i][prevJ][prevC];
+                    if (prevCost > n) continue;
+                    for (int newC = 0; newC < 2; newC++) {
+                        int cost = (newC == orig ? 0 : 1);
+                        int newJ = (newC == prevC ? prevJ + 1 : 1);
+                        if (newJ > K) continue;
+                        dp[i + 1][newJ][newC] = Math.min(dp[i + 1][newJ][newC], prevCost + cost);
+                    }
+                }
+            }
+        }
+        int minFlips = INF;
+        for (int j = 1; j <= K; j++) {
+            minFlips = Math.min(minFlips, Math.min(dp[n][j][0], dp[n][j][1]));
+        }
+        return minFlips <= numOps;
     }
 }
 # @lc code=end
