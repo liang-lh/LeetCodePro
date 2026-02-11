0de3566dc49be1656e2e3805740823628c1433a0
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 03:08:52 +0800

    Batch add 1 solutions: 3704.count-no-zero-p...

--- 3704.count-no-zero-pairs-that-sum-to-n.java
+++ 3704.count-no-zero-pairs-that-sum-to-n.java
@@ -7,39 +7,67 @@
 # @lc code=start
 class Solution {
     public long countNoZeroPairs(long n) {
-        String s = String.valueOf(n);
-        int L = s.length();
-        long[][][][] dp = new long[L + 1][2][2][3];
-        // Base case
-        for (int sa = 0; sa < 2; sa++) {
-            for (int sb = 0; sb < 2; sb++) {
-                dp[L][sa][sb][0] = (sa == 1 && sb == 1) ? 1L : 0L;
-            }
+        String s = Long.toString(n);
+        int len = s.length();
+        int[] digits = new int[len];
+        for (int i = 0; i < len; ++i) {
+            digits[i] = s.charAt(len - 1 - i) - '0';
         }
-        // Fill DP from pos = L-1 to 0
-        for (int p = L - 1; p >= 0; p--) {
-            int dig = s.charAt(p) - '0';
-            for (int sa = 0; sa < 2; sa++) {
-                for (int sb = 0; sb < 2; sb++) {
-                    for (int da = (sa == 0 ? 0 : 1); da <= 9; da++) {
-                        int nsa = sa | (da > 0 ? 1 : 0);
-                        for (int db = (sb == 0 ? 0 : 1); db <= 9; db++) {
-                            int nsb = sb | (db > 0 ? 1 : 0);
-                            for (int cin = 0; cin < 3; cin++) {
-                                long w = dp[p + 1][nsa][nsb][cin];
-                                if (w == 0) continue;
-                                int total = da + db + cin;
-                                if (total % 10 == dig) {
-                                    int cout = total / 10;
-                                    dp[p][sa][sb][cout] += w;
-                                }
-                            }
+        final int STATES = 32;
+        long[][] dp = new long[len + 1][STATES];
+        int init_st = 0;
+        dp[0][init_st] = 1;
+        for (int p = 0; p < len; ++p) {
+            for (int st = 0; st < STATES; ++st) {
+                if (dp[p][st] == 0) continue;
+                int carry = st & 1;
+                int hb = (st >> 1) & 1;
+                int swb = (st >> 2) & 1;
+                int ha = (st >> 3) & 1;
+                int swa = (st >> 4) & 1;
+                for (int da = 0; da < 10; ++da) {
+                    boolean valid_da;
+                    int new_swa;
+                    int new_ha = ha | (da != 0 ? 1 : 0);
+                    if (swa == 1) {
+                        valid_da = (da == 0);
+                        new_swa = 1;
+                    } else {
+                        valid_da = true;
+                        new_swa = (da == 0) ? 1 : 0;
+                    }
+                    if (!valid_da) continue;
+                    for (int db = 0; db < 10; ++db) {
+                        boolean valid_db;
+                        int new_swb;
+                        int new_hb = hb | (db != 0 ? 1 : 0);
+                        if (swb == 1) {
+                            valid_db = (db == 0);
+                            new_swb = 1;
+                        } else {
+                            valid_db = true;
+                            new_swb = (db == 0) ? 1 : 0;
                         }
+                        if (!valid_db) continue;
+                        int total = da + db + carry;
+                        if (total % 10 != digits[p]) continue;
+                        int new_carry = total / 10;
+                        int new_st = (new_swa << 4) | (new_ha << 3) | (new_swb << 2) | (new_hb << 1) | new_carry;
+                        dp[p + 1][new_st] += dp[p][st];
                     }
                 }
             }
         }
-        return dp[0][0][0][0];
+        long ans = 0;
+        for (int st = 0; st < STATES; ++st) {
+            int carry = st & 1;
+            int hb = (st >> 1) & 1;
+            int ha = (st >> 3) & 1;
+            if (carry == 0 && ha == 1 && hb == 1) {
+                ans += dp[len][st];
+            }
+        }
+        return ans;
     }
 }
 # @lc code=end
