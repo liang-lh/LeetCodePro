2d50a032042436f1b0b5445a85ab0b16fe74d152
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 20:23:15 +0800

    Batch add 1 solutions: 3765.complete-prime-...

--- 3765.complete-prime-number.java
+++ 3765.complete-prime-number.java
@@ -3,47 +3,26 @@
 #
 # [3765] Complete Prime Number
 #
+
 # @lc code=start
 class Solution {
     public boolean completePrime(int num) {
         String s = String.valueOf(num);
-        int len = s.length();
-        
-        // Check all prefixes
-        for (int i = 1; i <= len; i++) {
-            String prefix = s.substring(0, i);
-            int prefixNum = Integer.parseInt(prefix);
-            if (!isPrime(prefixNum)) {
-                return false;
-            }
+        int n = s.length();
+        for (int k = 1; k <= n; ++k) {
+            int prefix = Integer.parseInt(s.substring(0, k));
+            if (!isPrime(prefix)) return false;
+            int suffix = Integer.parseInt(s.substring(n - k));
+            if (!isPrime(suffix)) return false;
         }
-        
-        // Check all suffixes
-        for (int i = 0; i < len; i++) {
-            String suffix = s.substring(i);
-            int suffixNum = Integer.parseInt(suffix);
-            if (!isPrime(suffixNum)) {
-                return false;
-            }
-        }
-        
         return true;
     }
-    
-    private boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        if (n == 2) {
-            return true;
-        }
-        if (n % 2 == 0) {
-            return false;
-        }
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
-                return false;
-            }
+    private boolean isPrime(int x) {
+        if (x <= 1) return false;
+        if (x == 2 || x == 3) return true;
+        if (x % 2 == 0 || x % 3 == 0) return false;
+        for (int i = 5; i * i <= x; i += 6) {
+            if (x % i == 0 || x % (i + 2) == 0) return false;
         }
         return true;
     }
