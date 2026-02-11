c1478acd48d58e4fe0da48a843b989f7abcf29ba
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 14:10:09 +0800

    Batch add 1 solutions: 3425.longest-special...

--- 3425.longest-special-path.java
+++ 3425.longest-special-path.java
@@ -3,67 +3,72 @@
 #
 # [3425] Longest Special Path
 #
+
 # @lc code=start
+import java.util.*;
+
 class Solution {
-    private long maxLen = 0;
-    private int minNodes = 1;
-    
     public int[] longestSpecialPath(int[][] edges, int[] nums) {
         int n = nums.length;
-        List<List<int[]>> graph = new ArrayList<>();
+        List<List<int[]>> adj = new ArrayList<>(n);
         for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
-        
         for (int[] e : edges) {
-            graph.get(e[0]).add(new int[]{e[1], e[2]});
-            graph.get(e[1]).add(new int[]{e[0], e[2]});
+            int u = e[0], v = e[1], w = e[2];
+            adj.get(u).add(new int[]{v, w});
+            adj.get(v).add(new int[]{u, w});
         }
-        
-        dfs(0, -1, graph, nums, new ArrayList<>(), new HashMap<>());
-        
-        return new int[]{(int)maxLen, minNodes};
-    }
-    
-    private void dfs(int u, int parent, List<List<int[]>> graph, int[] nums,
-                     List<Integer> pathEdges, Map<Integer, Integer> valueToDepth) {
-        int val = nums[u];
-        int currentDepth = pathEdges.size();
-        
-        int validStartDepth = 0;
-        if (valueToDepth.containsKey(val)) {
-            validStartDepth = valueToDepth.get(val) + 1;
-        }
-        
-        long pathLen = 0;
-        for (int i = validStartDepth; i < currentDepth; i++) {
-            pathLen += pathEdges.get(i);
-        }
-        
-        int nodeCount = currentDepth - validStartDepth + 1;
-        
-        if (pathLen > maxLen || (pathLen == maxLen && nodeCount < minNodes)) {
-            maxLen = pathLen;
-            minNodes = nodeCount;
-        }
-        
-        int prevDepth = valueToDepth.getOrDefault(val, -1);
-        valueToDepth.put(val, currentDepth);
-        
-        for (int[] next : graph.get(u)) {
-            int v = next[0], edgeLen = next[1];
-            if (v != parent) {
-                pathEdges.add(edgeLen);
-                dfs(v, u, graph, nums, pathEdges, valueToDepth);
-                pathEdges.remove(pathEdges.size() - 1);
+        int[] leftArr = {0};
+        long[] maxLenArr = {0L};
+        int[] minNodesArr = {1};
+        List<Long> pathDist = new ArrayList<>();
+        Map<Integer, Integer> valueToPos = new HashMap<>();
+
+        class DfsHelper {
+            public void dfs(int u, int parent, long edgeWeight, List<Long> pathDist, Map<Integer, Integer> valueToPos,
+                            int[] leftArr, long[] maxLenArr, int[] minNodesArr,
+                            List<List<int[]>> adj, int[] nums) {
+                int depth = pathDist.size();
+                int val = nums[u];
+                int tempLeft = leftArr[0];
+                Integer prevOcc = valueToPos.get(val);
+                if (prevOcc != null && prevOcc >= leftArr[0]) {
+                    tempLeft = Math.max(tempLeft, prevOcc + 1);
+                }
+                int oldLastPos = (prevOcc != null ? prevOcc : -1);
+                long currDist = (depth == 0 ? 0L : pathDist.get(depth - 1)) + edgeWeight;
+                pathDist.add(currDist);
+                valueToPos.put(val, depth);
+                long thisLen = pathDist.get(depth) - pathDist.get(tempLeft);
+                int thisNodes = depth - tempLeft + 1;
+                if (thisLen > maxLenArr[0]) {
+                    maxLenArr[0] = thisLen;
+                    minNodesArr[0] = thisNodes;
+                } else if (thisLen == maxLenArr[0]) {
+                    minNodesArr[0] = Math.min(minNodesArr[0], thisNodes);
+                }
+                int savedLeft = leftArr[0];
+                leftArr[0] = tempLeft;
+                for (int[] nei : adj.get(u)) {
+                    int v = nei[0];
+                    int w = nei[1];
+                    if (v != parent) {
+                        dfs(v, u, w, pathDist, valueToPos, leftArr, maxLenArr, minNodesArr, adj, nums);
+                    }
+                }
+                leftArr[0] = savedLeft;
+                pathDist.remove(pathDist.size() - 1);
+                if (oldLastPos == -1) {
+                    valueToPos.remove(val);
+                } else {
+                    valueToPos.put(val, oldLastPos);
+                }
             }
         }
-        
-        if (prevDepth == -1) {
-            valueToDepth.remove(val);
-        } else {
-            valueToDepth.put(val, prevDepth);
-        }
+        DfsHelper helper = new DfsHelper();
+        helper.dfs(0, -1, 0L, pathDist, valueToPos, leftArr, maxLenArr, minNodesArr, adj, nums);
+        return new int[]{(int)maxLenArr[0], minNodesArr[0]};
     }
 }
 # @lc code=end
