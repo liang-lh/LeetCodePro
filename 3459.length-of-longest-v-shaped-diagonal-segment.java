9546b5e4585f0604702094ea73dbd29d11bc3ea4
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 14:48:48 +0800

    Batch add 1 solutions: 3459.length-of-longe...

--- 3459.length-of-longest-v-shaped-diagonal-segment.java
+++ 3459.length-of-longest-v-shaped-diagonal-segment.java
@@ -3,92 +3,89 @@
 #
 # [3459] Length of Longest V-Shaped Diagonal Segment
 #
+
 # @lc code=start
 class Solution {
     public int lenOfVDiagonal(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int maxLen = 0;
-        
-        // Four diagonal directions: down-right, down-left, up-left, up-right
-        int[][] dirs = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
-        
-        // For each cell with value 1, try starting a V-shaped segment
+        int[][][] fwdOdd = new int[n][m][4];
+        int[][][] fwdEven = new int[n][m][4];
+        int[][] dirs = {{1,1}, {1,-1}, {-1,-1}, {-1,1}};
+        // Precompute forward extensions
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    for (int dir = 0; dir < 4; dir++) {
-                        maxLen = Math.max(maxLen, findMaxLength(grid, i, j, dir, dirs, n, m));
+                for (int d = 0; d < 4; d++) {
+                    // fwdOdd: start exp=2
+                    {
+                        int cnt = 0;
+                        int ci = i, cj = j;
+                        int exp = 2;
+                        while (true) {
+                            int ni = ci + dirs[d][0];
+                            int nj = cj + dirs[d][1];
+                            if (ni < 0 || ni >= n || nj < 0 || nj >= m || grid[ni][nj] != exp) {
+                                break;
+                            }
+                            cnt++;
+                            ci = ni;
+                            cj = nj;
+                            exp = 2 - exp;
+                        }
+                        fwdOdd[i][j][d] = cnt;
+                    }
+                    // fwdEven: start exp=0
+                    {
+                        int cnt = 0;
+                        int ci = i, cj = j;
+                        int exp = 0;
+                        while (true) {
+                            int ni = ci + dirs[d][0];
+                            int nj = cj + dirs[d][1];
+                            if (ni < 0 || ni >= n || nj < 0 || nj >= m || grid[ni][nj] != exp) {
+                                break;
+                            }
+                            cnt++;
+                            ci = ni;
+                            cj = nj;
+                            exp = 2 - exp;
+                        }
+                        fwdEven[i][j][d] = cnt;
                     }
                 }
             }
         }
-        
-        return maxLen;
-    }
-    
-    private int findMaxLength(int[][] grid, int r, int c, int dir, int[][] dirs, int n, int m) {
-        int maxLen = 1;
-        int seqIdx = 0;
-        int lenBeforeTurn = 1;
-        
-        while (true) {
-            // Try making a clockwise turn from current position
-            int newDir = (dir + 1) % 4;
-            int nextR = r + dirs[newDir][0];
-            int nextC = c + dirs[newDir][1];
-            
-            if (isValid(nextR, nextC, n, m)) {
-                int expectedVal = getExpectedValue(seqIdx + 1);
-                if (grid[nextR][nextC] == expectedVal) {
-                    int lenAfterTurn = continueSegment(grid, nextR, nextC, seqIdx + 1, newDir, dirs, n, m);
-                    maxLen = Math.max(maxLen, lenBeforeTurn + lenAfterTurn);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    ans = Math.max(ans, 1);
+                    for (int d = 0; d < 4; d++) {
+                        ans = Math.max(ans, 1 + fwdOdd[i][j][d]);
+                        // Simulate first leg for possible turn
+                        int ci = i, cj = j;
+                        int len = 1;
+                        int nextExp = 2;
+                        while (true) {
+                            int ni = ci + dirs[d][0];
+                            int nj = cj + dirs[d][1];
+                            if (ni < 0 || ni >= n || nj < 0 || nj >= m || grid[ni][nj] != nextExp) {
+                                break;
+                            }
+                            ci = ni;
+                            cj = nj;
+                            len++;
+                            int dNext = (d + 1) % 4;
+                            int expAfter = (len % 2 == 0) ? 0 : 2;
+                            int addLen = expAfter == 2 ? fwdOdd[ci][cj][dNext] : fwdEven[ci][cj][dNext];
+                            ans = Math.max(ans, len + addLen);
+                            nextExp = 2 - nextExp;
+                        }
+                    }
                 }
             }
-            
-            // Continue in the current direction
-            r += dirs[dir][0];
-            c += dirs[dir][1];
-            seqIdx++;
-            
-            if (!isValid(r, c, n, m)) break;
-            
-            int expectedVal = getExpectedValue(seqIdx);
-            if (grid[r][c] != expectedVal) break;
-            
-            lenBeforeTurn++;
         }
-        
-        // Return max of path with turn or straight path without turn
-        return Math.max(maxLen, lenBeforeTurn);
-    }
-    
-    private int continueSegment(int[][] grid, int r, int c, int seqIdx, int dir, int[][] dirs, int n, int m) {
-        int len = 1;
-        
-        while (true) {
-            r += dirs[dir][0];
-            c += dirs[dir][1];
-            seqIdx++;
-            
-            if (!isValid(r, c, n, m)) break;
-            
-            int expectedVal = getExpectedValue(seqIdx);
-            if (grid[r][c] != expectedVal) break;
-            
-            len++;
-        }
-        
-        return len;
-    }
-    
-    private int getExpectedValue(int seqIdx) {
-        if (seqIdx == 0) return 1;
-        return (seqIdx % 2 == 1) ? 2 : 0;
-    }
-    
-    private boolean isValid(int r, int c, int n, int m) {
-        return r >= 0 && r < n && c >= 0 && c < m;
+        return ans;
     }
 }
 # @lc code=end
