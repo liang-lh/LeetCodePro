0535fc9e75db27e18e834bf921093b4b73369d67
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 23:45:17 +0800

    Batch add 1 solutions: 3503.longest-palindr...

--- 3503.longest-palindrome-after-substring-concatenation-i.java
+++ 3503.longest-palindrome-after-substring-concatenation-i.java
@@ -6,58 +6,48 @@
 
 # @lc code=start
 class Solution {
-    private static boolean isPalindrome(String str) {
-        int L = str.length();
-        for (int k = 0; k < L / 2; ++k) {
-            if (str.charAt(k) != str.charAt(L - 1 - k)) {
-                return false;
+  public int longestPalindrome(String s, String t) {
+    int ans = 0;
+    int ns = s.length();
+    int nt = t.length();
+    for (int i = 0; i <= ns; ++i) {
+      for (int j = i; j <= ns; ++j) {
+        int leftLen = j - i;
+        for (int p = 0; p <= nt; ++p) {
+          for (int q = p; q <= nt; ++q) {
+            int rightLen = q - p;
+            int totalLen = leftLen + rightLen;
+            if (totalLen <= ans) continue;
+            boolean isPal = true;
+            for (int k = 0; k < totalLen / 2; ++k) {
+              // left position k
+              char leftChar;
+              if (k < leftLen) {
+                leftChar = s.charAt(i + k);
+              } else {
+                leftChar = t.charAt(p + k - leftLen);
+              }
+              // right position totalLen - 1 - k
+              int rightPos = totalLen - 1 - k;
+              char rightChar;
+              if (rightPos < leftLen) {
+                rightChar = s.charAt(i + rightPos);
+              } else {
+                rightChar = t.charAt(p + rightPos - leftLen);
+              }
+              if (leftChar != rightChar) {
+                isPal = false;
+                break;
+              }
             }
+            if (isPal) {
+              ans = Math.max(ans, totalLen);
+            }
+          }
         }
-        return true;
+      }
     }
-
-    public int longestPalindrome(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        int ans = 0;
-
-        // Case 1: substrings from s only
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j <= n; ++j) {
-                String sub = s.substring(i, j);
-                if (isPalindrome(sub)) {
-                    ans = Math.max(ans, j - i);
-                }
-            }
-        }
-
-        // Case 2: substrings from t only
-        for (int i = 0; i < m; ++i) {
-            for (int j = i + 1; j <= m; ++j) {
-                String sub = t.substring(i, j);
-                if (isPalindrome(sub)) {
-                    ans = Math.max(ans, j - i);
-                }
-            }
-        }
-
-        // Case 3: non-empty from s + non-empty from t
-        for (int i = 0; i < n; ++i) {
-            for (int lenS = 1; lenS <= n - i; ++lenS) {
-                String left = s.substring(i, i + lenS);
-                for (int k = 0; k < m; ++k) {
-                    for (int lenT = 1; lenT <= m - k; ++lenT) {
-                        String right = t.substring(k, k + lenT);
-                        String combined = left + right;
-                        if (isPalindrome(combined)) {
-                            ans = Math.max(ans, combined.length());
-                        }
-                    }
-                }
-            }
-        }
-
-        return ans;
-    }
+    return ans;
+  }
 }
 # @lc code=end
