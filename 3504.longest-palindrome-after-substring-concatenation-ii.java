11d9997c5b8e018e3d87a7f06738c3e345422df4
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 23:53:55 +0800

    Batch add 1 solutions: 3504.longest-palindr...

--- 3504.longest-palindrome-after-substring-concatenation-ii.java
+++ 3504.longest-palindrome-after-substring-concatenation-ii.java
@@ -9,134 +9,145 @@ class Solution {
     public int longestPalindrome(String s, String t) {
         int ns = s.length();
         int nt = t.length();
-        boolean[][] palS = computePal(s, ns);
-        int[] maxStartS = computeMaxStart(palS, ns);
-        int lpsS = 0;
+        
+        // Palindromes in s
+        boolean[][] palS = new boolean[ns][ns];
+        int maxS = 1;
         for (int i = 0; i < ns; i++) {
-            lpsS = Math.max(lpsS, maxStartS[i]);
+            palS[i][i] = true;
         }
-        boolean[][] palT = computePal(t, nt);
-        int[] maxEndT = computeMaxEnd(palT, nt);
-        int lpsT = 0;
-        for (int j = 0; j < nt; j++) {
-            lpsT = Math.max(lpsT, maxEndT[j]);
+        for (int i = 0; i < ns - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                palS[i][i + 1] = true;
+                maxS = 2;
+            }
         }
-        int ans = Math.max(lpsS, lpsT);
-        String rt = new StringBuilder(t).reverse().toString();
-        RollingHash hs = new RollingHash(s);
-        RollingHash hrt = new RollingHash(rt);
-        for (int sx = 0; sx < ns; sx++) {
-            for (int cy = 0; cy < nt; cy++) {
-                int left = 0;
-                int right = Math.min(ns - sx, nt - cy);
-                while (left <= right) {
-                    int mid = left + (right - left + 1) / 2;
-                    int rtst = nt - cy - mid;
-                    if (rtst >= 0 && hs.same(sx, sx + mid - 1, hrt, rtst, rtst + mid - 1)) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
-                }
-                int k = right;
-                if (k >= 1) {
-                    // s extra
-                    int temp = 2 * k;
-                    if (sx + k < ns) {
-                        temp += maxStartS[sx + k];
-                    }
-                    ans = Math.max(ans, temp);
-                    // t extra
-                    temp = 2 * k;
-                    if (cy > 0) {
-                        temp += maxEndT[cy - 1];
-                    }
-                    ans = Math.max(ans, temp);
+        for (int len = 3; len <= ns; len++) {
+            for (int i = 0; i <= ns - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && palS[i + 1][j - 1]) {
+                    palS[i][j] = true;
+                    maxS = Math.max(maxS, len);
                 }
             }
         }
+        
+        // Palindromes in t
+        boolean[][] palT = new boolean[nt][nt];
+        int maxT = 1;
+        for (int i = 0; i < nt; i++) {
+            palT[i][i] = true;
+        }
+        for (int i = 0; i < nt - 1; i++) {
+            if (t.charAt(i) == t.charAt(i + 1)) {
+                palT[i][i + 1] = true;
+                maxT = 2;
+            }
+        }
+        for (int len = 3; len <= nt; len++) {
+            for (int i = 0; i <= nt - len; i++) {
+                int j = i + len - 1;
+                if (t.charAt(i) == t.charAt(j) && palT[i + 1][j - 1]) {
+                    palT[i][j] = true;
+                    maxT = Math.max(maxT, len);
+                }
+            }
+        }
+        
+        int ans = Math.max(maxS, maxT);
+        
+        // Equal arms: 2 * LCS(s, reverse(t))
+        String rt = new StringBuilder(t).reverse().toString();
+        int lcsEq = lcsSubstr(s, rt);
+        ans = Math.max(ans, 2 * lcsEq);
+        
+        // Centers in s
+        int[][] dpSrt = lcsDp(s, rt);
+        int[] maxKendS = new int[ns];
+        for (int p = 0; p < ns; p++) {
+            int mx = 0;
+            for (int q = 1; q <= nt; q++) {
+                mx = Math.max(mx, dpSrt[p + 1][q]);
+            }
+            maxKendS[p] = mx;
+        }
+        for (int l = 0; l < ns; l++) {
+            for (int r = l; r < ns; r++) {
+                if (palS[l][r]) {
+                    int d = r - l + 1;
+                    int k = (l == 0 ? 0 : maxKendS[l - 1]);
+                    ans = Math.max(ans, 2 * k + d);
+                }
+            }
+        }
+        
+        // Centers in t
+        String rs = new StringBuilder(s).reverse().toString();
+        int[][] dpTrs = lcsDp(t, rs);
+        int[] maxKendT = new int[nt];
+        for (int p = 0; p < nt; p++) {
+            int mx = 0;
+            for (int q = 1; q <= ns; q++) {
+                mx = Math.max(mx, dpTrs[p + 1][q]);
+            }
+            maxKendT[p] = mx;
+        }
+        int[] maxKstartT = new int[nt];
+        for (int st = 0; st < nt; st++) {
+            int curmax = 0;
+            int rem = nt - st;
+            for (int k = 1; k <= rem; k++) {
+                int ep = st + k - 1;
+                if (maxKendT[ep] >= k) {
+                    curmax = k;
+                }
+            }
+            maxKstartT[st] = curmax;
+        }
+        for (int l = 0; l < nt; l++) {
+            for (int r = l; r < nt; r++) {
+                if (palT[l][r]) {
+                    int d = r - l + 1;
+                    int k = 0;
+                    if (r + 1 < nt) {
+                        k = maxKstartT[r + 1];
+                    }
+                    ans = Math.max(ans, 2 * k + d);
+                }
+            }
+        }
+        
         return ans;
     }
-    private boolean[][] computePal(String str, int n) {
-        boolean[][] pal = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            pal[i][i] = true;
-        }
-        for (int i = 0; i < n - 1; i++) {
-            if (str.charAt(i) == str.charAt(i + 1)) {
-                pal[i][i + 1] = true;
-            }
-        }
-        for (int leng = 3; leng <= n; leng++) {
-            for (int i = 0; i <= n - leng; i++) {
-                int j = i + leng - 1;
-                if (str.charAt(i) == str.charAt(j) && pal[i + 1][j - 1]) {
-                    pal[i][j] = true;
+    
+    private int lcsSubstr(String a, String b) {
+        int la = a.length();
+        int lb = b.length();
+        int[][] dp = new int[la + 1][lb + 1];
+        int mx = 0;
+        for (int i = 1; i <= la; i++) {
+            for (int j = 1; j <= lb; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    mx = Math.max(mx, dp[i][j]);
                 }
             }
         }
-        return pal;
+        return mx;
     }
-    private int[] computeMaxStart(boolean[][] pal, int n) {
-        int[] maxSt = new int[n];
-        for (int i = 0; i < n; i++) {
-            int mx = 1;
-            for (int j = i; j < n; j++) {
-                if (pal[i][j]) {
-                    mx = j - i + 1;
+    
+    private int[][] lcsDp(String a, String b) {
+        int la = a.length();
+        int lb = b.length();
+        int[][] dp = new int[la + 1][lb + 1];
+        for (int i = 1; i <= la; i++) {
+            for (int j = 1; j <= lb; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
             }
-            maxSt[i] = mx;
         }
-        return maxSt;
-    }
-    private int[] computeMaxEnd(boolean[][] pal, int n) {
-        int[] maxEn = new int[n];
-        for (int j = 0; j < n; j++) {
-            int mx = 1;
-            for (int i = j; i >= 0; i--) {
-                if (pal[i][j]) {
-                    mx = j - i + 1;
-                }
-            }
-            maxEn[j] = mx;
-        }
-        return maxEn;
-    }
-    private static class RollingHash {
-        private long[] h1, p1, h2, p2;
-        private final long MOD1 = 1000000007L;
-        private final long MOD2 = 1000000009L;
-        private final long BASE1 = 131L;
-        private final long BASE2 = 137L;
-        private final int n;
-        public RollingHash(String s) {
-            n = s.length();
-            h1 = new long[n + 1];
-            p1 = new long[n + 1];
-            h2 = new long[n + 1];
-            p2 = new long[n + 1];
-            p1[0] = 1;
-            p2[0] = 1;
-            for (int i = 0; i < n; i++) {
-                long val = s.charAt(i) - 'a' + 1;
-                h1[i + 1] = (h1[i] * BASE1 + val) % MOD1;
-                p1[i + 1] = p1[i] * BASE1 % MOD1;
-                h2[i + 1] = (h2[i] * BASE2 + val) % MOD2;
-                p2[i + 1] = p2[i] * BASE2 % MOD2;
-            }
-        }
-        public long getHash1(int l, int r) {
-            long res = (h1[r + 1] - h1[l] * p1[r - l + 1] % MOD1 + MOD1) % MOD1;
-            return res;
-        }
-        public long getHash2(int l, int r) {
-            long res = (h2[r + 1] - h2[l] * p2[r - l + 1] % MOD2 + MOD2) % MOD2;
-            return res;
-        }
-        public boolean same(int l1, int r1, RollingHash other, int l2, int r2) {
-            return getHash1(l1, r1) == other.getHash1(l2, r2) && getHash2(l1, r1) == other.getHash2(l2, r2);
-        }
+        return dp;
     }
 }
 # @lc code=end
