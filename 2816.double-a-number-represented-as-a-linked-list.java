9abc9914867576c34c7df3e57c64d2a519578345
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 12:43:57 +0800

    Batch add 1 solutions: 2816.double-a-number...

--- 2816.double-a-number-represented-as-a-linked-list.java
+++ 2816.double-a-number-represented-as-a-linked-list.java
@@ -3,6 +3,7 @@
 #
 # [2816] Double a Number Represented as a Linked List
 #
+
 # @lc code=start
 /**
  * Definition for singly-linked list.
@@ -16,34 +17,33 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        int carry = doubleHelper(head);
-        
-        // If there's a carry after processing the head, create a new head
-        if (carry > 0) {
-            ListNode newHead = new ListNode(carry);
-            newHead.next = head;
-            return newHead;
+        ListNode revHead = reverseList(head);
+        ListNode curr = revHead;
+        int carry = 0;
+        ListNode tail = null;
+        while (curr != null) {
+            int prod = curr.val * 2 + carry;
+            curr.val = prod % 10;
+            carry = prod / 10;
+            tail = curr;
+            curr = curr.next;
         }
-        
-        return head;
+        if (carry != 0) {
+            tail.next = new ListNode(carry);
+        }
+        return reverseList(revHead);
     }
     
-    private int doubleHelper(ListNode node) {
-        if (node == null) {
-            return 0;
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
         }
-        
-        // Recursively get carry from the right side
-        int carry = doubleHelper(node.next);
-        
-        // Double current value and add carry from right
-        int doubled = node.val * 2 + carry;
-        
-        // Update current node value (keep only the ones digit)
-        node.val = doubled % 10;
-        
-        // Return carry to the left (the tens digit)
-        return doubled / 10;
+        return prev;
     }
 }
 # @lc code=end
