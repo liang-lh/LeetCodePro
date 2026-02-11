419de4c5e00e7879f6a4785c6403b6f56bc4de4c
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 19:52:53 +0800

    Batch add 1 solutions: 3721.longest-balance...

--- 3721.longest-balanced-subarray-ii.java
+++ 3721.longest-balanced-subarray-ii.java
@@ -3,30 +3,57 @@
 #
 # [3721] Longest Balanced Subarray II
 #
+
 # @lc code=start
 class Solution {
-    public int longestBalanced(int[] nums) {
+    private boolean canBalance(int[] nums, int L) {
         int n = nums.length;
-        int maxLen = 0;
-        
-        for (int i = 0; i < n; i++) {
-            java.util.Set<Integer> evenSet = new java.util.HashSet<>();
-            java.util.Set<Integer> oddSet = new java.util.HashSet<>();
-            
-            for (int j = i; j < n; j++) {
-                if (nums[j] % 2 == 0) {
-                    evenSet.add(nums[j]);
-                } else {
-                    oddSet.add(nums[j]);
-                }
-                
-                if (evenSet.size() == oddSet.size()) {
-                    maxLen = Math.max(maxLen, j - i + 1);
-                }
+        if (L == 0) return true;
+        if (L > n) return false;
+        final int MAX = 100000;
+        int[] evenFreq = new int[MAX + 1];
+        int[] oddFreq = new int[MAX + 1];
+        int evenDistinct = 0;
+        int oddDistinct = 0;
+        // Initialize first window
+        for (int i = 0; i < L; i++) {
+            int num = nums[i];
+            if ((num & 1) == 0) {  // even
+                if (evenFreq[num]++ == 0) evenDistinct++;
+            } else {  // odd
+                if (oddFreq[num]++ == 0) oddDistinct++;
             }
         }
-        
-        return maxLen;
+        if (evenDistinct == oddDistinct) return true;
+        // Slide the window
+        for (int i = L; i < n; i++) {
+            // Remove nums[i - L]
+            int rem = nums[i - L];
+            if ((rem & 1) == 0) {
+                if (--evenFreq[rem] == 0) evenDistinct--;
+            } else {
+                if (--oddFreq[rem] == 0) oddDistinct--;
+            }
+            // Add nums[i]
+            int addNum = nums[i];
+            if ((addNum & 1) == 0) {
+                if (evenFreq[addNum]++ == 0) evenDistinct++;
+            } else {
+                if (oddFreq[addNum]++ == 0) oddDistinct++;
+            }
+            if (evenDistinct == oddDistinct) return true;
+        }
+        return false;
+    }
+
+    public int longestBalanced(int[] nums) {
+        int n = nums.length;
+        for (int L = n; L >= 0; L--) {
+            if (canBalance(nums, L)) {
+                return L;
+            }
+        }
+        return 0;
     }
 }
 # @lc code=end
