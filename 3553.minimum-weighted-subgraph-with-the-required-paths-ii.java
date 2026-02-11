d74378d8f552d78db03ee5cc854f600c211f7139
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 01:01:38 +0800

    Batch add 1 solutions: 3553.minimum-weighte...

--- 3553.minimum-weighted-subgraph-with-the-required-paths-ii.java
+++ 3553.minimum-weighted-subgraph-with-the-required-paths-ii.java
@@ -5,96 +5,100 @@
 #
 
 # @lc code=start
+import java.util.*;
 class Solution {
     public int[] minimumWeight(int[][] edges, int[][] queries) {
         int n = edges.length + 1;
-        @SuppressWarnings("unchecked")
-        java.util.List<int[]>[] adj = new java.util.List[n];
+        List<int[]>[] adj = new List[n];
         for (int i = 0; i < n; i++) {
-            adj[i] = new java.util.ArrayList<int[]>();
+            adj[i] = new ArrayList<>();
         }
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1], w = edge[2];
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], w = e[2];
             adj[u].add(new int[]{v, w});
             adj[v].add(new int[]{u, w});
         }
-        long[] dep = new long[n];
-        int[] lev = new int[n];
-        int[][] par = new int[n][18];
-        boolean[] visited = new boolean[n];
+        final int LOG = 18;
+        int[][] par = new int[n][LOG];
+        long[] wdepth = new long[n];
+        int[] level = new int[n];
+        boolean[] vis = new boolean[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 18; j++) {
-                par[i][j] = -1;
-            }
+            Arrays.fill(par[i], -1);
         }
-        java.util.Queue<Integer> q = new java.util.LinkedList<>();
-        q.offer(0);
-        visited[0] = true;
-        dep[0] = 0;
-        lev[0] = 0;
-        par[0][0] = -1;
+        // BFS preprocess
+        Queue<Integer> q = new LinkedList<>();
+        int root = 0;
+        q.offer(root);
+        vis[root] = true;
+        par[root][0] = -1;
+        level[root] = 0;
+        wdepth[root] = 0;
         while (!q.isEmpty()) {
             int u = q.poll();
-            for (int[] nei : adj[u]) {
-                int v = nei[0];
-                int w = nei[1];
-                if (!visited[v]) {
-                    visited[v] = true;
-                    dep[v] = dep[u] + w;
-                    lev[v] = lev[u] + 1;
+            for (int[] e : adj[u]) {
+                int v = e[0];
+                if (!vis[v]) {
+                    vis[v] = true;
                     par[v][0] = u;
+                    level[v] = level[u] + 1;
+                    wdepth[v] = wdepth[u] + e[1];
                     q.offer(v);
                 }
             }
         }
-        for (int j = 1; j < 18; j++) {
+        // Build jumping table
+        for (int k = 1; k < LOG; k++) {
             for (int i = 0; i < n; i++) {
-                int p = par[i][j - 1];
+                int p = par[i][k - 1];
                 if (p != -1) {
-                    par[i][j] = par[p][j - 1];
+                    par[i][k] = par[p][k - 1];
+                } else {
+                    par[i][k] = -1;
                 }
             }
         }
-        class DistHelper {
-            long dist(int u, int v) {
-                int x = u, y = v;
-                if (lev[x] > lev[y]) {
-                    int tmp = x;
-                    x = y;
-                    y = tmp;
-                }
-                int delta = lev[y] - lev[x];
-                for (int j = 0; j < 18; j++) {
-                    if ((delta & (1 << j)) != 0) {
-                        y = par[y][j];
-                    }
-                }
-                if (x == y) {
-                    return dep[u] + dep[v] - 2L * dep[x];
-                }
-                for (int j = 17; j >= 0; j--) {
-                    if (par[x][j] != -1 && par[y][j] != -1 && par[x][j] != par[y][j]) {
-                        x = par[x][j];
-                        y = par[y][j];
-                    }
-                }
-                int lca = par[x][0];
-                return dep[u] + dep[v] - 2L * dep[lca];
-            }
-        }
-        DistHelper dh = new DistHelper();
         int m = queries.length;
         int[] ans = new int[m];
         for (int i = 0; i < m; i++) {
-            int src1 = queries[i][0];
-            int src2 = queries[i][1];
-            int dest = queries[i][2];
-            long d12 = dh.dist(src1, src2);
-            long d1d = dh.dist(src1, dest);
-            long d2d = dh.dist(src2, dest);
-            ans[i] = (int) ((d12 + d1d + d2d) / 2);
+            int a = queries[i][0];
+            int b = queries[i][1];
+            int c = queries[i][2];
+            long dab = getDist(a, b, par, level, wdepth, LOG);
+            long dac = getDist(a, c, par, level, wdepth, LOG);
+            long dbc = getDist(b, c, par, level, wdepth, LOG);
+            ans[i] = (int) ((dab + dac + dbc) / 2);
         }
         return ans;
+    }
+
+    private int getLCA(int u, int v, int[][] par, int[] level, int LOG) {
+        if (level[u] > level[v]) {
+            int tmp = u;
+            u = v;
+            v = tmp;
+        }
+        int diff = level[v] - level[u];
+        for (int k = 0; k < LOG; k++) {
+            if ((diff & (1 << k)) != 0 && par[v][k] != -1) {
+                v = par[v][k];
+            }
+        }
+        if (u == v) {
+            return u;
+        }
+        for (int k = LOG - 1; k >= 0; k--) {
+            if (par[u][k] != -1 && par[v][k] != -1 && par[u][k] != par[v][k]) {
+                u = par[u][k];
+                v = par[v][k];
+            }
+        }
+        return par[u][0];
+    }
+
+    private long getDist(int u, int v, int[][] par, int[] level, long[] wdepth, int LOG) {
+        int l = getLCA(u, v, par, level, LOG);
+        return wdepth[u] + wdepth[v] - 2L * wdepth[l];
     }
 }
 # @lc code=end
