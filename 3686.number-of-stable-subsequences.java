635b051b7d3fd4b5fc739a9f2310405ce5c34b0b
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 02:43:45 +0800

    Batch add 1 solutions: 3686.number-of-stabl...

--- 3686.number-of-stable-subsequences.java
+++ 3686.number-of-stable-subsequences.java
@@ -7,28 +7,28 @@
 # @lc code=start
 class Solution {
     public int countStableSubsequences(int[] nums) {
-        final long MOD = 1000000007L;
-        long o1 = 0, o2 = 0, e1 = 0, e2 = 0;
+        final int MOD = 1000000007;
+        long a = 0, b = 0, c = 0, d = 0;
         for (int num : nums) {
             int p = num % 2;
-            long no1 = 0, no2 = 0, ne1 = 0, ne2 = 0;
-            if (p == 1) {
-                no1 = (o1 + e1 + e2 + 1) % MOD;
-                no2 = (o2 + o1) % MOD;
-                ne1 = e1 % MOD;
-                ne2 = e2 % MOD;
+            long na, nb, nc, nd;
+            if (p == 0) {
+                na = (a + 1 + c + d) % MOD;
+                nb = (b + a) % MOD;
+                nc = c;
+                nd = d;
             } else {
-                ne1 = (e1 + o1 + o2 + 1) % MOD;
-                ne2 = (e2 + e1) % MOD;
-                no1 = o1 % MOD;
-                no2 = o2 % MOD;
+                nc = (c + 1 + a + b) % MOD;
+                nd = (d + c) % MOD;
+                na = a;
+                nb = b;
             }
-            o1 = no1;
-            o2 = no2;
-            e1 = ne1;
-            e2 = ne2;
+            a = na;
+            b = nb;
+            c = nc;
+            d = nd;
         }
-        return (int) ((o1 + o2 + e1 + e2) % MOD);
+        return (int) ((a + b + c + d) % MOD);
     }
 }
 # @lc code=end
