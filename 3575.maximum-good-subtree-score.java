4384139d4a7b696c8cedf93e2f0f6c9c592832b7
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 01:32:55 +0800

    Batch add 1 solutions: 3575.maximum-good-su...

--- 3575.maximum-good-subtree-score.java
+++ 3575.maximum-good-subtree-score.java
@@ -9,79 +9,141 @@ class Solution {
     public int goodSubtreeSum(int[] vals, int[] par) {
         final int MOD = 1000000007;
         int n = vals.length;
-        int N = 1 << 10;
-        boolean[] canUse = new boolean[n];
-        int[] dMask = new int[n];
-        for (int i = 0; i < n; ++i) {
+        int[] degree = new int[n];
+        for (int i = 1; i < n; i++) {
+            degree[par[i]]++;
+        }
+        int[][] chld = new int[n][];
+        int[] ptr = new int[n];
+        for (int i = 0; i < n; i++) {
+            chld[i] = new int[degree[i]];
+        }
+        for (int i = 1; i < n; i++) {
+            int p = par[i];
+            chld[p][ptr[p]++] = i;
+        }
+        int[] masks = new int[n];
+        long[] nodevals = new long[n];
+        for (int i = 0; i < n; i++) {
             String s = Integer.toString(vals[i]);
-            int[] count = new int[10];
-            for (char c : s.toCharArray()) {
-                ++count[c - '0'];
-            }
-            int mask = 0;
-            boolean valid = true;
-            for (int d = 0; d < 10; ++d) {
-                if (count[d] > 1) {
-                    valid = false;
+            boolean[] digUsed = new boolean[10];
+            boolean dup = false;
+            int msk = 0;
+            for (int k = 0; k < s.length(); k++) {
+                int d = s.charAt(k) - '0';
+                if (digUsed[d]) {
+                    dup = true;
+                    break;
                 }
-                if (count[d] > 0) {
-                    mask |= (1 << d);
-                }
+                digUsed[d] = true;
+                msk |= (1 << d);
             }
-            canUse[i] = valid;
-            dMask[i] = mask;
+            if (dup) {
+                nodevals[i] = 0L;
+                masks[i] = 0;
+            } else {
+                nodevals[i] = vals[i];
+                masks[i] = msk;
+            }
         }
-        java.util.List<Integer>[] adj = new java.util.List[n];
-        for (int i = 0; i < n; ++i) {
-            adj[i] = new java.util.ArrayList<Integer>();
-        }
-        for (int i = 1; i < n; ++i) {
-            adj[par[i]].add(i);
-        }
-        final long[] total = {0L};
-        java.util.function.Function<Integer, long[]> dfsFunc = new java.util.function.Function<Integer, long[]>() {
-            @Override
-            public long[] apply(Integer u) {
-                long[] dp = new long[N];
-                java.util.Arrays.fill(dp, Long.MIN_VALUE / 2);
-                dp[0] = 0L;
-                for (int v : adj[u]) {
-                    long[] childDp = apply(v);
-                    long[] newDp = new long[N];
-                    java.util.Arrays.fill(newDp, Long.MIN_VALUE / 2);
-                    for (int m1 = 0; m1 < N; ++m1) {
-                        if (dp[m1] == Long.MIN_VALUE / 2) continue;
-                        for (int m2 = 0; m2 < N; ++m2) {
-                            if (childDp[m2] == Long.MIN_VALUE / 2) continue;
-                            if ((m1 & m2) == 0) {
-                                int newMask = m1 | m2;
-                                newDp[newMask] = Math.max(newDp[newMask], dp[m1] + childDp[m2]);
+        long[] maxScore = new long[n];
+        int MS = 1 << 10;
+        class DfsHelper {
+            long[] dfs(int u, int[] masks, long[] nodevals, int[][] chld, long[] maxScore, int MS) {
+                long[] curr = new long[MS];
+                for (int i = 0; i < MS; i++) {
+                    curr[i] = -1L;
+                }
+                curr[0] = 0L;
+                for (int ci = 0; ci < chld[u].length; ci++) {
+                    int v = chld[u][ci];
+                    long[] chdp = dfs(v, masks, nodevals, chld, maxScore, MS);
+                    long[] nextt = new long[MS];
+                    for (int i = 0; i < MS; i++) {
+                        nextt[i] = -1L;
+                    }
+                    java.util.ArrayList<java.lang.Integer> currValid = new java.util.ArrayList<java.lang.Integer>();
+                    for (int m1 = 0; m1 < MS; m1++) {
+                        if (curr[m1] >= 0L) {
+                            currValid.add(m1);
+                        }
+                    }
+                    java.util.ArrayList<java.lang.Integer> chValid = new java.util.ArrayList<java.lang.Integer>();
+                    for (int m2 = 0; m2 < MS; m2++) {
+                        if (chdp[m2] >= 0L) {
+                            chValid.add(m2);
+                        }
+                    }
+                    long est = (long) currValid.size() * chValid.size();
+                    if (est > 500000L) {
+                        // full loop
+                        for (int m1 = 0; m1 < MS; m1++) {
+                            if (curr[m1] < 0L) continue;
+                            for (int m2 = 0; m2 < MS; m2++) {
+                                if (chdp[m2] < 0L) continue;
+                                if ((m1 & m2) != 0) continue;
+                                int nm = m1 | m2;
+                                long ns = curr[m1] + chdp[m2];
+                                if (nextt[nm] < ns) {
+                                    nextt[nm] = ns;
+                                }
+                            }
+                        }
+                    } else {
+                        // list loop
+                        for (int j = 0; j < currValid.size(); j++) {
+                            int m1 = currValid.get(j);
+                            for (int k = 0; k < chValid.size(); k++) {
+                                int m2 = chValid.get(k);
+                                if ((m1 & m2) == 0) {
+                                    int nm = m1 | m2;
+                                    long ns = curr[m1] + chdp[m2];
+                                    if (nextt[nm] < ns) {
+                                        nextt[nm] = ns;
+                                    }
+                                }
                             }
                         }
                     }
-                    dp = newDp;
+                    curr = nextt;
                 }
-                long[] fullDp = java.util.Arrays.copyOf(dp, N);
-                if (canUse[u]) {
-                    int mu = dMask[u];
-                    for (int m = 0; m < N; ++m) {
-                        if (dp[m] == Long.MIN_VALUE / 2) continue;
-                        if ((m & mu) == 0) {
-                            int nm = m | mu;
-                            fullDp[nm] = Math.max(fullDp[nm], dp[m] + (long) vals[u]);
+                long maxU = 0L;
+                for (int m = 0; m < MS; m++) {
+                    if (curr[m] > maxU) {
+                        maxU = curr[m];
+                    }
+                }
+                int omask = masks[u];
+                long oval = nodevals[u];
+                long maxAddU = 0L;
+                for (int m = 0; m < MS; m++) {
+                    if (curr[m] < 0L) continue;
+                    if ((m & omask) == 0) {
+                        maxAddU = Math.max(maxAddU, curr[m] + oval);
+                    }
+                }
+                maxU = Math.max(maxU, maxAddU);
+                maxScore[u] = maxU;
+                for (int m = 0; m < MS; m++) {
+                    if (curr[m] < 0L) continue;
+                    if ((m & omask) == 0) {
+                        int nm = m | omask;
+                        long ns = curr[m] + oval;
+                        if (curr[nm] < ns) {
+                            curr[nm] = ns;
                         }
                     }
                 }
-                long maxS = 0L;
-                for (long v : fullDp) {
-                    maxS = Math.max(maxS, v);
-                }
-                total[0] = (total[0] + maxS % MOD + MOD) % MOD;
-                return fullDp;
+                return curr;
             }
-        };
-        dfsFunc.apply(0);
-        return (int) total[0];
+        }
+        DfsHelper helper = new DfsHelper();
+        helper.dfs(0, masks, nodevals, chld, maxScore, MS);
+        long ans = 0L;
+        for (long x : maxScore) {
+            ans = (ans + x) % MOD;
+        }
+        return (int) ans;
     }
 }
 # @lc code=end
