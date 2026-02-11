539b8d42b9f7e178ffb55e2165f1c186eece8ba6
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 00:11:21 +0800

    Batch add 1 solutions: 3513.number-of-uniqu...

--- 3513.number-of-unique-xor-triplets-i.java
+++ 3513.number-of-unique-xor-triplets-i.java
@@ -8,8 +8,9 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        if (n <= 2) {
+            return n;
+        }
         int log = 31 - Integer.numberOfLeadingZeros(n);
         return 1 << (log + 1);
     }
