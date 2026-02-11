36e02d4521ebc0eb3a14f72a2c0e35badf569a5e
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 13:01:03 +0800

    Batch add 1 solutions: 2289.steps-to-make-a...

--- 2289.steps-to-make-array-non-decreasing.java
+++ 2289.steps-to-make-array-non-decreasing.java
@@ -3,27 +3,26 @@
 #
 # [2289] Steps to Make Array Non-decreasing
 #
-# @lc code=start
-import java.util.Stack;
 
+import java.util.*;
+
+# @lc code=start
 class Solution {
     public int totalSteps(int[] nums) {
-        int n = nums.length;
-        Stack<int[]> stack = new Stack<>(); // [value, steps]
-        int maxSteps = 0;
-        
-        // Process from right to left
-        for (int i = n - 1; i >= 0; i--) {
-            int steps = 0;
-            // Pop all smaller elements and calculate steps
-            while (!stack.isEmpty() && nums[i] > stack.peek()[0]) {
-                steps = Math.max(steps + 1, stack.pop()[1]);
+        Deque<int[]> stack = new ArrayDeque<>();
+        int ans = 0;
+        for (int num : nums) {
+            int cur = 0;
+            while (!stack.isEmpty() && num >= stack.peek()[1]) {
+                cur = Math.max(cur, stack.pop()[0]);
             }
-            stack.push(new int[]{nums[i], steps});
-            maxSteps = Math.max(maxSteps, steps);
+            if (!stack.isEmpty()) {
+                cur++;
+            }
+            ans = Math.max(ans, cur);
+            stack.push(new int[]{cur, num});
         }
-        
-        return maxSteps;
+        return ans;
     }
 }
 # @lc code=end
