2e1c30ba1d71091919620529fb3e6046b4843c5c
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 02:15:29 +0800

    Batch add 1 solutions: 3621.number-of-integ...

--- 3621.number-of-integers-with-popcount-depth-equal-to-k-i.java
+++ 3621.number-of-integers-with-popcount-depth-equal-to-k-i.java
@@ -6,78 +6,54 @@
 
 # @lc code=start
 class Solution {
-    private static final int MAX_LOG = 60;
-    private static final int MAX_POP = 64;
-    private long[][][] memo;
-    private int[] digits;
-    private int targetCnt;
-
-    private int getDepth(int x) {
-        int d = 0;
-        while (x != 1) {
-            x = Integer.bitCount(x);
-            d++;
-        }
-        return d;
-    }
-
-    private long dfs(int pos, int cnt, int tight) {
-        if (pos == MAX_LOG) {
-            return cnt == targetCnt ? 1L : 0L;
-        }
-        if (cnt > targetCnt) {
-            return 0L;
-        }
-        if (tight == 0 && memo[pos][cnt][0] != -1L) {
-            return memo[pos][cnt][0];
-        }
-        long res = 0L;
-        int lim = tight == 1 ? digits[pos] : 1;
-        for (int d = 0; d <= lim; d++) {
-            int newCnt = cnt + d;
-            if (newCnt > targetCnt) continue;
-            int newTight = (tight == 1 && d == lim) ? 1 : 0;
-            res += dfs(pos + 1, newCnt, newTight);
-        }
-        if (tight == 0) {
-            memo[pos][cnt][0] = res;
-        }
-        return res;
-    }
-
-    private long countNumbers(long num, int pop) {
-        if (num <= 0 || pop == 0) return 0L;
-        targetCnt = pop;
-        digits = new int[MAX_LOG];
-        for (int i = 0; i < MAX_LOG; i++) {
-            digits[i] = (int) ((num >> (MAX_LOG - 1 - i)) & 1L);
-        }
-        memo = new long[MAX_LOG][MAX_POP + 1][2];
-        for (int i = 0; i < MAX_LOG; i++) {
-            for (int j = 0; j <= MAX_POP; j++) {
-                memo[i][j][0] = -1L;
-                memo[i][j][1] = -1L;
-            }
-        }
-        return dfs(0, 0, 1);
-    }
-
     public long popcountDepth(long n, int k) {
         if (k == 0) {
-            return 1L;
+            return n >= 1 ? 1L : 0L;
         }
-        int[] depths = new int[MAX_POP + 1];
-        for (int i = 1; i <= MAX_POP; i++) {
-            depths[i] = getDepth(i);
+        int[] depths = new int[65];
+        for (int m = 1; m <= 64; m++) {
+            long num = m;
+            int dep = 0;
+            while (num > 1) {
+                num = Long.bitCount(num);
+                dep++;
+            }
+            depths[m] = dep;
+        }
+        String s = Long.toBinaryString(n);
+        int length = s.length();
+        int[] digits = new int[length];
+        for (int i = 0; i < length; i++) {
+            digits[i] = s.charAt(i) - '0';
         }
         long answer = 0L;
-        for (int s = 1; s <= MAX_POP; s++) {
-            if (depths[s] == k - 1) {
-                answer += countNumbers(n, s);
+        for (int ones_cnt = 1; ones_cnt <= 64 && ones_cnt <= length; ones_cnt++) {
+            if (depths[ones_cnt] != k - 1) continue;
+            long[][][] dp_table = new long[length + 1][ones_cnt + 1][2];
+            for (int cur_ones = 0; cur_ones <= ones_cnt; cur_ones++) {
+                for (int tight = 0; tight < 2; tight++) {
+                    dp_table[length][cur_ones][tight] = (cur_ones == ones_cnt ? 1L : 0L);
+                }
             }
+            for (int pos = length - 1; pos >= 0; pos--) {
+                for (int cur_ones = 0; cur_ones <= ones_cnt; cur_ones++) {
+                    for (int tight = 0; tight < 2; tight++) {
+                        int upper = (tight == 1 ? digits[pos] : 1);
+                        long total = 0L;
+                        for (int d = 0; d <= upper; d++) {
+                            int new_ones = cur_ones + d;
+                            if (new_ones > ones_cnt) continue;
+                            int new_tight = (tight == 1 && d == upper) ? 1 : 0;
+                            total += dp_table[pos + 1][new_ones][new_tight];
+                        }
+                        dp_table[pos][cur_ones][tight] = total;
+                    }
+                }
+            }
+            answer += dp_table[0][0][1];
         }
-        if (k == 1) {
-            answer -= 1L;
+        if (k == 1 && n >= 1) {
+            answer--;
         }
         return answer;
     }
