4ed3f5f9a98a304893f89de6b27945149a6ab3b0
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 00:55:06 +0800

    Batch add 1 solutions: 3548.equal-sum-grid-...

--- 3548.equal-sum-grid-partition-ii.java
+++ 3548.equal-sum-grid-partition-ii.java
@@ -5,124 +5,116 @@
 #
 
 # @lc code=start
+import java.util.*;
+
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        long total = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                total += grid[i][j];
-            }
-        }
-        long[] prefixRow = new long[m + 1];
+        long S = 0;
         long[] rowSum = new long[m];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                rowSum[i] += grid[i][j];
-            }
-            prefixRow[i + 1] = prefixRow[i] + rowSum[i];
-        }
-        long[] prefixCol = new long[n + 1];
         long[] colSum = new long[n];
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m; i++) {
-                colSum[j] += grid[i][j];
-            }
-            prefixCol[j + 1] = prefixCol[j] + colSum[j];
-        }
-        final int MAXV = 100000;
-        int[] minR = new int[MAXV + 1];
-        int[] maxR = new int[MAXV + 1];
-        int[] minC = new int[MAXV + 1];
-        int[] maxC = new int[MAXV + 1];
-        for (int v = 0; v <= MAXV; v++) {
-            minR[v] = m + 1;
-            maxR[v] = -1;
-            minC[v] = n + 1;
-            maxC[v] = -1;
-        }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int v = grid[i][j];
-                minR[v] = Math.min(minR[v], i);
-                maxR[v] = Math.max(maxR[v], i);
-                minC[v] = Math.min(minC[v], j);
-                maxC[v] = Math.max(maxC[v], j);
+                long val = grid[i][j];
+                S += val;
+                rowSum[i] += val;
+                colSum[j] += val;
+            }
+        }
+        long[] preRow = new long[m + 1];
+        for (int i = 1; i <= m; i++) {
+            preRow[i] = preRow[i - 1] + rowSum[i - 1];
+        }
+        long[] preCol = new long[n + 1];
+        for (int j = 1; j <= n; j++) {
+            preCol[j] = preCol[j - 1] + colSum[j - 1];
+        }
+        Map<Integer, Integer> minR = new HashMap<>();
+        Map<Integer, Integer> maxR = new HashMap<>();
+        Map<Integer, Integer> minC = new HashMap<>();
+        Map<Integer, Integer> maxC = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int val = grid[i][j];
+                minR.put(val, Math.min(minR.getOrDefault(val, m), i));
+                maxR.put(val, Math.max(maxR.getOrDefault(val, -1), i));
+                minC.put(val, Math.min(minC.getOrDefault(val, n), j));
+                maxC.put(val, Math.max(maxC.getOrDefault(val, -1), j));
             }
         }
         // Horizontal cuts
         for (int k = 1; k < m; k++) {
-            long sum1 = prefixRow[k];
-            long sum2 = total - sum1;
-            if (sum1 == sum2) return true;
-            // Discount from top
-            if (sum1 > sum2) {
-                long dif = sum1 - sum2;
-                if (dif > MAXV) continue;
-                int d = (int) dif;
-                int rr = k, cc = n;
+            long A = preRow[k];
+            long B = S - A;
+            if (A == B) return true;
+            long dv = Math.abs(A - B);
+            if (dv < 1 || dv > 100000) continue;
+            int v = (int) dv;
+            if (A > B) { // discount top
+                int rs = 0, re = k - 1, cs = 0, ce = n - 1;
+                int hh = k, ww = n;
+                if ((long) hh * ww <= 1) continue;
                 boolean ok = false;
-                if (rr >= 2 && cc >= 2) {
-                    if (minR[d] < k) ok = true;
-                } else if (rr == 1 && cc >= 2) {
-                    if (grid[0][0] == d || grid[0][n - 1] == d) ok = true;
-                } else if (cc == 1 && rr >= 2) {
-                    if (grid[0][0] == d || grid[k - 1][0] == d) ok = true;
+                if (hh >= 2 && ww >= 2) {
+                    int mr = minR.getOrDefault(v, m);
+                    if (mr <= re) ok = true;
+                } else if (hh == 1) {
+                    if (grid[rs][cs] == v || grid[rs][ce] == v) ok = true;
+                } else if (ww == 1) {
+                    if (grid[rs][cs] == v || grid[re][cs] == v) ok = true;
                 }
                 if (ok) return true;
-            }
-            // Discount from bot
-            if (sum2 > sum1) {
-                long dif = sum2 - sum1;
-                if (dif > MAXV) continue;
-                int d = (int) dif;
-                int rr = m - k, cc = n;
+            } else { // discount bottom
+                int rs = k, re = m - 1, cs = 0, ce = n - 1;
+                int hh = m - k, ww = n;
+                if ((long) hh * ww <= 1) continue;
                 boolean ok = false;
-                if (rr >= 2 && cc >= 2) {
-                    if (maxR[d] >= k) ok = true;
-                } else if (rr == 1 && cc >= 2) {
-                    if (grid[k][0] == d || grid[k][n - 1] == d) ok = true;
-                } else if (cc == 1 && rr >= 2) {
-                    if (grid[k][0] == d || grid[m - 1][0] == d) ok = true;
+                if (hh >= 2 && ww >= 2) {
+                    int mr = maxR.getOrDefault(v, -1);
+                    if (mr >= rs) ok = true;
+                } else if (hh == 1) {
+                    if (grid[rs][cs] == v || grid[rs][ce] == v) ok = true;
+                } else if (ww == 1) {
+                    if (grid[rs][cs] == v || grid[re][cs] == v) ok = true;
                 }
                 if (ok) return true;
             }
         }
         // Vertical cuts
         for (int k = 1; k < n; k++) {
-            long sum1 = prefixCol[k];
-            long sum2 = total - sum1;
-            if (sum1 == sum2) return true;
-            // Discount from left
-            if (sum1 > sum2) {
-                long dif = sum1 - sum2;
-                if (dif > MAXV) continue;
-                int d = (int) dif;
-                int rr = m, cc = k;
+            long A = preCol[k];
+            long B = S - A;
+            if (A == B) return true;
+            long dv = Math.abs(A - B);
+            if (dv < 1 || dv > 100000) continue;
+            int v = (int) dv;
+            if (A > B) { // discount left
+                int rs = 0, re = m - 1, cs = 0, ce = k - 1;
+                int hh = m, ww = k;
+                if ((long) hh * ww <= 1) continue;
                 boolean ok = false;
-                if (rr >= 2 && cc >= 2) {
-                    if (minC[d] < k) ok = true;
-                } else if (rr == 1 && cc >= 2) {
-                    if (grid[0][0] == d || grid[0][k - 1] == d) ok = true;
-                } else if (cc == 1 && rr >= 2) {
-                    if (grid[0][0] == d || grid[m - 1][0] == d) ok = true;
+                if (hh >= 2 && ww >= 2) {
+                    int mc = minC.getOrDefault(v, n);
+                    if (mc <= ce) ok = true;
+                } else if (hh == 1) {
+                    if (grid[rs][cs] == v || grid[rs][ce] == v) ok = true;
+                } else if (ww == 1) {
+                    if (grid[rs][cs] == v || grid[re][cs] == v) ok = true;
                 }
                 if (ok) return true;
-            }
-            // Discount from right
-            if (sum2 > sum1) {
-                long dif = sum2 - sum1;
-                if (dif > MAXV) continue;
-                int d = (int) dif;
-                int rr = m, cc = n - k;
+            } else { // discount right
+                int rs = 0, re = m - 1, cs = k, ce = n - 1;
+                int hh = m, ww = n - k;
+                if ((long) hh * ww <= 1) continue;
                 boolean ok = false;
-                if (rr >= 2 && cc >= 2) {
-                    if (maxC[d] >= k) ok = true;
-                } else if (rr == 1 && cc >= 2) {
-                    if (grid[0][k] == d || grid[0][n - 1] == d) ok = true;
-                } else if (cc == 1 && rr >= 2) {
-                    if (grid[0][k] == d || grid[m - 1][k] == d) ok = true;
+                if (hh >= 2 && ww >= 2) {
+                    int mc = maxC.getOrDefault(v, -1);
+                    if (mc >= cs) ok = true;
+                } else if (hh == 1) {
+                    if (grid[rs][cs] == v || grid[rs][ce] == v) ok = true;
+                } else if (ww == 1) {
+                    if (grid[rs][cs] == v || grid[re][cs] == v) ok = true;
                 }
                 if (ok) return true;
             }
