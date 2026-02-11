b3acb3e1c7ebbb8fbab009d46176c01e752e8766
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 02:05:29 +0800

    Batch add 1 solutions: 3605.minimum-stabili...

--- 3605.minimum-stability-factor-of-array.java
+++ 3605.minimum-stability-factor-of-array.java
@@ -6,68 +6,62 @@
 
 # @lc code=start
 class Solution {
+    private static int gcd(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
     public int minStable(int[] nums, int maxC) {
         int n = nums.length;
         if (n == 0) return 0;
         int[] logg = new int[n + 1];
-        logg[1] = 0;
-        for (int i = 2; i <= n; i++) {
-            logg[i] = logg[i / 2] + 1;
+        for (int i = 1; i <= n; i++) {
+            logg[i] = (i == 1) ? 0 : logg[i / 2] + 1;
         }
         int LOG = logg[n] + 1;
         int[][] st = new int[LOG][n];
-        java.util.function.IntBinaryOperator mygcd = (a, b) -> {
-            a = Math.abs(a);
-            b = Math.abs(b);
-            while (b != 0) {
-                int t = b;
-                b = a % b;
-                a = t;
-            }
-            return a;
-        };
         for (int i = 0; i < n; i++) {
             st[0][i] = nums[i];
         }
         for (int j = 1; j < LOG; j++) {
             for (int i = 0; i + (1 << j) <= n; i++) {
-                st[j][i] = mygcd.applyAsInt(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
+                st[j][i] = gcd(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
             }
         }
         int left = 0, right = n;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            int K = mid + 1;
-            boolean can = false;
-            if (K > n) {
-                can = true;
-            } else {
-                java.util.ArrayList<Integer> bad = new java.util.ArrayList<>();
-                int kk = logg[K];
-                int len_shift = (1 << kk);
-                for (int i = 0; i <= n - K; i++) {
-                    int g = mygcd.applyAsInt(st[kk][i], st[kk][i + K - len_shift]);
-                    if (g >= 2) {
-                        bad.add(i);
-                    }
-                }
-                int m = bad.size();
-                if (m == 0) {
-                    can = true;
-                } else {
-                    int hits = 0;
-                    int j = 0;
-                    while (j < m) {
-                        int pick_pos = bad.get(j) + K - 1;
-                        hits++;
-                        while (j < m && bad.get(j) <= pick_pos) {
-                            j++;
+            int W = mid + 1;
+            int changes = 0;
+            int last = -1;
+            boolean canDo = true;
+            if (W <= n) {
+                int len = W;
+                int k = logg[len];
+                int pw = 1 << k;
+                for (int i = 0; i <= n - W; i++) {
+                    int g1 = st[k][i];
+                    int g2 = st[k][i + W - pw];
+                    int g = gcd(g1, g2);
+                    if (g > 1) {
+                        if (last < i) {
+                            changes++;
+                            last = i + W - 1;
+                            if (changes > maxC) {
+                                canDo = false;
+                                break;
+                            }
                         }
                     }
-                    can = hits <= maxC;
                 }
             }
-            if (can) {
+            if (canDo && changes <= maxC) {
                 right = mid;
             } else {
                 left = mid + 1;
