a0fb7ed4377ea921773f8bba615a6605a5b09f7f
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 16:42:35 +0800

    Batch add 1 solutions: 3513.number-of-uniqu...

--- 3513.number-of-unique-xor-triplets-i.java
+++ 3513.number-of-unique-xor-triplets-i.java
@@ -1,1 +1,17 @@
-T
+#
+# @lc app=leetcode id=3513 lang=java
+#
+# [3513] Number of Unique XOR Triplets I
+#
+
+# @lc code=start
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int log = 31 - Integer.numberOfLeadingZeros(n);
+        return 1 << (log + 1);
+    }
+}
+# @lc code=end
