0339a35229a45dd7cdb0f7b27ef4528f6762f403
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 18:32:08 +0800

    Batch add 1 solutions: 3605.minimum-stabili...

--- 3605.minimum-stability-factor-of-array.java
+++ 3605.minimum-stability-factor-of-array.java
@@ -3,75 +3,77 @@
 #
 # [3605] Minimum Stability Factor of Array
 #
-# @lc code=start
-import java.util.*;
 
+# @lc code=start
 class Solution {
     public int minStable(int[] nums, int maxC) {
         int n = nums.length;
-        
-        // Binary search on the answer
+        if (n == 0) return 0;
+        int[] logg = new int[n + 1];
+        logg[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            logg[i] = logg[i / 2] + 1;
+        }
+        int LOG = logg[n] + 1;
+        int[][] st = new int[LOG][n];
+        java.util.function.IntBinaryOperator mygcd = (a, b) -> {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            while (b != 0) {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        };
+        for (int i = 0; i < n; i++) {
+            st[0][i] = nums[i];
+        }
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 0; i + (1 << j) <= n; i++) {
+                st[j][i] = mygcd.applyAsInt(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
+            }
+        }
         int left = 0, right = n;
-        
         while (left < right) {
-            int mid = (left + right) / 2;
-            if (canAchieve(nums, maxC, mid)) {
+            int mid = left + (right - left) / 2;
+            int K = mid + 1;
+            boolean can = false;
+            if (K > n) {
+                can = true;
+            } else {
+                java.util.ArrayList<Integer> bad = new java.util.ArrayList<>();
+                int kk = logg[K];
+                int len_shift = (1 << kk);
+                for (int i = 0; i <= n - K; i++) {
+                    int g = mygcd.applyAsInt(st[kk][i], st[kk][i + K - len_shift]);
+                    if (g >= 2) {
+                        bad.add(i);
+                    }
+                }
+                int m = bad.size();
+                if (m == 0) {
+                    can = true;
+                } else {
+                    int hits = 0;
+                    int j = 0;
+                    while (j < m) {
+                        int pick_pos = bad.get(j) + K - 1;
+                        hits++;
+                        while (j < m && bad.get(j) <= pick_pos) {
+                            j++;
+                        }
+                    }
+                    can = hits <= maxC;
+                }
+            }
+            if (can) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
-        
         return left;
-    }
-    
-    private boolean canAchieve(int[] nums, int maxC, int target) {
-        List<int[]> intervals = findMaximalStableIntervals(nums);
-        int mods = 0;
-        
-        for (int[] interval : intervals) {
-            int len = interval[1] - interval[0] + 1;
-            if (len > target) {
-                mods += len / (target + 1);
-            }
-        }
-        
-        return mods <= maxC;
-    }
-    
-    private List<int[]> findMaximalStableIntervals(int[] nums) {
-        List<int[]> intervals = new ArrayList<>();
-        int n = nums.length;
-        int i = 0;
-        
-        while (i < n) {
-            if (nums[i] < 2) {
-                i++;
-                continue;
-            }
-            
-            int j = i;
-            int gcd = nums[i];
-            
-            while (j + 1 < n && gcd(gcd, nums[j + 1]) >= 2) {
-                j++;
-                gcd = gcd(gcd, nums[j]);
-            }
-            
-            intervals.add(new int[]{i, j});
-            i = j + 1;
-        }
-        
-        return intervals;
-    }
-    
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
     }
 }
 # @lc code=end
