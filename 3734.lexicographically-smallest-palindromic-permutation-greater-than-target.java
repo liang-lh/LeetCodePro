a4a6ed14d5fc25a65623926ec67e997e8652c1c2
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 03:15:07 +0800

    Batch add 1 solutions: 3734.lexicographical...

--- 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.java
+++ 3734.lexicographically-smallest-palindromic-permutation-greater-than-target.java
@@ -6,113 +6,131 @@
 
 # @lc code=start
 class Solution {
-    public String lexPalindromicPermutation(String s, String target) {
-        int n = s.length();
-        int[] total = new int[26];
-        for (char ch : s.toCharArray()) {
-            total[ch - 'a']++;
-        }
-        int leftEnd = (n - 1) / 2;
-        for (int k = n - 1; k >= 0; k--) {
-            char[] res = new char[n];
-            boolean validPrefix = true;
-            for (int j = 0; j < k; j++) {
-                char ch = target.charAt(j);
-                int mir = n - 1 - j;
-                if (res[j] != 0 && res[j] != ch) {
-                    validPrefix = false;
-                    break;
-                }
-                res[j] = ch;
-                if (res[mir] != 0 && res[mir] != ch) {
-                    validPrefix = false;
-                    break;
-                }
-                res[mir] = ch;
-            }
-            if (!validPrefix) continue;
-            int[] cnt = new int[26];
-            System.arraycopy(total, 0, cnt, 0, 26);
-            boolean feasible = true;
-            for (int i = 0; i < n; i++) {
-                if (res[i] != 0) {
-                    int idx = res[i] - 'a';
-                    cnt[idx]--;
-                    if (cnt[idx] < 0) {
-                        feasible = false;
-                        break;
-                    }
-                }
-            }
-            if (!feasible) continue;
-            int mirK = n - 1 - k;
-            char tgtK = target.charAt(k);
-            char chosenC = 0;
-            boolean mirrorAlreadySet = (mirK < k);
-            int deduct = 1;
-            if (mirK > k) deduct = 2;
-            if (mirrorAlreadySet) {
-                char forced = res[mirK];
-                int idx = forced - 'a';
-                if (forced > tgtK && cnt[idx] >= 1) {
-                    chosenC = forced;
-                }
-            } else {
-                for (char c = (char)(tgtK + 1); c <= 'z'; c++) {
-                    int idx = c - 'a';
-                    if (cnt[idx] >= deduct) {
-                        chosenC = c;
-                        break;
-                    }
-                }
-            }
-            if (chosenC == 0) continue;
-            res[k] = chosenC;
-            if (mirK != k) {
-                res[mirK] = chosenC;
-            }
-            cnt[chosenC - 'a'] -= deduct;
-            boolean canFill = true;
-            for (int i = k + 1; i <= leftEnd; i++) {
-                if (res[i] != 0) continue;
-                int mirI = n - 1 - i;
-                int need = (mirI == i ? 1 : 2);
-                char d = 0;
-                for (char cc = 'a'; cc <= 'z'; cc++) {
-                    int idx = cc - 'a';
-                    if (cnt[idx] >= need) {
-                        d = cc;
-                        break;
-                    }
-                }
-                if (d == 0) {
-                    canFill = false;
-                    break;
-                }
-                res[i] = d;
-                res[mirI] = d;
-                cnt[d - 'a'] -= need;
-            }
-            if (!canFill) continue;
-            feasible = true;
-            for (int i = 0; i < n; i++) {
-                if (res[i] == 0) {
-                    feasible = false;
-                    break;
-                }
-            }
-            if (!feasible) continue;
-            for (int i = 0; i < 26; i++) {
-                if (cnt[i] != 0) {
-                    feasible = false;
-                    break;
-                }
-            }
-            if (feasible) {
-                return new String(res);
-            }
-        }
-        return "";
+  public String lexPalindromicPermutation(String s, String target) {
+    int n = s.length();
+    int[] freq = new int[26];
+    for (char ch : s.toCharArray()) {
+      freq[ch - 'a']++;
     }
+    int odd_cnt = 0;
+    char mid_c = 0;
+    for (int i = 0; i < 26; i++) {
+      if (freq[i] % 2 == 1) {
+        odd_cnt++;
+        mid_c = (char) (i + 'a');
+      }
+    }
+    if (odd_cnt != (n % 2)) {
+      return "";
+    }
+    int[] pair_cnt = new int[26];
+    for (int i = 0; i < 26; i++) {
+      pair_cnt[i] = freq[i] / 2;
+    }
+    int m = n / 2;
+    // Check equal left case
+    String tgt_left = target.substring(0, m);
+    int[] used = new int[26];
+    for (int j = 0; j < m; j++) {
+      used[tgt_left.charAt(j) - 'a']++;
+    }
+    boolean eq_possible = true;
+    for (int i = 0; i < 26; i++) {
+      if (used[i] != pair_cnt[i]) {
+        eq_possible = false;
+        break;
+      }
+    }
+    if (eq_possible) {
+      StringBuilder sb = new StringBuilder(tgt_left);
+      if (n % 2 == 1) {
+        sb.append(mid_c);
+      }
+      String rev = new StringBuilder(tgt_left).reverse().toString();
+      sb.append(rev);
+      String candidate = sb.toString();
+      if (candidate.compareTo(target) > 0) {
+        return candidate;
+      }
+    }
+    // Next greater left
+    String ref = tgt_left;
+    int len_ = m;
+    int pivot = -1;
+    for (int kk = len_ - 1; kk >= 0; kk--) {
+      int[] temp_rem = new int[26];
+      for (int ii = 0; ii < 26; ii++) {
+        temp_rem[ii] = pair_cnt[ii];
+      }
+      boolean ok = true;
+      for (int j = 0; j < kk; j++) {
+        int id = ref.charAt(j) - 'a';
+        if (temp_rem[id] == 0) {
+          ok = false;
+          break;
+        }
+        temp_rem[id]--;
+      }
+      if (!ok) {
+        continue;
+      }
+      char tc = ref.charAt(kk);
+      boolean can_inc = false;
+      for (char ch = (char) (tc + 1); ch <= 'z'; ch++) {
+        int id = ch - 'a';
+        if (temp_rem[id] > 0) {
+          can_inc = true;
+          break;
+        }
+      }
+      if (can_inc) {
+        pivot = kk;
+        break;
+      }
+    }
+    if (pivot == -1) {
+      return "";
+    }
+    // Build left
+    StringBuilder left_sb = new StringBuilder();
+    int[] rrem = new int[26];
+    for (int ii = 0; ii < 26; ii++) {
+      rrem[ii] = pair_cnt[ii];
+    }
+    for (int j = 0; j < pivot; j++) {
+      char chh = ref.charAt(j);
+      left_sb.append(chh);
+      rrem[chh - 'a']--;
+    }
+    char tcc = ref.charAt(pivot);
+    char inc_chh = 0;
+    for (char ch = (char) (tcc + 1); ch <= 'z'; ch++) {
+      if (rrem[ch - 'a'] > 0) {
+        inc_chh = ch;
+        break;
+      }
+    }
+    left_sb.append(inc_chh);
+    rrem[inc_chh - 'a']--;
+    for (int j = pivot + 1; j < m; j++) {
+      char sm_ch = 0;
+      for (char ch = 'a'; ch <= 'z'; ch++) {
+        if (rrem[ch - 'a'] > 0) {
+          sm_ch = ch;
+          break;
+        }
+      }
+      left_sb.append(sm_ch);
+      rrem[sm_ch - 'a']--;
+    }
+    String leftt = left_sb.toString();
+    StringBuilder full_sb = new StringBuilder(leftt);
+    if (n % 2 == 1) {
+      full_sb.append(mid_c);
+    }
+    String reev = new StringBuilder(leftt).reverse().toString();
+    full_sb.append(reev);
+    return full_sb.toString();
+  }
 }
 # @lc code=end
