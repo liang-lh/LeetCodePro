5a3760a2390d856be9eb908fb984cf5ed8b3d845
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 15:01:20 +0800

    Batch add 1 solutions: 3458.select-k-disjoi...

--- 3458.select-k-disjoint-special-substrings.java
+++ 3458.select-k-disjoint-special-substrings.java
@@ -3,64 +3,50 @@
 #
 # [3458] Select K Disjoint Special Substrings
 #
+
 # @lc code=start
 import java.util.*;
 
 class Solution {
     public boolean maxSubstringLength(String s, int k) {
         int n = s.length();
-        if (k == 0) return true;
-        
-        // Find first and last occurrence of each character
         int[] first = new int[26];
-        int[] last = new int[26];
-        Arrays.fill(first, n);
-        Arrays.fill(last, -1);
-        
+        for (int i = 0; i < 26; i++) first[i] = n;
+        int[] lastPos = new int[26];
+        for (int i = 0; i < 26; i++) lastPos[i] = -1;
         for (int i = 0; i < n; i++) {
             int c = s.charAt(i) - 'a';
             first[c] = Math.min(first[c], i);
-            last[c] = i;
+            lastPos[c] = Math.max(lastPos[c], i);
         }
-        
-        // Find all special substrings
-        List<int[]> intervals = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            int minFirst = i, maxLast = i;
-            boolean canExtend = true;
-            
-            for (int j = i; j < n && canExtend; j++) {
-                int c = s.charAt(j) - 'a';
-                minFirst = Math.min(minFirst, first[c]);
-                maxLast = Math.max(maxLast, last[c]);
-                
-                // Check if [i, j] is special
-                if (minFirst == i && maxLast == j && (i != 0 || j != n - 1)) {
-                    intervals.add(new int[]{i, j});
+        List<int[]> cands = new ArrayList<>();
+        for (int c = 0; c < 26; c++) {
+            if (first[c] < n) {
+                int st = first[c];
+                int en = lastPos[c];
+                boolean valid = true;
+                for (int j = st; j <= en; j++) {
+                    int ch = s.charAt(j) - 'a';
+                    if (first[ch] < st || lastPos[ch] > en) {
+                        valid = false;
+                        break;
+                    }
                 }
-                
-                // If minFirst < i, can't form special substring starting from i
-                if (minFirst < i) {
-                    canExtend = false;
+                if (valid && (st > 0 || en < n - 1)) {
+                    cands.add(new int[]{st, en});
                 }
             }
         }
-        
-        // Find maximum number of non-overlapping intervals using greedy algorithm
-        intervals.sort((a, b) -> Integer.compare(a[1], b[1]));
-        
-        int count = 0;
-        int lastEnd = -1;
-        
-        for (int[] interval : intervals) {
-            if (interval[0] > lastEnd) {
-                count++;
-                lastEnd = interval[1];
+        Collections.sort(cands, (a, b) -> Integer.compare(a[1], b[1]));
+        int cnt = 0;
+        int prevEnd = -1;
+        for (int[] iv : cands) {
+            if (iv[0] > prevEnd) {
+                cnt++;
+                prevEnd = iv[1];
             }
         }
-        
-        return count >= k;
+        return cnt >= k;
     }
 }
 # @lc code=end
