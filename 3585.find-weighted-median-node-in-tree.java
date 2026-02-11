cf76c62a621cea6e921aabaf8de649960d1502ec
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 01:49:45 +0800

    Batch add 1 solutions: 3585.find-weighted-m...

--- 3585.find-weighted-median-node-in-tree.java
+++ 3585.find-weighted-median-node-in-tree.java
@@ -6,126 +6,139 @@
 
 # @lc code=start
 class Solution {
-    public int[] findMedian(int n, int[][] edges, int[][] queries) {
-        java.util.List<int[]>[] adj = new java.util.ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new java.util.ArrayList<int[]>();
-        }
-        for (int[] e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            adj[u].add(new int[]{v, w});
-            adj[v].add(new int[]{u, w});
-        }
-        final int LOG = 18;
-        long[] depth = new long[n];
-        int[] level = new int[n];
-        int[][] par = new int[n][LOG];
-        long[][] updist = new long[n][LOG];
-        for (int i = 0; i < n; i++) {
-            java.util.Arrays.fill(par[i], -1);
-        }
-        // BFS for depth, level, par[0], updist[0]
-        boolean[] vis = new boolean[n];
-        java.util.Queue<Integer> q = new java.util.LinkedList<>();
-        vis[0] = true;
-        depth[0] = 0;
-        level[0] = 0;
-        par[0][0] = -1;
-        updist[0][0] = 0;
-        q.offer(0);
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            for (int[] pr : adj[node]) {
-                int v = pr[0];
-                int w = pr[1];
-                if (!vis[v]) {
-                    vis[v] = true;
-                    par[v][0] = node;
-                    updist[v][0] = w;
-                    depth[v] = depth[node] + w;
-                    level[v] = level[node] + 1;
-                    q.offer(v);
-                }
-            }
-        }
-        // Fill lifting tables
-        for (int k = 1; k < LOG; k++) {
-            for (int i = 0; i < n; i++) {
-                int a = par[i][k - 1];
-                if (a != -1) {
-                    par[i][k] = par[a][k - 1];
-                    updist[i][k] = updist[i][k - 1] + updist[a][k - 1];
-                } else {
-                    par[i][k] = -1;
-                    updist[i][k] = 0;
-                }
-            }
-        }
-        int[] ans = new int[queries.length];
-        for (int j = 0; j < queries.length; j++) {
-            int u = queries[j][0];
-            int v = queries[j][1];
-            if (u == v) {
-                ans[j] = u;
-                continue;
-            }
-            // Compute LCA using levels
-            int aa = u, bb = v;
-            if (level[aa] > level[bb]) {
-                int t = aa;
-                aa = bb;
-                bb = t;
-            }
-            // Lift bb to level[aa]
-            for (int k = LOG - 1; k >= 0; k--) {
-                if (par[bb][k] != -1 && level[par[bb][k]] >= level[aa]) {
-                    bb = par[bb][k];
-                }
-            }
-            int lca_;
-            if (aa == bb) {
-                lca_ = aa;
-            } else {
-                for (int k = LOG - 1; k >= 0; k--) {
-                    if (par[aa][k] != -1 && par[bb][k] != -1 && par[aa][k] != par[bb][k]) {
-                        aa = par[aa][k];
-                        bb = par[bb][k];
-                    }
-                }
-                lca_ = par[aa][0];
-            }
-            long total = depth[u] + depth[v] - 2L * depth[lca_];
-            long targ = (total + 1L) / 2L;
-            long distul = depth[u] - depth[lca_];
-            int med;
-            if (distul >= targ) {
-                long maxd = targ - 1L;
-                int z = u;
-                long remd = maxd;
-                for (int k = LOG - 1; k >= 0; k--) {
-                    if (par[z][k] != -1 && updist[z][k] <= remd) {
-                        remd -= updist[z][k];
-                        z = par[z][k];
-                    }
-                }
-                med = par[z][0];
-            } else {
-                long distlv = depth[v] - depth[lca_];
-                long rem = targ - distul;
-                long maxupv = distlv - rem;
-                int y = v;
-                long remd = maxupv;
-                for (int k = LOG - 1; k >= 0; k--) {
-                    if (par[y][k] != -1 && updist[y][k] <= remd) {
-                        remd -= updist[y][k];
-                        y = par[y][k];
-                    }
-                }
-                med = y;
-            }
-            ans[j] = med;
-        }
-        return ans;
+  public int[] findMedian(int n, int[][] edges, int[][] queries) {
+    java.util.List<int[]>[] adj = new java.util.ArrayList[n];
+    for (int i = 0; i < n; i++) {
+      adj[i] = new java.util.ArrayList<>();
     }
+    for (int[] edge : edges) {
+      int u = edge[0], v = edge[1], w = edge[2];
+      adj[u].add(new int[]{v, w});
+      adj[v].add(new int[]{u, w});
+    }
+    // BFS to compute dist, dep, parent
+    long[] dist = new long[n];
+    int[] dep = new int[n];
+    int[] parent = new int[n];
+    java.util.Arrays.fill(parent, -1);
+    boolean[] visited = new boolean[n];
+    java.util.Queue<Integer> queue = new java.util.LinkedList<>();
+    queue.offer(0);
+    visited[0] = true;
+    dist[0] = 0;
+    dep[0] = 0;
+    while (!queue.isEmpty()) {
+      int u = queue.poll();
+      for (int[] p : adj[u]) {
+        int v = p[0];
+        int w = p[1];
+        if (!visited[v]) {
+          visited[v] = true;
+          parent[v] = u;
+          dist[v] = dist[u] + w;
+          dep[v] = dep[u] + 1;
+          queue.offer(v);
+        }
+      }
+    }
+    // Binary lifting
+    final int LOG = 20;
+    int[][] up = new int[LOG][n];
+    long[][] distup = new long[LOG][n];
+    for (int i = 0; i < n; i++) {
+      up[0][i] = parent[i];
+      if (parent[i] != -1) {
+        distup[0][i] = dist[i] - dist[parent[i]];
+      }
+    }
+    for (int k = 1; k < LOG; k++) {
+      for (int i = 0; i < n; i++) {
+        up[k][i] = -1;
+        distup[k][i] = 0;
+        int mid = up[k - 1][i];
+        if (mid != -1) {
+          int p = up[k - 1][mid];
+          if (p != -1) {
+            up[k][i] = p;
+            distup[k][i] = distup[k - 1][i] + distup[k - 1][mid];
+          }
+        }
+      }
+    }
+    // Process queries
+    int[] answer = new int[queries.length];
+    for (int qi = 0; qi < queries.length; qi++) {
+      int uu = queries[qi][0];
+      int vv = queries[qi][1];
+      if (uu == vv) {
+        answer[qi] = uu;
+        continue;
+      }
+      // Compute LCA
+      int a = uu, b = vv;
+      if (dep[a] > dep[b]) {
+        int temp = a;
+        a = b;
+        b = temp;
+      }
+      int diff = dep[b] - dep[a];
+      for (int k = 0; k < LOG; k++) {
+        if ((diff & (1 << k)) != 0) {
+          b = up[k][b];
+        }
+      }
+      int lca;
+      if (a == b) {
+        lca = a;
+      } else {
+        for (int k = LOG - 1; k >= 0; k--) {
+          if (up[k][a] != up[k][b] && up[k][a] != -1 && up[k][b] != -1) {
+            a = up[k][a];
+            b = up[k][b];
+          }
+        }
+        lca = up[0][a];
+      }
+      long du = dist[uu] - dist[lca];
+      long dv = dist[vv] - dist[lca];
+      long tot = du + dv;
+      long tar = (tot + 1L) / 2;
+      int med;
+      if (du >= tar) {
+        // lift_first_ge from uu to lca
+        int cur = uu;
+        long cd = 0;
+        for (int k = LOG - 1; k >= 0; k--) {
+          int p = up[k][cur];
+          if (p != -1 && dep[p] >= dep[lca] && cd + distup[k][cur] < tar) {
+            cd += distup[k][cur];
+            cur = p;
+          }
+        }
+        int nxt = up[0][cur];
+        if (nxt != -1 && dep[nxt] >= dep[lca] && cd + distup[0][cur] >= tar) {
+          med = nxt;
+        } else {
+          med = lca;
+        }
+      } else {
+        long rem = tar - du;
+        long maxfv = dv - rem;
+        // lift_max_le from vv to lca
+        int cur = vv;
+        long cd = 0;
+        for (int k = LOG - 1; k >= 0; k--) {
+          int p = up[k][cur];
+          if (p != -1 && dep[p] >= dep[lca] && cd + distup[k][cur] <= maxfv) {
+            cd += distup[k][cur];
+            cur = p;
+          }
+        }
+        med = cur;
+      }
+      answer[qi] = med;
+    }
+    return answer;
+  }
 }
 # @lc code=end
