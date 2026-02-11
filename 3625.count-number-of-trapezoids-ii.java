8c14580fba178cc281c1406dd4b11070362ac7be
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 02:16:21 +0800

    Batch add 1 solutions: 3625.count-number-of...

--- 3625.count-number-of-trapezoids-ii.java
+++ 3625.count-number-of-trapezoids-ii.java
@@ -6,117 +6,68 @@
 
 # @lc code=start
 class Solution {
-    private static class Slope {
-        int dx, dy;
-
-        Slope(int ddx, int ddy) {
-            int adx = Math.abs(ddx);
-            int ady = Math.abs(ddy);
-            int g = gcd(adx, ady);
-            dx = ddx / g;
-            dy = ddy / g;
-            if (dx < 0 || (dx == 0 && dy < 0)) {
-                dx = -dx;
-                dy = -dy;
-            }
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Slope other = (Slope) obj;
-            return dx == other.dx && dy == other.dy;
-        }
-
-        @Override
-        public int hashCode() {
-            return 31 * dx + dy;
-        }
-    }
-
-    private static int gcd(int a, int b) {
-        while (b != 0) {
-            int t = b;
-            b = a % b;
-            a = t;
-        }
-        return a;
-    }
-
-    private static long getKey(int x, int y) {
-        final int OFFSET = 1005;
-        final long MUL = 2005L;
-        return ((long) x + OFFSET) * MUL + (y + OFFSET);
-    }
-
     public int countTrapezoids(int[][] points) {
         int n = points.length;
-        java.util.Set<Slope> slopeSet = new java.util.HashSet<>();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int dxp = points[j][0] - points[i][0];
-                int dyp = points[j][1] - points[i][1];
-                slopeSet.add(new Slope(dxp, dyp));
+        java.util.Map<Long, Integer> midCount = new java.util.HashMap<>();
+        java.util.Set<String> slopeSet = new java.util.HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // midpoint for parallelograms
+                long mx = (long) points[i][0] + points[j][0];
+                long my = (long) points[i][1] + points[j][1];
+                long mkey = (mx + 2001L) * 100000L + (my + 2001L);
+                midCount.put(mkey, midCount.getOrDefault(mkey, 0) + 1);
+                // slope
+                int dx0 = points[j][0] - points[i][0];
+                int dy0 = points[j][1] - points[i][1];
+                int absdx = Math.abs(dx0);
+                int absdy = Math.abs(dy0);
+                int a = absdx;
+                int b = absdy;
+                while (b != 0) {
+                    int t = b;
+                    b = a % b;
+                    a = t;
+                }
+                int g = a;
+                if (g == 0) continue; // impossible
+                int dx = dx0 / g;
+                int dy = dy0 / g;
+                if (dx < 0 || (dx == 0 && dy < 0)) {
+                    dx = -dx;
+                    dy = -dy;
+                }
+                String skey = dx + "," + dy;
+                slopeSet.add(skey);
             }
         }
-
-        long total = 0;
-        for (Slope s : slopeSet) {
-            int dx = s.dx;
-            int dy = s.dy;
-            java.util.Map<Long, Integer> lineCnt = new java.util.HashMap<>();
-            for (int p = 0; p < n; ++p) {
-                long lkey = (long) dx * points[p][1] - (long) dy * points[p][0];
-                lineCnt.merge(lkey, 1, Integer::sum);
+        long para = 0;
+        for (int cnt : midCount.values()) {
+            para += (long) cnt * (cnt - 1) / 2;
+        }
+        long S = 0;
+        for (String sk : slopeSet) {
+            String[] parts = sk.split(",");
+            int dx = Integer.parseInt(parts[0]);
+            int dy = Integer.parseInt(parts[1]);
+            java.util.Map<Long, Integer> lineCount = new java.util.HashMap<>();
+            for (int k = 0; k < n; k++) {
+                long lid = (long) dx * points[k][1] - (long) dy * points[k][0];
+                lineCount.put(lid, lineCount.getOrDefault(lid, 0) + 1);
             }
             long sumC = 0;
             long sumCsq = 0;
-            for (int c : lineCnt.values()) {
-                if (c < 2) continue;
-                long cc = (long) c * (c - 1) / 2;
-                sumC += cc;
-                sumCsq += cc * cc;
-            }
-            long contrib = (sumC * sumC - sumCsq) / 2;
-            total += contrib;
-        }
-
-        // Count parallelograms
-        java.util.Map<Long, Integer> ptIdx = new java.util.HashMap<>();
-        for (int i = 0; i < n; ++i) {
-            long key = getKey(points[i][0], points[i][1]);
-            ptIdx.put(key, i);
-        }
-
-        long paraTimes = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (j == i) continue;
-                long vx = points[j][0] - points[i][0];
-                long vy = points[j][1] - points[i][1];
-                for (int k = 0; k < n; ++k) {
-                    if (k == i || k == j) continue;
-                    long wx = points[k][0] - points[i][0];
-                    long wy = points[k][1] - points[i][1];
-                    long cross = vx * wy - vy * wx;
-                    if (cross == 0) continue;
-                    int dx = points[j][0] + points[k][0] - points[i][0];
-                    int dy_ = points[j][1] + points[k][1] - points[i][1];
-                    long dkey = getKey(dx, dy_);
-                    Integer lobj = ptIdx.get(dkey);
-                    if (lobj != null) {
-                        int l = lobj;
-                        if (l != i && l != j && l != k) {
-                            ++paraTimes;
-                        }
-                    }
+            for (int ct : lineCount.values()) {
+                if (ct >= 2) {
+                    long c = (long) ct * (ct - 1) / 2;
+                    sumC += c;
+                    sumCsq += c * c;
                 }
             }
+            long contrib = (sumC * sumC - sumCsq) / 2;
+            S += contrib;
         }
-        long numPara = paraTimes / 8;
-
-        return (int) (total - numPara);
+        return (int) (S - para);
     }
 }
 # @lc code=end
