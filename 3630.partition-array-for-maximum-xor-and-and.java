f4f6267f3ce1b5eb7081a472d7b30e9b333c4bad
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 18:48:45 +0800

    Batch add 1 solutions: 3630.partition-array...

--- 3630.partition-array-for-maximum-xor-and-and.java
+++ 3630.partition-array-for-maximum-xor-and-and.java
@@ -3,50 +3,64 @@
 #
 # [3630] Partition Array for Maximum XOR and AND
 #
+
 # @lc code=start
 class Solution {
     public long maximizeXorAndXor(int[] nums) {
         int n = nums.length;
-        long maxValue = 0;
-        
-        // Total number of partitions: 3^n
-        int totalPartitions = (int) Math.pow(3, n);
-        
-        for (int partition = 0; partition < totalPartitions; partition++) {
-            int xorA = 0;
-            int andB = 0;
-            int xorC = 0;
-            boolean hasBElements = false;
-            int tempAndB = -1; // All bits set initially
-            
-            int temp = partition;
-            for (int i = 0; i < n; i++) {
-                int assignment = temp % 3;
-                temp /= 3;
-                
-                if (assignment == 0) { // Assign to A
-                    xorA ^= nums[i];
-                } else if (assignment == 1) { // Assign to B
-                    if (!hasBElements) {
-                        tempAndB = nums[i];
-                        hasBElements = true;
-                    } else {
-                        tempAndB &= nums[i];
-                    }
-                } else { // Assign to C (assignment == 2)
-                    xorC ^= nums[i];
+        int N = 1 << n;
+        int full = N - 1;
+        long[] sub_xor = new long[N];
+        long[] sub_and = new long[N];
+        for (int i = 0; i < N; i++) {
+            sub_and[i] = -1L;
+        }
+        sub_and[0] = 0L;
+        for (int i = 0; i < n; i++) {
+            long val = nums[i];
+            for (int sub = 0; sub < N; sub++) {
+                if ((sub & (1 << i)) != 0) {
+                    sub_xor[sub] ^= val;
+                    sub_and[sub] &= val;
                 }
             }
-            
-            if (hasBElements) {
-                andB = tempAndB;
-            }
-            
-            long value = (long)xorA + (long)andB + (long)xorC;
-            maxValue = Math.max(maxValue, value);
         }
-        
-        return maxValue;
+        long ans = 0;
+        long LOW32 = 0xFFFFFFFFL;
+        for (int maskB = 0; maskB < N; maskB++) {
+            long andB = sub_and[maskB];
+            int maskS = full ^ maskB;
+            long xorS = sub_xor[maskS];
+            // Build basis
+            long[] basis = new long[32];
+            for (int j = 0; j < n; j++) {
+                if ((maskS & (1 << j)) == 0) continue;
+                long val = nums[j];
+                for (int p = 31; p >= 0; p--) {
+                    if ((val & (1L << p)) == 0) continue;
+                    if (basis[p] != 0) {
+                        val ^= basis[p];
+                    } else {
+                        basis[p] = val;
+                        break;
+                    }
+                }
+            }
+            // Greedy max proj = max (x & m)
+            long m = (~xorS) & LOW32;
+            long proj = 0;
+            for (int p = 31; p >= 0; p--) {
+                if ((m & (1L << p)) == 0) continue;
+                if (basis[p] == 0) continue;
+                if ((proj & (1L << p)) == 0) {
+                    proj ^= basis[p];
+                }
+            }
+            proj &= m;
+            long curr = andB + xorS + 2 * proj;
+            if (curr > ans) ans = curr;
+        }
+        return ans;
     }
 }
 # @lc code=end
