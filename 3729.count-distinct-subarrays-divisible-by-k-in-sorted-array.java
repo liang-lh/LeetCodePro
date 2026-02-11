9e648c2b0113bc07cff4adad17ac2a43b58b46de
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 03:11:14 +0800

    Batch add 1 solutions: 3729.count-distinct-...

--- 3729.count-distinct-subarrays-divisible-by-k-in-sorted-array.java
+++ 3729.count-distinct-subarrays-divisible-by-k-in-sorted-array.java
@@ -7,82 +7,69 @@
 # @lc code=start
 class Solution {
     public long numGoodSubarrays(int[] nums, int k) {
-        long K = k;
         int n = nums.length;
-        java.util.List<java.lang.Long> vals = new java.util.ArrayList<>();
-        java.util.List<java.lang.Integer> cnts = new java.util.ArrayList<>();
-        for (int i = 0; i < n; ) {
-            long v = nums[i];
-            int j = i;
-            while (j < n && nums[j] == nums[i]) ++j;
-            vals.add(v);
-            cnts.add(j - i);
-            i = j;
+        if (n == 0) return 0;
+        long kk = k;
+        long[] modpref = new long[n + 1];
+        modpref[0] = 0;
+        long cur = 0;
+        for (int i = 0; i < n; i++) {
+            cur = (cur + (long) nums[i]) % kk;
+            modpref[i + 1] = cur;
         }
-        int m = cnts.size();
-        long[] prefixMod = new long[m + 1];
-        prefixMod[0] = 0;
-        for (int g = 0; g < m; ++g) {
-            long v = vals.get(g);
-            int c = cnts.get(g);
-            long groupSumMod = ((long) c * v) % K;
-            prefixMod[g + 1] = (prefixMod[g] + groupSumMod + K) % K;
-        }
-        long ans = 0;
-        // intra-group
-        for (int g = 0; g < m; ++g) {
-            long vv = vals.get(g) % K;
-            long gg = gcd(vv, K);
-            long period = K / gg;
-            ans += (long) cnts.get(g) / period;
-        }
-        // inter-group
-        java.util.Map<java.lang.Long, java.lang.Long> futureRight = new java.util.HashMap<>();
-        for (int s = m - 1; s >= 0; --s) {
-            // left_freq
-            java.util.Map<java.lang.Long, java.lang.Long> leftF = new java.util.HashMap<>();
-            long Vs = vals.get(s) % K;
-            long Ms1 = prefixMod[s + 1];
-            int cs = cnts.get(s);
-            for (int a = 1; a <= cs; ++a) {
-                long ra = ((long) a * Vs) % K;
-                long lmod = (ra - Ms1 + 2 * K) % K;
-                leftF.put(lmod, leftF.getOrDefault(lmod, 0L) + 1L);
-            }
-            // contributions
-            for (java.util.Map.Entry<java.lang.Long, java.lang.Long> e : leftF.entrySet()) {
-                long r = e.getKey();
-                long cl = e.getValue();
-                long need = (K - r + K) % K;
-                long cr = futureRight.getOrDefault(need, 0L);
-                ans += cl * cr;
-            }
-            // right_freq
-            java.util.Map<java.lang.Long, java.lang.Long> rightF = new java.util.HashMap<>();
-            long Vt = vals.get(s) % K;
-            long Mt = prefixMod[s];
-            int ct = cnts.get(s);
-            for (int b = 1; b <= ct; ++b) {
-                long rb = ((long) b * Vt) % K;
-                long rmod = (rb + Mt + K) % K;
-                rightF.put(rmod, rightF.getOrDefault(rmod, 0L) + 1L);
-            }
-            // add to futureRight
-            for (java.util.Map.Entry<java.lang.Long, java.lang.Long> e : rightF.entrySet()) {
-                long rmod = e.getKey();
-                long cc = e.getValue();
-                futureRight.put(rmod, futureRight.getOrDefault(rmod, 0L) + cc);
+        int[] run_starts_arr = new int[n];
+        int num_runs = 0;
+        run_starts_arr[num_runs++] = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i - 1]) {
+                run_starts_arr[num_runs++] = i;
             }
         }
-        return ans;
-    }
-    private static long gcd(long a, long b) {
-        while (b != 0) {
-            long temp = b;
-            b = a % b;
-            a = temp;
+        java.util.HashMap<Long, Long> prev_count = new java.util.HashMap<>();
+        long multi = 0;
+        for (int g = 0; g < num_runs; g++) {
+            int st = run_starts_arr[g];
+            int next_st = (g + 1 < num_runs) ? run_starts_arr[g + 1] : n;
+            int en = next_st - 1;
+            for (int j = st; j <= en; j++) {
+                long needed = modpref[j + 1];
+                Long cntt = prev_count.get(needed);
+                if (cntt != null) {
+                    multi += cntt;
+                }
+            }
+            for (int i = st; i <= en; i++) {
+                long md = modpref[i];
+                Long val = prev_count.get(md);
+                long newc = (val == null ? 1L : val + 1L);
+                prev_count.put(md, newc);
+            }
         }
-        return a;
+        long singles = 0;
+        for (int g = 0; g < num_runs; g++) {
+            int st = run_starts_arr[g];
+            int next_st = (g + 1 < num_runs) ? run_starts_arr[g + 1] : n;
+            long cv = (long) (next_st - st);
+            long v = nums[st];
+            long modv = v % kk;
+            long addd = 0;
+            if (modv == 0) {
+                addd = cv;
+            } else {
+                long aa = modv;
+                long bb = kk;
+                while (bb != 0) {
+                    long temp = bb;
+                    bb = aa % bb;
+                    aa = temp;
+                }
+                long dd = aa;
+                long pp = kk / dd;
+                addd = cv / pp;
+            }
+            singles += addd;
+        }
+        return multi + singles;
     }
 }
 # @lc code=end
