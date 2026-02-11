b3e125bb6031c0d3f537f7f9cd81517dc184f716
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 04:07:17 +0800

    Batch add 1 solutions: 3777.minimum-deletio...

--- 3777.minimum-deletions-to-make-alternating-substring.java
+++ 3777.minimum-deletions-to-make-alternating-substring.java
@@ -6,83 +6,69 @@
 
 # @lc code=start
 class Solution {
-    static class FenwickTree {
+    private static class FenwickTree {
         private int[] tree;
         private int n;
-        
-        public FenwickTree(int nn) {
-            n = nn;
-            tree = new int[nn + 1];
+
+        public FenwickTree(int size) {
+            this.n = size;
+            this.tree = new int[size + 1];
         }
-        
-        public void update(int idx, int delta) {
-            while (idx <= n) {
-                tree[idx] += delta;
-                idx += idx & -idx;
+
+        public void update(int index, int delta) {
+            while (index <= this.n) {
+                this.tree[index] += delta;
+                index += index & -index;
             }
         }
-        
-        public int query(int idx) {
+
+        public int query(int index) {
             int sum = 0;
-            while (idx > 0) {
-                sum += tree[idx];
-                idx -= idx & -idx;
+            while (index > 0) {
+                sum += this.tree[index];
+                index -= index & -index;
             }
             return sum;
         }
-        
+
         public int query(int left, int right) {
-            if (left > right) return 0;
-            return query(right) - query(left - 1);
+            return this.query(right) - this.query(left - 1);
         }
     }
-    
+
     public int[] minDeletions(String s, int[][] queries) {
         int n = s.length();
-        char[] sc = s.toCharArray();
+        char[] ch = s.toCharArray();
         FenwickTree ft = new FenwickTree(n - 1);
-        for (int i = 0; i < n - 1; i++) {
-            int val = (sc[i] == sc[i + 1] ? 1 : 0);
-            ft.update(i + 1, val);
+        for (int i = 0; i < n - 1; ++i) {
+            int d = (ch[i] != ch[i + 1]) ? 1 : 0;
+            ft.update(i + 1, d);
         }
-        java.util.List<Integer> answer = new java.util.ArrayList<>();
-        for (int[] query : queries) {
-            if (query.length == 2) { // flip
-                int j = query[1];
-                int old_eq_prev = 0;
-                boolean has_prev = j > 0;
-                if (has_prev) {
-                    old_eq_prev = (sc[j - 1] == sc[j] ? 1 : 0);
+        int[] answer = new int[queries.length];
+        int ansIdx = 0;
+        for (int[] q : queries) {
+            if (q[0] == 1) {
+                int j = q[1];
+                int old_left = (j > 0) ? ((ch[j - 1] != ch[j]) ? 1 : 0) : 0;
+                int old_right = (j < n - 1) ? ((ch[j] != ch[j + 1]) ? 1 : 0) : 0;
+                ch[j] = (ch[j] == 'A') ? 'B' : 'A';
+                int new_left = (j > 0) ? ((ch[j - 1] != ch[j]) ? 1 : 0) : 0;
+                int new_right = (j < n - 1) ? ((ch[j] != ch[j + 1]) ? 1 : 0) : 0;
+                if (j > 0) {
+                    ft.update(j, new_left - old_left);
                 }
-                int old_eq_next = 0;
-                boolean has_next = j < n - 1;
-                if (has_next) {
-                    old_eq_next = (sc[j] == sc[j + 1] ? 1 : 0);
+                if (j < n - 1) {
+                    ft.update(j + 1, new_right - old_right);
                 }
-                // flip
-                sc[j] = (sc[j] == 'A' ? 'B' : 'A');
-                // update prev pair eq[j-1]
-                if (has_prev) {
-                    int new_eq_prev = (sc[j - 1] == sc[j] ? 1 : 0);
-                    ft.update(j, new_eq_prev - old_eq_prev);
-                }
-                // update next pair eq[j]
-                if (has_next) {
-                    int new_eq_next = (sc[j] == sc[j + 1] ? 1 : 0);
-                    ft.update(j + 1, new_eq_next - old_eq_next);
-                }
-            } else { // query [2, l, r]
-                int l = query[1];
-                int r = query[2];
-                int dels = (l == r) ? 0 : ft.query(l + 1, r);
-                answer.add(dels);
+            } else {
+                int l = q[1];
+                int r = q[2];
+                int sum_diff = (l < r) ? ft.query(l + 1, r) : 0;
+                int del = (r - l) - sum_diff;
+                answer[ansIdx++] = del;
             }
         }
-        int[] res = new int[answer.size()];
-        for (int i = 0; i < answer.size(); i++) {
-            res[i] = answer.get(i);
-        }
-        return res;
+        return java.util.Arrays.copyOf(answer, ansIdx);
     }
 }
 # @lc code=end
