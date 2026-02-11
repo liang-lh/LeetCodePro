fa1400e3aed7774fdec788b249bdc572b780f929
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 13:04:56 +0800

    Batch add 1 solutions: 3341.find-minimum-ti...

--- 3341.find-minimum-time-to-reach-last-room-i.java
+++ 3341.find-minimum-time-to-reach-last-room-i.java
@@ -3,62 +3,43 @@
 #
 # [3341] Find Minimum Time to Reach Last Room I
 #
+
 # @lc code=start
 import java.util.*;
-
 class Solution {
     public int minTimeToReach(int[][] moveTime) {
         int n = moveTime.length;
         int m = moveTime[0].length;
-        
-        // Priority queue: [time, row, col]
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.offer(new int[]{0, 0, 0});
-        
-        // Track minimum time to reach each cell
-        int[][] minTime = new int[n][m];
+        long[][] dist = new long[n][m];
         for (int i = 0; i < n; i++) {
-            Arrays.fill(minTime[i], Integer.MAX_VALUE);
+            for (int j = 0; j < m; j++) {
+                dist[i][j] = Long.MAX_VALUE / 2;
+            }
         }
-        minTime[0][0] = 0;
-        
-        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
+        dist[0][0] = 0;
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
+        pq.offer(new long[]{0, 0, 0});
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int time = curr[0];
-            int row = curr[1];
-            int col = curr[2];
-            
-            // If we reached the destination
-            if (row == n - 1 && col == m - 1) {
-                return time;
-            }
-            
-            // Skip if we've already found a better path
-            if (time > minTime[row][col]) {
-                continue;
-            }
-            
-            // Try all 4 directions
-            for (int[] dir : directions) {
-                int newRow = row + dir[0];
-                int newCol = col + dir[1];
-                
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m) {
-                    // Calculate arrival time: wait for room to open + 1 second to move
-                    int arrivalTime = Math.max(time, moveTime[newRow][newCol]) + 1;
-                    
-                    // Only process if this is a better path
-                    if (arrivalTime < minTime[newRow][newCol]) {
-                        minTime[newRow][newCol] = arrivalTime;
-                        pq.offer(new int[]{arrivalTime, newRow, newCol});
+            long[] curr = pq.poll();
+            long dtime = curr[0];
+            int x = (int) curr[1];
+            int y = (int) curr[2];
+            if (dtime > dist[x][y]) continue;
+            for (int[] dir : dirs) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    long depart = Math.max(dtime, (long) moveTime[nx][ny]);
+                    long ndist = depart + 1;
+                    if (ndist < dist[nx][ny]) {
+                        dist[nx][ny] = ndist;
+                        pq.offer(new long[]{ndist, nx, ny});
                     }
                 }
             }
         }
-        
-        return -1; // Should never reach here given the constraints
+        return (int) dist[n - 1][m - 1];
     }
 }
 # @lc code=end
