c320e8724a88c6b488826201d0252a2a482d3c96
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 01:45:05 +0800

    Batch add 1 solutions: 3563.lexicographical...

--- 3563.lexicographically-smallest-string-after-adjacent-removals.java
+++ 3563.lexicographically-smallest-string-after-adjacent-removals.java
@@ -8,48 +8,38 @@
 class Solution {
     public String lexicographicallySmallestString(String s) {
         int n = s.length();
-        boolean[][] can = new boolean[n+1][n+1];
+        boolean[][] can = new boolean[n + 1][n + 1];
         for (int i = 0; i <= n; i++) {
             can[i][i] = true;
         }
-        for (int len = 1; len <= n; len++) {
+        for (int len = 2; len <= n; len += 2) {
             for (int i = 0; i <= n - len; i++) {
                 int j = i + len;
-                boolean possible = false;
-                for (int k = i + 1; k < j; k++) {
-                    if (can[i][k] && can[k][j]) {
-                        possible = true;
+                for (int p = i + 1; p < j; p += 2) {
+                    int x = s.charAt(i) - 'a';
+                    int y = s.charAt(p) - 'a';
+                    int d = Math.abs(x - y);
+                    if ((d == 1 || d == 25) && can[i + 1][p] && can[p + 1][j]) {
+                        can[i][j] = true;
                         break;
                     }
                 }
-                if (len >= 2) {
-                    char left = s.charAt(i);
-                    char right = s.charAt(j - 1);
-                    int dx = Math.abs((left - 'a') - (right - 'a'));
-                    boolean consec = (dx == 1) || (left == 'a' && right == 'z') || (left == 'z' && right == 'a');
-                    if (consec && can[i + 1][j - 1]) {
-                        possible = true;
-                    }
-                }
-                can[i][j] = possible;
             }
         }
         String[] best = new String[n + 1];
         best[n] = "";
         for (int i = n - 1; i >= 0; i--) {
-            String minStr = s.substring(i);
-            if (can[i][n]) {
-                minStr = "";
-            }
-            for (int k = i; k < n; k++) {
+            String cand1 = s.charAt(i) + best[i + 1];
+            String min_str = cand1;
+            for (int k = i + 2; k <= n; k += 2) {
                 if (can[i][k]) {
-                    String cand = s.charAt(k) + best[k + 1];
-                    if (cand.compareTo(minStr) < 0) {
-                        minStr = cand;
+                    String cand = best[k];
+                    if (cand.compareTo(min_str) < 0) {
+                        min_str = cand;
                     }
                 }
             }
-            best[i] = minStr;
+            best[i] = min_str;
         }
         return best[0];
     }
