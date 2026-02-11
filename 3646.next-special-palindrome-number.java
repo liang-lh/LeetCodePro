5164348eb4bfb8d7a1bbf3f4396e9495de4daec6
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 19:03:46 +0800

    Batch add 1 solutions: 3646.next-special-pa...

--- 3646.next-special-palindrome-number.java
+++ 3646.next-special-palindrome-number.java
@@ -1,3 +1,4 @@
+import java.util.*;
 #
 # @lc app=leetcode id=3646 lang=java
 #
@@ -5,49 +6,81 @@
 #
 # @lc code=start
 class Solution {
+    private static final String MAX_LONG_STR = "9223372036854775807";
+
     public long specialPalindrome(long n) {
-        long candidate = n + 1;
-        while (candidate <= 1_000_000_000_000_000L) {
-            if (isSpecialPalindrome(candidate)) {
-                return candidate;
+        List<Long> specials = new ArrayList<>();
+        for (int mask = 1; mask < (1 << 9); mask++) {
+            int L = 0;
+            int[] req = new int[10];
+            for (int k = 1; k <= 9; k++) {
+                if ((mask & (1 << (k - 1))) != 0) {
+                    L += k;
+                    req[k] = k;
+                }
             }
-            candidate++;
+            if (L == 0 || L > 19) continue;
+            int num_odd = 0;
+            int odd_d = -1;
+            for (int d = 1; d <= 9; d++) {
+                if (req[d] % 2 == 1) {
+                    num_odd++;
+                    odd_d = d;
+                }
+            }
+            boolean is_odd_len = (L % 2 == 1);
+            if ((is_odd_len && num_odd != 1) || (!is_odd_len && num_odd != 0)) continue;
+            int[] half_cnt = new int[10];
+            for (int d = 1; d <= 9; d++) {
+                half_cnt[d] = req[d] / 2;
+            }
+            int left_len = L / 2;
+            char[] path = new char[left_len];
+            int[] remain = new int[10];
+            for (int d = 1; d <= 9; d++) remain[d] = half_cnt[d];
+            generate(0, path, remain, specials, L, odd_d, left_len);
         }
-        return -1;
+        Collections.sort(specials);
+        for (long x : specials) {
+            if (x > n) {
+                return x;
+            }
+        }
+        return -1L; // Theoretically unreachable
     }
-    
-    private boolean isSpecialPalindrome(long num) {
-        String s = String.valueOf(num);
-        
-        if (!isPalindrome(s)) {
-            return false;
+
+    private void generate(int pos, char[] path, int[] remain, List<Long> specials, int L, int odd_d, int left_len) {
+        if (pos == left_len) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < left_len; i++) {
+                sb.append(path[i]);
+            }
+            String left = sb.toString();
+            if (L % 2 == 1) {
+                sb.append((char) ('0' + odd_d));
+            }
+            sb.append(new StringBuilder(left).reverse().toString());
+            String numStr = sb.toString();
+            if (numStr.length() == 19 && numStr.compareTo(MAX_LONG_STR) > 0) {
+                return;
+            }
+            long num;
+            try {
+                num = Long.parseLong(numStr);
+            } catch (NumberFormatException e) {
+                return;
+            }
+            specials.add(num);
+            return;
         }
-        
-        int[] freq = new int[10];
-        for (char c : s.toCharArray()) {
-            freq[c - '0']++;
-        }
-        
-        for (int digit = 0; digit <= 9; digit++) {
-            if (freq[digit] > 0 && freq[digit] != digit) {
-                return false;
+        for (int d = 1; d <= 9; d++) {
+            if (remain[d] > 0) {
+                path[pos] = (char) ('0' + d);
+                remain[d]--;
+                generate(pos + 1, path, remain, specials, L, odd_d, left_len);
+                remain[d]++;
             }
         }
-        
-        return true;
-    }
-    
-    private boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
     }
 }
 # @lc code=end
