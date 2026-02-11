1689da9155c90e3bf8b66cd8df14ff0193d6b735
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 03:44:31 +0800

    Batch add 1 solutions: 3768.minimum-inversi...

--- 3768.minimum-inversion-count-in-subarrays-of-fixed-length.java
+++ 3768.minimum-inversion-count-in-subarrays-of-fixed-length.java
@@ -6,82 +6,72 @@
 
 # @lc code=start
 class Solution {
-  public long minInversionCount(int[] nums, int k) {
-    int n = nums.length;
-    if (k == 0 || n == 0) return 0;
-    int[] sortedVals = new int[n];
-    for (int i = 0; i < n; i++) {
-      sortedVals[i] = nums[i];
-    }
-    Arrays.sort(sortedVals);
-    int maxr = 0;
-    if (n > 0) maxr = 1;
-    for (int i = 1; i < n; i++) {
-      if (sortedVals[i] != sortedVals[i - 1]) {
-        maxr++;
-      }
-    }
-    int[] uniqueVals = new int[maxr];
-    if (maxr > 0) {
-      int ptr = 0;
-      uniqueVals[ptr++] = sortedVals[0];
-      for (int i = 1; i < n; i++) {
-        if (sortedVals[i] != sortedVals[i - 1]) {
-          uniqueVals[ptr++] = sortedVals[i];
-        }
-      }
-    }
-    int[] rank = new int[n];
-    for (int i = 0; i < n; i++) {
-      rank[i] = Arrays.binarySearch(uniqueVals, 0, maxr, nums[i]) + 1;
-    }
     class Fenwick {
-      long[] tree;
-      int nn;
-      public Fenwick(int _n) {
-        nn = _n;
-        tree = new long[_n + 2];
-      }
-      public void update(int idx, long val) {
-        while (idx <= nn) {
-          tree[idx] += val;
-          idx += idx & -idx;
+        private int[] tree;
+        private int n;
+        public Fenwick(int _n) {
+            n = _n;
+            tree = new int[n + 2];
         }
-      }
-      public long query(int idx) {
-        long s = 0;
-        while (idx > 0) {
-          s += tree[idx];
-          idx -= idx & -idx;
+        public void update(int idx, int val) {
+            while (idx <= n) {
+                tree[idx] += val;
+                idx += idx & -idx;
+            }
         }
-        return s;
-      }
-      public long query(int l, int r) {
-        if (l > r) return 0;
-        return query(r) - query(l - 1);
-      }
+        public int query(int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += tree[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
     }
-    Fenwick ft = new Fenwick(maxr);
-    long inv = 0;
-    for (int i = 0; i < k; i++) {
-      int r = rank[i];
-      inv += ft.query(r + 1, maxr);
-      ft.update(r, 1);
+    public long minInversionCount(int[] nums, int k) {
+        int len = nums.length;
+        int[] sortedVals = nums.clone();
+        java.util.Arrays.sort(sortedVals);
+        int m = 1;
+        for (int i = 1; i < len; ++i) {
+            if (sortedVals[i] != sortedVals[i - 1]) {
+                sortedVals[m++] = sortedVals[i];
+            }
+        }
+        int[] ranks = new int[len];
+        for (int i = 0; i < len; ++i) {
+            int v = nums[i];
+            int low = 0, high = m;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                if (sortedVals[mid] < v) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            ranks[i] = low + 1;
+        }
+        Fenwick ft = new Fenwick(m);
+        long inv = 0;
+        int L = 0;
+        for (int i = 0; i < k; ++i) {
+            int rk = ranks[i];
+            inv += (long)ft.query(m) - ft.query(rk);
+            ft.update(rk, 1);
+        }
+        long minInv = inv;
+        for (int i = k; i < len; ++i) {
+            int rkL = ranks[L];
+            inv -= ft.query(rkL - 1);
+            ft.update(rkL, -1);
+            ++L;
+            int rk = ranks[i];
+            inv += (long)ft.query(m) - ft.query(rk);
+            ft.update(rk, 1);
+            if (inv < minInv) minInv = inv;
+        }
+        return minInv;
     }
-    long ans = inv;
-    for (int st = 1; st <= n - k; st++) {
-      int rm = rank[st - 1];
-      long sub = ft.query(1, rm - 1);
-      inv -= sub;
-      ft.update(rm, -1);
-      int adidx = st + k - 1;
-      int ad = rank[adidx];
-      long adinv = ft.query(ad + 1, maxr);
-      inv += adinv;
-      ft.update(ad, 1);
-      if (inv < ans) ans = inv;
-    }
-    return ans;
-  }
 }
 # @lc code=end
