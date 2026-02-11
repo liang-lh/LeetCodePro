36ad8db7905bcde45c4139cf18d168c1e90e0708
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 02:45:04 +0800

    Batch add 1 solutions: 3695.maximize-altern...

--- 3695.maximize-alternating-sum-using-swaps.java
+++ 3695.maximize-alternating-sum-using-swaps.java
@@ -7,66 +7,67 @@
 # @lc code=start
 import java.util.*;
 class Solution {
-    private static class UF {
-        int[] p;
+    public long maxAlternatingSum(int[] nums, int[][] swaps) {
+        int n = nums.length;
+        UnionFind uf = new UnionFind(n);
+        for (int[] sw : swaps) {
+            uf.union(sw[0], sw[1]);
+        }
+        Map<Integer, List<Integer>> compValues = new HashMap<>();
+        Map<Integer, Integer> compEvenCnt = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int root = uf.find(i);
+            compValues.computeIfAbsent(root, k -> new ArrayList<>()).add(nums[i]);
+            if ((i & 1) == 0) {
+                compEvenCnt.put(root, compEvenCnt.getOrDefault(root, 0) + 1);
+            }
+        }
+        long total = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : compValues.entrySet()) {
+            List<Integer> vals = entry.getValue();
+            vals.sort((a, b) -> Integer.compare(b, a));
+            int rootKey = entry.getKey();
+            int E = compEvenCnt.getOrDefault(rootKey, 0);
+            int sz = vals.size();
+            long contrib = 0;
+            for (int j = 0; j < E; j++) {
+                contrib += vals.get(j);
+            }
+            for (int j = E; j < sz; j++) {
+                contrib -= vals.get(j);
+            }
+            total += contrib;
+        }
+        return total;
+    }
+
+    static class UnionFind {
+        int[] parent;
         int[] rank;
-        UF(int n) {
-            p = new int[n];
+        UnionFind(int n) {
+            parent = new int[n];
             rank = new int[n];
-            for(int i = 0; i < n; i++) {
-                p[i] = i;
-                rank[i] = 0;
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
             }
         }
         int find(int x) {
-            if (p[x] != x) p[x] = find(p[x]);
-            return p[x];
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
         }
         void union(int x, int y) {
             int px = find(x);
             int py = find(y);
             if (px == py) return;
             if (rank[px] < rank[py]) {
-                p[px] = py;
+                parent[px] = py;
             } else {
-                p[py] = px;
+                parent[py] = px;
                 if (rank[px] == rank[py]) rank[px]++;
             }
         }
-    }
-
-    public long maxAlternatingSum(int[] nums, int[][] swaps) {
-        int n = nums.length;
-        UF uf = new UF(n);
-        for (int[] sw : swaps) {
-            uf.union(sw[0], sw[1]);
-        }
-        Map<Integer, List<Integer>> compValues = new HashMap<>();
-        Map<Integer, Integer> posCounts = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            int r = uf.find(i);
-            compValues.computeIfAbsent(r, k -> new ArrayList<>()).add(nums[i]);
-            if (i % 2 == 0) {
-                posCounts.put(r, posCounts.getOrDefault(r, 0) + 1);
-            }
-        }
-        long ans = 0;
-        for (Map.Entry<Integer, List<Integer>> entry : compValues.entrySet()) {
-            List<Integer> vals = entry.getValue();
-            int pc = posCounts.getOrDefault(entry.getKey(), 0);
-            long S = 0;
-            for (int v : vals) {
-                S += (long) v;
-            }
-            Collections.sort(vals, Collections.reverseOrder());
-            long sp = 0;
-            int take = Math.min(pc, vals.size());
-            for (int j = 0; j < take; j++) {
-                sp += (long) vals.get(j);
-            }
-            ans += 2 * sp - S;
-        }
-        return ans;
     }
 }
 # @lc code=end
