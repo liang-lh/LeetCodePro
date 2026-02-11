8aee15711fe4a9a8f6dde88aa4219b1e934ce165
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 19:05:20 +0800

    Batch add 1 solutions: 3670.maximum-product...

--- 3670.maximum-product-of-two-integers-with-no-common-bits.java
+++ 3670.maximum-product-of-two-integers-with-no-common-bits.java
@@ -5,22 +5,58 @@
 #
 # @lc code=start
 class Solution {
+    private static class TrieNode {
+        TrieNode[] children = new TrieNode[2];
+    }
+
     public long maxProduct(int[] nums) {
-        long maxProduct = 0;
-        int n = nums.length;
-        
-        // Check all pairs
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                // Check if nums[i] and nums[j] have no common set bits
-                if ((nums[i] & nums[j]) == 0) {
-                    long product = (long) nums[i] * nums[j];
-                    maxProduct = Math.max(maxProduct, product);
+        TrieNode root = new TrieNode();
+        for (int num : nums) {
+            insert(root, num);
+        }
+        long ans = 0;
+        for (int num : nums) {
+            int cand = getMaxCompatible(root, num);
+            ans = Math.max(ans, (long) num * cand);
+        }
+        return ans;
+    }
+
+    private void insert(TrieNode root, int num) {
+        TrieNode curr = root;
+        for (int i = 19; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            if (curr.children[bit] == null) {
+                curr.children[bit] = new TrieNode();
+            }
+            curr = curr.children[bit];
+        }
+    }
+
+    private int getMaxCompatible(TrieNode root, int x) {
+        TrieNode curr = root;
+        int y = 0;
+        for (int i = 19; i >= 0; i--) {
+            int bitX = (x >> i) & 1;
+            int bitY = 0;
+            TrieNode next = null;
+            if (bitX == 1) {
+                next = curr.children[0];
+            } else {
+                if (curr.children[1] != null) {
+                    bitY = 1;
+                    next = curr.children[1];
+                } else if (curr.children[0] != null) {
+                    next = curr.children[0];
                 }
             }
+            if (next == null) {
+                return 0;
+            }
+            y |= (bitY << i);
+            curr = next;
         }
-        
-        return maxProduct;
+        return y;
     }
 }
 # @lc code=end
