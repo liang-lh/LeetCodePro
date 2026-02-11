76849832c780ffe12f069169dad226c172992cd0
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 20:16:38 +0800

    Batch add 1 solutions: 3757.number-of-effec...

--- 3757.number-of-effective-subsequences.java
+++ 3757.number-of-effective-subsequences.java
@@ -3,55 +3,64 @@
 #
 # [3757] Number of Effective Subsequences
 #
-# @lc code=start
-import java.util.HashMap;
-import java.util.Map;
 
+# @lc code=start
 class Solution {
     public int countEffective(int[] nums) {
-        int MOD = 1000000007;
         int n = nums.length;
-        
-        // Calculate total OR of all elements
-        int totalOR = 0;
-        for (int num : nums) {
-            totalOR |= num;
+        if (n == 0) return 0;
+        int S = 0;
+        for (int x : nums) S |= x;
+        int k = 0;
+        for (int b = 0; b < 32; b++) {
+            if ((S & (1 << b)) != 0) k++;
         }
-        
-        // DP: dp[or_value] = count of subsequences where remaining elements have OR = or_value
-        Map<Integer, Long> dp = new HashMap<>();
-        dp.put(0, 1L);  // Initial state: processed zero elements, remaining OR = 0
-        
-        // Process each element
-        for (int num : nums) {
-            Map<Integer, Long> newDp = new HashMap<>();
-            
-            for (Map.Entry<Integer, Long> entry : dp.entrySet()) {
-                int orVal = entry.getKey();
-                long count = entry.getValue();
-                
-                // Option 1: Remove this element (include in subsequence)
-                // Remaining OR stays the same
-                newDp.put(orVal, (newDp.getOrDefault(orVal, 0L) + count) % MOD);
-                
-                // Option 2: Keep this element (don't include in subsequence)
-                // Remaining OR becomes (orVal | num)
-                int newOr = orVal | num;
-                newDp.put(newOr, (newDp.getOrDefault(newOr, 0L) + count) % MOD);
-            }
-            
-            dp = newDp;
-        }
-        
-        // Count effective subsequences (where remaining OR < totalOR)
-        long result = 0;
-        for (Map.Entry<Integer, Long> entry : dp.entrySet()) {
-            if (entry.getKey() < totalOR) {
-                result = (result + entry.getValue()) % MOD;
+        int[] bits = new int[k];
+        int idx = 0;
+        for (int b = 0; b < 32; b++) {
+            if ((S & (1 << b)) != 0) {
+                bits[idx++] = b;
             }
         }
-        
-        return (int) result;
+        int MS = 1 << k;
+        long[] fr = new long[MS];
+        for (int num : nums) {
+            int mask = 0;
+            for (int j = 0; j < k; j++) {
+                if ((num & (1 << bits[j])) != 0) {
+                    mask |= (1 << j);
+                }
+            }
+            fr[mask]++;
+        }
+        long[] dp = new long[MS];
+        for (int i = 0; i < MS; i++) dp[i] = fr[i];
+        for (int b = 0; b < k; b++) {
+            for (int mask = 0; mask < MS; mask++) {
+                if ((mask & (1 << b)) == 0) {
+                    dp[mask | (1 << b)] += dp[mask];
+                }
+            }
+        }
+        final int MOD = 1000000007;
+        long[] pow2 = new long[n + 1];
+        pow2[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            pow2[i] = pow2[i - 1] * 2L % MOD;
+        }
+        long ans = 0;
+        int full = (1 << k) - 1;
+        for (int m = 1; m < MS; m++) {
+            int pop = Integer.bitCount(m);
+            long disj = dp[full ^ m];
+            long ways = pow2[(int) disj];
+            if ((pop & 1) == 1) {
+                ans = (ans + ways) % MOD;
+            } else {
+                ans = (ans + MOD - ways) % MOD;
+            }
+        }
+        return (int) ans;
     }
 }
 # @lc code=end
