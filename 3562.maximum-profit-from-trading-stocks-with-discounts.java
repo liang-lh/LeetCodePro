e4aee83ed11f5886a29577a8a6722c679ab8253a
liang-lh <liang-lh@users.noreply.github.com>
2026-02-12 01:15:14 +0800

    Batch add 1 solutions: 3562.maximum-profit-...

--- 3562.maximum-profit-from-trading-stocks-with-discounts.java
+++ 3562.maximum-profit-from-trading-stocks-with-discounts.java
@@ -6,96 +6,91 @@
 
 # @lc code=start
 class Solution {
+    private static final int NEG_INF = Integer.MIN_VALUE / 2;
+
     public int maxProfit(int n, int[] present, int[] future, int[][] hierarchy, int budget) {
-        java.util.List<java.util.List<Integer>> children = new java.util.ArrayList<java.util.List<Integer>>(n);
+        @SuppressWarnings("unchecked")
+        java.util.ArrayList<java.lang.Integer>[] children = new java.util.ArrayList[n];
         for (int i = 0; i < n; i++) {
-            children.add(new java.util.ArrayList<Integer>());
+            children[i] = new java.util.ArrayList<>();
         }
         for (int[] h : hierarchy) {
-            children.get(h[0] - 1).add(h[1] - 1);
+            int u = h[0] - 1;
+            int v = h[1] - 1;
+            children[u].add(v);
         }
-        int B = budget;
-        final int NEG_INF = -1000000;
-        int[][][] memo = new int[n][2][B + 1];
+        int[][][] memo = new int[n][2][budget + 1];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 2; j++) {
-                java.util.Arrays.fill(memo[i][j], NEG_INF);
+                java.util.Arrays.fill(memo[i][j], -1);
             }
         }
-        dfs(0, present, future, children, memo, B, NEG_INF);
+        int[] rootDp = getDP(0, 0, children, present, future, budget, memo);
         int ans = 0;
-        for (int s = 0; s <= B; s++) {
-            if (memo[0][0][s] > ans) {
-                ans = memo[0][0][s];
+        for (int c = 0; c <= budget; c++) {
+            if (rootDp[c] != NEG_INF) {
+                ans = Math.max(ans, rootDp[c]);
             }
         }
         return ans;
     }
 
-    private void dfs(int u, int[] present, int[] future, java.util.List<java.util.List<Integer>> children, int[][][] memo, int B, int NEG_INF) {
-        for (int v : children.get(u)) {
-            dfs(v, present, future, children, memo, B, NEG_INF);
+    private int[] getDP(int u, int hasDisc, java.util.ArrayList<java.lang.Integer>[] children, int[] present, int[] future, int maxb, int[][][] memo) {
+        if (memo[u][hasDisc][0] != -1) {
+            int[] res = new int[maxb + 1];
+            for (int c = 0; c <= maxb; c++) {
+                res[c] = memo[u][hasDisc][c];
+            }
+            return res;
         }
-        // Compute child_no knapsack (no discount for children)
-        int[] child_no = new int[B + 1];
-        java.util.Arrays.fill(child_no, NEG_INF);
-        child_no[0] = 0;
-        for (int v : children.get(u)) {
-            int[] dpv = memo[v][0];
-            int[] temp = new int[B + 1];
-            java.util.Arrays.fill(temp, NEG_INF);
-            for (int s = 0; s <= B; s++) {
-                if (child_no[s] == NEG_INF) continue;
-                for (int t = 0; t <= B - s; t++) {
-                    if (dpv[t] != NEG_INF) {
-                        temp[s + t] = Math.max(temp[s + t], child_no[s] + dpv[t]);
-                    }
+        int fullCost = present[u];
+        int discCost = fullCost / 2;
+        int costU = hasDisc == 1 ? discCost : fullCost;
+        int profitU = future[u] - costU;
+        int[] childNo = getCombined(children[u], 0, children, present, future, maxb, memo);
+        int[] childYes = getCombined(children[u], 1, children, present, future, maxb, memo);
+        int[] res = new int[maxb + 1];
+        java.util.Arrays.fill(res, NEG_INF);
+        // not buy u
+        for (int c = 0; c <= maxb; c++) {
+            if (childNo[c] != NEG_INF) {
+                res[c] = Math.max(res[c], childNo[c]);
+            }
+        }
+        // buy u
+        for (int c2 = 0; c2 <= maxb - costU; c2++) {
+            if (childYes[c2] != NEG_INF) {
+                int totalC = costU + c2;
+                int totalP = profitU + childYes[c2];
+                res[totalC] = Math.max(res[totalC], totalP);
+            }
+        }
+        // memoize
+        for (int c = 0; c <= maxb; c++) {
+            memo[u][hasDisc][c] = res[c];
+        }
+        return res;
+    }
+
+    private int[] getCombined(java.util.ArrayList<java.lang.Integer> childs, int chDisc, java.util.ArrayList<java.lang.Integer>[] children, int[] present, int[] future, int maxb, int[][][] memo) {
+        int[] curr = new int[maxb + 1];
+        java.util.Arrays.fill(curr, NEG_INF);
+        curr[0] = 0;
+        for (int v : childs) {
+            int[] childDp = getDP(v, chDisc, children, present, future, maxb, memo);
+            int[] nextDp = new int[maxb + 1];
+            java.util.Arrays.fill(nextDp, NEG_INF);
+            for (int c1 = 0; c1 <= maxb; c1++) {
+                if (curr[c1] == NEG_INF) continue;
+                for (int k = 0; k <= maxb - c1; k++) {
+                    if (childDp[k] == NEG_INF) continue;
+                    int nc = c1 + k;
+                    nextDp[nc] = Math.max(nextDp[nc], curr[c1] + childDp[k]);
                 }
             }
-            child_no = temp;
+            curr = nextDp;
         }
-        // Compute child_yes knapsack (discount for children)
-        int[] child_yes = new int[B + 1];
-        java.util.Arrays.fill(child_yes, NEG_INF);
-        child_yes[0] = 0;
-        for (int v : children.get(u)) {
-            int[] dpv = memo[v][1];
-            int[] temp = new int[B + 1];
-            java.util.Arrays.fill(temp, NEG_INF);
-            for (int s = 0; s <= B; s++) {
-                if (child_yes[s] == NEG_INF) continue;
-                for (int t = 0; t <= B - s; t++) {
-                    if (dpv[t] != NEG_INF) {
-                        temp[s + t] = Math.max(temp[s + t], child_yes[s] + dpv[t]);
-                    }
-                }
-            }
-            child_yes = temp;
-        }
-        // Compute for both bp
-        for (int bp = 0; bp < 2; bp++) {
-            int cost_buy = (bp == 1) ? present[u] / 2 : present[u];
-            int profit_buy = future[u] - cost_buy;
-            int[] res = new int[B + 1];
-            java.util.Arrays.fill(res, NEG_INF);
-            // Case 1: u does not buy
-            for (int s = 0; s <= B; s++) {
-                if (child_no[s] != NEG_INF) {
-                    res[s] = Math.max(res[s], child_no[s]);
-                }
-            }
-            // Case 2: u buys
-            if (cost_buy <= B) {
-                for (int s = 0; s <= B - cost_buy; s++) {
-                    if (child_yes[s] != NEG_INF) {
-                        int tot_s = cost_buy + s;
-                        int tot_p = profit_buy + child_yes[s];
-                        res[tot_s] = Math.max(res[tot_s], tot_p);
-                    }
-                }
-            }
-            System.arraycopy(res, 0, memo[u][bp], 0, B + 1);
-        }
+        return curr;
     }
 }
 # @lc code=end
