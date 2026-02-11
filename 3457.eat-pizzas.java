64a7abac8e084ddcf3d3bff3804c7e961cdeea93
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 14:43:28 +0800

    Batch add 1 solutions: 3457.eat-pizzas.java...

--- 3457.eat-pizzas.java
+++ 3457.eat-pizzas.java
@@ -3,35 +3,25 @@
 #
 # [3457] Eat Pizzas!
 #
-# @lc code=start
-import java.util.Arrays;
 
+# @lc code=start
 class Solution {
     public long maxWeight(int[] pizzas) {
         int n = pizzas.length;
-        Arrays.sort(pizzas); // Sort in ascending order
-        
-        int totalDays = n / 4;
-        long totalWeight = 0;
-        int left = n - 1; // Pointer to largest pizzas
-        int right = 0;    // Pointer to smallest pizzas
-        
-        for (int day = 1; day <= totalDays; day++) {
-            if (day % 2 == 1) { // Odd day - gain Z (max of 4)
-                totalWeight += pizzas[left]; // The largest pizza
-                left--;        // Move to next largest
-                right += 3;    // Consume 3 smallest
-            } else { // Even day - gain Y (2nd max of 4)
-                // Consuming [left, left-1] as two largest and [right, right+1] as two smallest
-                // After sorting the 4: [right, right+1, left-1, left]
-                // Y = left-1 (2nd largest)
-                totalWeight += pizzas[left - 1];
-                left -= 2;     // Consume top 2
-                right += 2;    // Consume bottom 2
-            }
+        java.util.Arrays.sort(pizzas);
+        int k = n / 4;
+        int numOddDays = (k + 1) / 2;
+        int numEvenDays = k / 2;
+        long total = 0;
+        for (int i = 0; i < numOddDays; i++) {
+            total += pizzas[n - 1 - i];
         }
-        
-        return totalWeight;
+        int startPos = numOddDays;
+        for (int j = 0; j < numEvenDays; j++) {
+            int pos = startPos + 2 * j + 1;
+            total += pizzas[n - 1 - pos];
+        }
+        return total;
     }
 }
 # @lc code=end
