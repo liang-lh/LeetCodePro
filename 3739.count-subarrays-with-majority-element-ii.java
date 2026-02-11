7eec34fb9bafaf06076b930ebc6db930335b3731
liang-lh <liang-lh@users.noreply.github.com>
2026-02-11 19:48:59 +0800

    Batch add 1 solutions: 3739.count-subarrays...

--- 3739.count-subarrays-with-majority-element-ii.java
+++ 3739.count-subarrays-with-majority-element-ii.java
@@ -3,26 +3,49 @@
 #
 # [3739] Count Subarrays With Majority Element II
 #
+
 # @lc code=start
 class Solution {
-    public long countMajoritySubarrays(int[] nums, int target) {
-        long result = 0;
-        int n = nums.length;
-        
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = i; j < n; j++) {
-                if (nums[j] == target) {
-                    count++;
-                }
-                int length = j - i + 1;
-                if (2 * count > length) {
-                    result++;
-                }
+    private static class FenwickTree {
+        int[] tree;
+        int n;
+        public FenwickTree(int _n) {
+            n = _n;
+            tree = new int[_n + 1];
+        }
+        public void update(int idx, int delta) {
+            while (idx <= n) {
+                tree[idx] += delta;
+                idx += idx & -idx;
             }
         }
-        
-        return result;
+        public long query(int idx) {
+            long sum = 0;
+            while (idx > 0) {
+                sum += tree[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    }
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int OFFSET = 100001;
+        int MAXN = 200010;
+        FenwickTree ft = new FenwickTree(MAXN);
+        int[] prefix = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + (nums[i - 1] == target ? 1 : -1);
+        }
+        long ans = 0;
+        ft.update(prefix[0] + OFFSET, 1);
+        for (int j = 1; j <= n; j++) {
+            int cur = prefix[j] + OFFSET;
+            ans += ft.query(cur - 1);
+            ft.update(cur, 1);
+        }
+        return ans;
     }
 }
 # @lc code=end
